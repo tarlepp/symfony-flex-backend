bc297c42817b300104ce1b00250500c4555d8acc
tarlepp <tarmo.leppanen@protacon.com>
2017-05-07 15:55:08 +0300

    Base tests for DefaultController

---
+++ tests/Functional/Controller/DefaultControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Functional/Controller/DefaultControllerTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Functional\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class DefaultControllerTest
+ *
+ * @package App\Tests\Functional\Controller
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class DefaultControllerTest extends WebTestCase
+{
+    public function testThatDefaultRouteReturns200(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}
