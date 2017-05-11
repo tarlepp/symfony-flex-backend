2dc9a8b685c976952dfd7db7e97fdc59b2221e21
tarlepp <tarmo.leppanen@protacon.com>
2017-05-11 19:21:11 +0300

    Added User and UserGroup resource classes.

---
+++ src/Resource/UserResource.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Resource/UserResource.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Resource;
+
+use App\Entity\Interfaces\EntityInterface;
+use App\Entity\User as Entity;
+use App\Repository\UserRepository as Repository;
+use App\Rest\DTO\Interfaces\RestDtoInterface;
+use App\Rest\Resource;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/** @noinspection PhpHierarchyChecksInspection */
+
+/**
+ * Class UserResource
+ *
+ * @package App\Resource
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ *
+ * @method Repository  getRepository(): Repository
+ * @method Entity[]    find(array $criteria = null, array $orderBy = null, int $limit = null, int $offset = null, array $search = null): array
+ * @method Entity|null findOne(string $id, bool $throwExceptionIfNotFound = null): ?EntityInterface
+ * @method Entity|null findOneBy(array $criteria, array $orderBy = null, bool $throwExceptionIfNotFound = null): ?EntityInterface
+ * @method Entity      create(RestDtoInterface $dto): EntityInterface
+ * @method Entity      update(string $id, RestDtoInterface $dto): EntityInterface
+ * @method Entity      delete(string $id): EntityInterface
+ * @method Entity      save(EntityInterface $entity, bool $skipValidation = null): EntityInterface
+ */
+class UserResource extends Resource
+{
+    /**
+     * Class constructor.
+     *
+     * @param Repository         $repository
+     * @param ValidatorInterface $validator
+     */
+    public function __construct(Repository $repository, ValidatorInterface $validator)
+    {
+        $this->repository = $repository;
+        $this->validator = $validator;
+    }
+}
