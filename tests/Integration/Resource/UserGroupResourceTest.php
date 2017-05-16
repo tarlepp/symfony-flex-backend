3c3b673dfd7de7a29e7bdfc6b4e7592ba8210eb0
tarlepp <tarmo.leppanen@protacon.com>
2017-05-16 22:40:20 +0300

    tests: User and UserGroup resource tests

---
+++ tests/Integration/Resource/UserGroupResourceTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Integration/Resource/UserResourceTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Resource;
+
+use App\Entity\UserGroup;
+use App\Repository\UserGroupRepository;
+use App\Resource\UserGroupResource;
+use App\Tests\Helpers\ResourceTestCase;
+
+/**
+ * Class UserResourceTest
+ *
+ * @package App\Tests\Integration\Resource
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UserGroupResourceTest extends ResourceTestCase
+{
+    protected $entityClass = UserGroup::class;
+    protected $resourceClass = UserGroupResource::class;
+    protected $repositoryClass = UserGroupRepository::class;
+}
---
+++ tests/Integration/Resource/UserResourceTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Integration/Resource/UserResourceTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Resource;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Resource\UserResource;
+use App\Tests\Helpers\ResourceTestCase;
+
+/**
+ * Class UserResourceTest
+ *
+ * @package App\Tests\Integration\Resource
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UserResourceTest extends ResourceTestCase
+{
+    protected $entityClass = User::class;
+    protected $resourceClass = UserResource::class;
+    protected $repositoryClass = UserRepository::class;
+}
