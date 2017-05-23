21938d0b59e554b2243f7c8f3e5f0d3141fcace2
tarlepp <tarmo.leppanen@protacon.com>
2017-05-23 19:25:05 +0300

    Tests: Added tests for App\Security\Roles class.

---
+++ tests/Unit/Security/RolesTest.php
@@ -0,0 +1,115 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Unit/Security/RolesTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Unit\Security;
+
+use App\Security\Roles;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class RolesTest
+ *
+ * @package App\Tests\Unit\Security
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class RolesTest extends KernelTestCase
+{
+    /**
+     * @var Roles
+     */
+    protected $service;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        static::bootKernel();
+
+        $this->service = static::$kernel->getContainer()->get(Roles::class);
+    }
+
+    public function testThatGetHierarchyReturnsExpected(): void
+    {
+        $expected = [
+            'ROLE_USER' => [
+                'ROLE_LOGGED',
+            ],
+            'ROLE_ADMIN' => [
+                'ROLE_USER',
+            ],
+            'ROLE_ROOT' => [
+                'ROLE_ADMIN',
+            ]
+        ];
+
+        static::assertSame($expected, $this->service->getHierarchy(), 'Roles hierarchy is not expected.');
+    }
+
+    public function testThatGetRolesReturnsExpected(): void
+    {
+        static::assertSame(
+            [
+                'ROLE_LOGGED',
+                'ROLE_USER',
+                'ROLE_ADMIN',
+                'ROLE_ROOT',
+            ],
+            $this->service->getRoles(),
+            'Returned roles are not expected.'
+        );
+    }
+
+    /**
+     * @dataProvider dataProviderTestThatGetRoleLabelReturnsExpected
+     *
+     * @param string $role
+     * @param string $expected
+     */
+    public function testThatGetRoleLabelReturnsExpected(string $role, string $expected): void
+    {
+        static::assertSame($expected, $this->service->getRoleLabel($role), 'Role label was not expected one.');
+    }
+
+    /**
+     * @dataProvider dataProviderTestThatGetShortReturnsExpected
+     *
+     * @param string $input
+     * @param string $expected
+     */
+    public function testThatGetShortReturnsExpected(string $input, string $expected): void
+    {
+        static::assertSame($expected, $this->service->getShort($input), 'Short role name was not expected');
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestThatGetRoleLabelReturnsExpected(): array
+    {
+        return [
+            [Roles::ROLE_LOGGED, 'Logged in users'],
+            [Roles::ROLE_USER, 'Normal users'],
+            [Roles::ROLE_ADMIN, 'Admin users'],
+            [Roles::ROLE_ROOT, 'Root users'],
+            ['Not supported role', 'Unknown - Not supported role'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestThatGetShortReturnsExpected(): array
+    {
+        return [
+            [Roles::ROLE_LOGGED, 'logged'],
+            [Roles::ROLE_USER, 'user'],
+            [Roles::ROLE_ADMIN, 'admin'],
+            [Roles::ROLE_ROOT, 'root'],
+            ['SOME_CUSTOM_ROLE', 'custom_role']
+        ];
+    }
+}
