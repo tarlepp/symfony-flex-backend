ae05d267c3e6291571a8237cdf5ffa886f8a3be7
tarlepp <tarmo.leppanen@protacon.com>
2017-06-03 18:22:06 +0300

    feat: Added user entity subscriber to encode password correctly.

---
+++ src/EventSubscriber/UserEntitySubscriber.php
@@ -0,0 +1,125 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/EventSubscriber/UserEntitySubscriber.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+
+/**
+ * Class UserEntitySubscriber
+ *
+ * @package App\EventSubscriber
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UserEntitySubscriber
+{
+    /**
+     * Used encoder factory.
+     *
+     * @var EncoderFactoryInterface
+     */
+    private $encoderFactory;
+
+    /**
+     * Constructor
+     *
+     * @param EncoderFactoryInterface $encoderFactory
+     */
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    /**
+     * Getter for user password encoder factory.
+     *
+     * @param User $user
+     *
+     * @return PasswordEncoderInterface
+     *
+     * @throws \RuntimeException
+     */
+    public function getEncoder(User $user): PasswordEncoderInterface
+    {
+        return $this->encoderFactory->getEncoder($user);
+    }
+
+    /**
+     * Doctrine lifecycle event for 'prePersist' event.
+     *
+     * @param LifecycleEventArgs $event
+     *
+     * @return void
+     *
+     * @throws \RuntimeException
+     */
+    public function prePersist(LifecycleEventArgs $event): void
+    {
+        // Get user entity object
+        $user = $event->getEntity();
+
+        // Valid user so lets change password
+        if ($user instanceof User) {
+            $this->changePassword($user);
+        }
+    }
+
+    /**
+     * Doctrine lifecycle event for 'preUpdate' event.
+     *
+     * @param PreUpdateEventArgs $event
+     *
+     * @return void
+     *
+     * @throws \RuntimeException
+     */
+    public function preUpdate(PreUpdateEventArgs $event): void
+    {
+        // Get user entity object
+        $user = $event->getEntity();
+
+        // Valid user so lets change password
+        if ($user instanceof User) {
+            $this->changePassword($user);
+        }
+    }
+
+    /**
+     * Method to change user password whenever it's needed.
+     *
+     * @param User $user
+     *
+     * @return void
+     *
+     * @throws \RuntimeException
+     */
+    protected function changePassword(User $user): void
+    {
+        // Get plain password from user entity
+        $plainPassword = $user->getPlainPassword();
+
+        // Yeah, we have new plain password set, so we need to encode it
+        if (!empty($plainPassword)) {
+            $encoder = $this->getEncoder($user);
+
+            // Password hash callback
+            $callback = function ($plainPassword) use ($encoder, $user) {
+                return $encoder->encodePassword($plainPassword, $user->getSalt());
+            };
+
+            // Set new password and encode it with user encoder
+            $user->setPassword($callback, $plainPassword);
+
+            // And clean up plain password from entity
+            $user->eraseCredentials();
+        }
+    }
+}
