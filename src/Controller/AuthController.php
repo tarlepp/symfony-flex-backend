447becfd12f66c0d5f42fec3f3a61cbe9735955d
tarlepp <tarmo.leppanen@protacon.com>
2017-06-16 20:03:38 +0300

    feat: Added endpoint action to fetch current user profile data.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -7,9 +7,15 @@ declare(strict_types=1);
  */
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Security\RolesInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class AuthController
@@ -38,5 +44,49 @@ class AuthController
     public function getTokenAction(): Response
     {
         return new Response('', 405);
+    }
+
+    /**
+     * Action to get current user profile data.
+     *
+     * @Method("GET")
+     *
+     * @Route("/profile");
+     *
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     *
+     * @param UserInterface|User  $user
+     * @param RolesInterface      $roles
+     * @param SerializerInterface $serializer
+     *
+     * @return JsonResponse
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function profileAction(
+        UserInterface $user,
+        RolesInterface $roles,
+        SerializerInterface $serializer
+    ): JsonResponse
+    {
+        // Specify used serialization groups
+        static $groups = [
+            'User',
+            'User.userGroups',
+            'User.roles',
+            'UserGroup',
+            'UserGroup.role',
+        ];
+
+        // Set roles service to user entity, so we can get inherited roles
+        $user->setRolesService($roles);
+
+        // Create response
+        return new JsonResponse(
+            $serializer->serialize($user, 'json', ['groups' => $groups]),
+            200,
+            [],
+            true
+        );
     }
 }
