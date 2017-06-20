7e4dc3e58a9f3a4e77668cee2e09c5c9d2e8f23d
tarlepp <tarmo.leppanen@protacon.com>
2017-06-20 23:06:37 +0300

    tests: Added generic ContainerTestCase

---
+++ src/Utils/Tests/ContainerTestCase.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Utils/Tests/ContainerTestCase.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Utils\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class ContainerTestCase
+ *
+ * @package App\Utils\Tests;
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+abstract class ContainerTestCase extends KernelTestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * Getter method for container
+     *
+     * @return ContainerInterface
+     */
+    public function getContainer(): ContainerInterface
+    {
+        if (!($this->container instanceof ContainerInterface)) {
+            self::bootKernel();
+
+            $this->container = static::$kernel->getContainer();
+        }
+
+        return $this->container;
+    }
+}
