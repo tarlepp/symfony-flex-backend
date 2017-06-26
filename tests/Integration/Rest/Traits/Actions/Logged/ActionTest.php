90a2dedaab8edacadbb80397dbc9ef4b0c387d9a
tarlepp <tarmo.leppanen@protacon.com>
2017-06-26 23:25:58 +0300

    tests: Added action tests for 'Authenticated' and 'Logged' users.

---
+++ tests/Integration/Rest/Traits/Actions/Logged/ActionTest.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/Rest/Traits/Actions/Logged/ActionTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Rest\Traits\Actions\Logged;
+
+use App\Utils\Tests\PHPUnitUtil;
+use Ramsey\Uuid\Uuid;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ActionTest
+ *
+ * @package App\Tests\Integration\Rest\Traits\Actions\Logged
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ActionTest extends KernelTestCase
+{
+    /**
+     * @dataProvider dataProviderTestThatTraitCallsExpectedMethod
+     *
+     * @param string $className
+     * @param string $method
+     * @param string $traitMethod
+     * @param array  $parameters
+     */
+    public function testThatTraitCallsExpectedMethod(
+        string $className,
+        string $method,
+        string $traitMethod,
+        array $parameters
+    ): void
+    {
+        $stub = $this->getMockForTrait(
+            $className,
+            [],
+            '',
+            true,
+            true,
+            true,
+            [$traitMethod]
+        );
+
+        $stub
+            ->expects(static::once())
+            ->method($traitMethod)
+            ->with(...$parameters);
+
+        $result = \call_user_func_array([$stub, $method], $parameters);
+
+        static::assertInstanceOf(Response::class, $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestThatTraitCallsExpectedMethod(): array
+    {
+        self::bootKernel();
+
+        $folder = static::$kernel->getRootDir() . '/Rest/Traits/Actions/Logged/';
+        $pattern = '/^.+\.php$/i';
+
+        $namespace = '\\App\\Rest\\Traits\\Actions\\Logged\\';
+
+        $iterator = function (string $filename) use ($folder, $namespace): array {
+            $base = \str_replace([$folder, \DIRECTORY_SEPARATOR, '.php'], ['', '\\', ''], $filename);
+            $class = $namespace . $base;
+
+            $parameters = [
+                $request = $this->createMock(Request::class),
+            ];
+
+            switch ($base) {
+                case 'DeleteAction':
+                case 'FindOneAction':
+                case 'UpdateAction':
+                    $parameters[] = Uuid::uuid4()->toString();
+                    break;
+            }
+
+            return [
+                $class,
+                \lcfirst($base),
+                \str_replace('Action',  'Method', \lcfirst($base)),
+                $parameters,
+            ];
+        };
+
+        return \array_map($iterator, PHPUnitUtil::recursiveFileSearch($folder, $pattern));
+    }
+}
