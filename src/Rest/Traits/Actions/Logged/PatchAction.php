78b72f99bd6b4cfd58f5edc918e4bafaa84f4642
tarlepp <tarmo.leppanen@protacon.com>
2017-06-29 18:06:24 +0300

    feat: Added generic PATCH actions for REST

---
+++ src/Rest/Traits/Actions/Logged/PatchAction.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Rest/Traits/Actions/Logged/PatchAction.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Rest\Traits\Actions\Logged;
+
+use App\Rest\Traits\Methods\PatchMethod;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Trait PatchAction
+ *
+ * Trait to add 'patchAction' for REST controllers for 'ROLE_LOGGED' users.
+ *
+ * @see \App\Rest\Traits\Methods\PatchMethod for detailed documents.
+ *
+ * @package App\Rest\Traits\Actions\Root
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+trait PatchAction
+{
+    use PatchMethod;
+
+    /**
+     * @Route(
+     *      "/{id}",
+     *      requirements={
+     *          "id" = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
+     *      }
+     *  )
+     *
+     * @Method({"PATCH"})
+     *
+     * @Security("has_role('ROLE_LOGGED')")
+     *
+     * @param Request              $request
+     * @param FormFactoryInterface $formFactory
+     * @param string               $id
+     *
+     * @return Response
+     *
+     * @throws \LogicException
+     * @throws \UnexpectedValueException
+     * @throws \Symfony\Component\OptionsResolver\Exception\InvalidOptionsException
+     * @throws \Symfony\Component\Form\Exception\LogicException
+     * @throws \Symfony\Component\Form\Exception\AlreadySubmittedException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     * @throws \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    public function patchAction(Request $request, FormFactoryInterface $formFactory, string $id): Response
+    {
+        return $this->patchMethod($request, $formFactory, $id);
+    }
+}
