880728e914e7a1be769fa6085ad4cfbeee8d4fa7
tarlepp <tarmo.leppanen@protacon.com>
2017-07-01 15:09:35 +0300

    tests: Added missing tests for App\Rest\DTO\UserGroup

--- tests/Integration/Rest/DTO/UserGroupTest.php
+++ tests/Integration/Rest/DTO/UserGroupTest.php
@@ -7,6 +7,9 @@ declare(strict_types=1);
  */
 namespace App\Tests\Integration\Rest\DTO;
 
+use App\Entity\EntityInterface;
+use App\Entity\Role as RoleEntity;
+use App\Entity\UserGroup as UserGroupEntity;
 use App\Rest\DTO\UserGroup;
 
 /**
@@ -18,4 +21,67 @@ use App\Rest\DTO\UserGroup;
 class UserGroupTest extends DtoTestCase
 {
     protected $dtoClass = UserGroup::class;
+
+    public function testThatLoadCallsExpectedEntityMethods(): void
+    {
+        /** @var \PHPUnit_Framework_MockObject_MockObject|EntityInterface|RoleEntity $userGroupEntity */
+        $roleEntity = $this->getMockBuilder(EntityInterface::class)
+            ->setMethods(['getId'])
+            ->getMock();
+
+        $roleEntity
+            ->expects(static::once())
+            ->method('getId')
+            ->willReturn('test role id');
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject|EntityInterface|UserGroupEntity $userGroupEntity */
+        $userGroupEntity = $this->getMockBuilder(EntityInterface::class)
+            ->setMethods(['getId', 'getName', 'getRole'])
+            ->getMock();
+
+        $userGroupEntity
+            ->expects(static::once())
+            ->method('getName')
+            ->willReturn('test name');
+
+        $userGroupEntity
+            ->expects(static::once())
+            ->method('getRole')
+            ->willReturn($roleEntity);
+
+        /** @var UserGroup $dto */
+        $dto = new $this->dtoClass();
+        $dto->load($userGroupEntity);
+
+        static::assertSame('test name', $dto->getName());
+        static::assertSame('test role id', $dto->getRole());
+    }
+
+    public function testThatUpdateMethodCallsExpectedEntityMethods(): void
+    {
+        $roleEntity = new RoleEntity('test role');
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject|EntityInterface|UserGroupEntity $userGroupEntity */
+        $userGroupEntity = $this->getMockBuilder(EntityInterface::class)
+            ->setMethods(['getId', 'setName', 'setRole'])
+            ->getMock();
+
+        $userGroupEntity
+            ->expects(static::once())
+            ->method('setName')
+            ->with('test name')
+            ->willReturn($userGroupEntity);
+
+        $userGroupEntity
+            ->expects(static::once())
+            ->method('setRole')
+            ->with($roleEntity)
+            ->willReturn($userGroupEntity);
+
+        /** @var UserGroup $dto */
+        $dto = new $this->dtoClass();
+        $dto->setName('test name');
+        $dto->setRole($roleEntity);
+        $dto->update($userGroupEntity);
+    }
 }
