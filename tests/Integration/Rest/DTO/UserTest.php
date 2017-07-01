7fffb2b61fc0eb9ba24b65a5fd210831aea2bfcd
tarlepp <tarmo.leppanen@protacon.com>
2017-07-01 14:22:32 +0300

    tests: Added missing tests for App\Rest\DTO\User

--- tests/Integration/Rest/DTO/UserTest.php
+++ tests/Integration/Rest/DTO/UserTest.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
  */
 namespace App\Tests\Integration\Rest\DTO;
 
+use App\Entity\EntityInterface;
 use App\Rest\DTO\User;
 
 /**
@@ -18,4 +19,49 @@ use App\Rest\DTO\User;
 class UserTest extends DtoTestCase
 {
     protected $dtoClass = User::class;
+
+    public function testThatUpdateMethodCallsExpectedEntityMethodIfPasswordIsVisited(): void
+    {
+        /** @var \PHPUnit_Framework_MockObject_MockObject|EntityInterface $entity */
+        $entity = $this->getMockBuilder(EntityInterface::class)
+            ->setMethods(['getId', 'setPlainPassword'])
+            ->getMock();
+
+        $entity
+            ->expects(static::once())
+            ->method('setPlainPassword')
+            ->with('password');
+
+        /** @var User $dto */
+        $dto = new $this->dtoClass();
+        $dto->setPassword('password');
+        $dto->update($entity);
+    }
+
+    public function testThatUpdateMethodCallsExpectedEntityMethodsIfUserGroupsIsVisited(): void
+    {
+        $userGroups = [
+            'foo',
+            'bar',
+        ];
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject|EntityInterface $entity */
+        $entity = $this->getMockBuilder(EntityInterface::class)
+            ->setMethods(['getId', 'clearUserGroups', 'addUserGroup'])
+            ->getMock();
+
+        $entity
+            ->expects(static::once())
+            ->method('clearUserGroups');
+
+        $entity
+            ->expects(static::exactly(\count($userGroups)))
+            ->method('addUserGroup')
+            ->willReturn($entity);
+
+        /** @var User $dto */
+        $dto = new $this->dtoClass();
+        $dto->setUserGroups($userGroups);
+        $dto->update($entity);
+    }
 }
