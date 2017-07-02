b903607390f1e95f4e3e7805c634d9b9f0f18b10
tarlepp <tarmo.leppanen@protacon.com>
2017-07-02 15:34:20 +0300

    tests: Added missing test for App\Entity\Role

---
+++ tests/Unit/Entity/RoleTest.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Unit/Entity/RoleTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\Role;
+use App\Entity\UserGroup;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class RoleTest
+ *
+ * @package App\Tests\Unit\Entity
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class RoleTest extends KernelTestCase
+{
+    public function testThatGetUserGroupsWorksLikeExpected(): void
+    {
+        $userGroup = new UserGroup();
+        $userGroup->setName('some name');
+
+        $role = new Role('some role');
+        $role->getUserGroups()->add($userGroup);
+
+        static::assertTrue($role->getUserGroups()->contains($userGroup));
+    }
+}
