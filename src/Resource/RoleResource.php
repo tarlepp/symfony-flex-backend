6839370acfbcc85043dca158928af89af03c09de
tarlepp <tarmo.leppanen@protacon.com>
2017-07-03 18:53:28 +0300

    feat: Added App\Resource\RoleResource

---
+++ src/Resource/RoleResource.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Resource/RoleResource.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Resource;
+
+use App\Entity\EntityInterface;
+use App\Entity\Role as Entity;
+use App\Repository\RoleRepository as Repository;
+use App\Rest\DTO\RestDtoInterface;
+use App\Rest\Resource;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/** @noinspection PhpHierarchyChecksInspection */
+/** @noinspection PhpMissingParentCallCommonInspection */
+
+/**
+ * Class RoleResource
+ *
+ * @package App\Resource
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ *
+ * @method Repository  getRepository(): Repository
+ * @method Entity[]    find(array $criteria = null, array $orderBy = null, int $limit = null, int $offset = null, array $search = null): array
+ * @method Entity|null findOne(string $id, bool $throwExceptionIfNotFound = null): ?EntityInterface
+ * @method Entity|null findOneBy(array $criteria, array $orderBy = null, bool $throwExceptionIfNotFound = null): ?EntityInterface
+ * @method Entity      create(RestDtoInterface $dto): EntityInterface
+ * @method Entity      update(string $id, RestDtoInterface $dto): EntityInterface
+ * @method Entity      delete(string $id): EntityInterface
+ * @method Entity      save(EntityInterface $entity, bool $skipValidation = null): EntityInterface
+ */
+class RoleResource extends Resource
+{
+    /**
+     * Class constructor.
+     *
+     * @param Repository         $repository
+     * @param ValidatorInterface $validator
+     */
+    public function __construct(Repository $repository, ValidatorInterface $validator)
+    {
+        $this->repository = $repository;
+        $this->validator = $validator;
+    }
+}
---
+++ tests/Integration/Resource/RoleResourceTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Integration/Resource/RoleResourceTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Resource;
+
+use App\Entity\Role;
+use App\Repository\RoleRepository;
+use App\Resource\RoleResource;
+
+/**
+ * Class RoleResourceTest
+ *
+ * @package App\Tests\Integration\Resource
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class RoleResourceTest extends ResourceTestCase
+{
+    protected $entityClass = Role::class;
+    protected $resourceClass = RoleResource::class;
+    protected $repositoryClass = RoleRepository::class;
+}
