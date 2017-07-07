43a75ecc115e536676fb2043c133e3b94ad3740e
tarlepp <tarmo.leppanen@protacon.com>
2017-07-07 20:17:38 +0300

    feat: Added RestApiDoc annotation

---
+++ src/Annotation/RestApiDoc.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Annotation/RestApiDoc.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/** @noinspection EmptyClassInspection */
+/**
+ * Class RestApiDoc
+ *
+ * @Annotation
+ * @Annotation\Target("METHOD")
+ *
+ * @package App\Annotation
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class RestApiDoc
+{
+}
