7749e57ac10ec5aa6e611cad1c919df314b82a79
tarlepp <tarmo.leppanen@protacon.com>
2017-07-24 15:17:46 +0300

    feat: Added UniqueUsername constraint and validator.

---
+++ src/Validator/Constraints/UniqueUsername.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Validator/Constraints/UniqueUsername.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Class UniqueUsername
+ *
+ * @Annotation
+ * @Target({"CLASS"})
+ *
+ * @package App\Validator\Constraints
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UniqueUsername extends Constraint
+{
+    // Unique constant for validator constrain
+    const IS_UNIQUE_USERNAME_ERROR = 'ea62740a-4d9b-4a25-9a56-46fb4c3d5fea';
+
+    // Error names configuration
+    protected static $errorNames = [
+        self::IS_UNIQUE_USERNAME_ERROR => 'IS_UNIQUE_USERNAME_ERROR'
+    ];
+
+    // Message for validation error
+    public $message = 'This username is already taken.';
+
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * Returns whether the constraint can be put onto classes, properties or both.
+     *
+     * This method should return one or more of the constants
+     * Constraint::CLASS_CONSTRAINT and Constraint::PROPERTY_CONSTRAINT.
+     *
+     * @return string|array One or more constant values
+     */
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
---
+++ src/Validator/Constraints/UniqueUsernameValidator.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Validator/Constraints/UniqueUsernameValidator.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Validator\Constraints;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Class UniqueUsernameValidator
+ *
+ * @package App\Validator\Constraints
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UniqueUsernameValidator extends ConstraintValidator
+{
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    /**
+     * UniqueUsernameValidator constructor.
+     *
+     * @param UserRepository $repository
+     */
+    public function __construct(UserRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Checks if the passed value is valid.
+     *
+     * In this case check if 'username' is available or not within User repository.
+     *
+     * @throws  NonUniqueResultException
+     *
+     * @param User                      $value      The value that should be validated
+     * @param Constraint|UniqueUsername $constraint The constraint for the validation
+     */
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$this->repository->isUsernameAvailable($value->getUsername(), $value->getId())) {
+            $this->context->buildViolation($constraint->message)->addViolation();
+        }
+    }
+}
