8b2693772412a53fbc831c4c7b0ea5aa469e9dc4
tarlepp <tarmo.leppanen@protacon.com>
2017-07-26 01:44:04 +0300

    tests: Added tests for UniqueEmailValidator

---
+++ tests/Integration/Validator/Constraints/UniqueEmailValidatorTest.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Validator/Constraints/UniqueEmailValidatorTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Validator\Constraints;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Validator\Constraints\UniqueEmail;
+use App\Validator\Constraints\UniqueEmailValidator;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Validator\Context\ExecutionContext;
+use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
+
+/**
+ * Class UniqueEmailValidatorTest
+ *
+ * @package App\Validator\Constraints
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UniqueEmailValidatorTest extends KernelTestCase
+{
+    /**
+     * @var UniqueEmail
+     */
+    private $constraint;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ExecutionContext
+     */
+    private $context;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ConstraintViolationBuilderInterface
+     */
+    private $builder;
+
+    public function setUp(): void
+    {
+        $this->constraint = new UniqueEmail();
+        $this->context = $this->getMockBuilder(ExecutionContext::class)->disableOriginalConstructor()->getMock();
+        $this->builder = $this->getMockBuilder(ConstraintViolationBuilderInterface::class)->getMock();
+    }
+
+    public function testThatValidateCallsExpectedMethods(): void
+    {
+        // Create new user
+        $user = new User();
+        $user->setEmail('john.doe@test.com');
+
+        /**
+         * @var \PHPUnit_Framework_MockObject_MockObject|UserRepository $repository
+         */
+        $repository = $this->getMockBuilder(UserRepository::class)->disableOriginalConstructor()->getMock();
+
+        $repository
+            ->expects(static::once())
+            ->method('isEmailAvailable')
+            ->with($user->getEmail(), $user->getId())
+            ->willReturn(false);
+
+        $this->context
+            ->expects(static::once())
+            ->method('buildViolation')
+            ->with($this->constraint->message)
+            ->willReturn($this->builder);
+
+        $this->builder
+            ->expects(static::once())
+            ->method('addViolation');
+
+        // Run validator
+        $validator = new UniqueEmailValidator($repository);
+        $validator->initialize($this->context);
+        $validator->validate($user, $this->constraint);
+    }
+}
