8347b14900f19d7c82bac122375b8132079ae2de
tarlepp <tarmo.leppanen@protacon.com>
2017-08-11 15:31:44 +0300

    refactor: Console form type

--- src/Form/Type/Console/UserGroupType.php
+++ src/Form/Type/Console/UserGroupType.php
@@ -7,16 +7,7 @@ declare(strict_types=1);
  */
 namespace App\Form\Type\Console;
 
-use App\Entity\Role as RoleEntity;
-use App\Form\DataTransformer\RoleTransformer;
-use App\Repository\RoleRepository;
-use App\Rest\DTO\UserGroup as UserGroupDto;
-use App\Security\Roles;
-use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Form\Type\Rest\UserGroup\UserGroupType as UserGroupBaseType;
 
 /**
  * Class UserGroupType
@@ -24,107 +15,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  * @package App\Form\Type\Console
  * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
-class UserGroupType extends AbstractType
+class UserGroupType extends UserGroupBaseType
 {
-    /**
-     * @var Roles
-     */
-    private $roles;
-
-    /**
-     * @var RoleRepository
-     */
-    private $roleRepository;
-
-    /**
-     * @var ObjectManager
-     */
-    private $objectManager;
-
-    /**
-     * UserGroupType constructor.
-     *
-     * @param Roles          $roles
-     * @param RoleRepository $roleRepository
-     * @param ObjectManager  $objectManager
-     */
-    public function __construct(
-        Roles $roles,
-        RoleRepository $roleRepository,
-        ObjectManager $objectManager
-    )
-    {
-        $this->roles = $roles;
-        $this->roleRepository = $roleRepository;
-        $this->objectManager = $objectManager;
-    }
-
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
-     *
-     * @throws \Symfony\Component\Form\Exception\InvalidArgumentException
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add(
-                'name',
-                Type\TextType::class,
-                [
-                    'label'         => 'Group name',
-                    'required'      => true,
-                    'empty_data'    => '',
-                ]
-            )
-            ->add(
-                'role',
-                Type\ChoiceType::class,
-                [
-                    'label'         => 'Role',
-                    'choices'       => $this->getRoleChoices(),
-                    'required'      => true,
-                    'empty_data'    => '',
-                ]
-            );
-
-        $builder->get('role')->addModelTransformer(new RoleTransformer($this->objectManager));
-    }
-
-    /**
-     * Method to create choices array for user groups.
-     *
-     * @return  array
-     */
-    public function getRoleChoices(): array
-    {
-        // Initialize output
-        $choices = [];
-
-        $helper = $this->roles;
-
-        $iterator = function (RoleEntity $role) use (&$choices, $helper) {
-            $name = $helper->getRoleLabel($role->getId());
-
-            $choices[$name] = $role->getId();
-        };
-
-        \array_map($iterator, $this->roleRepository->findAll());
-
-        return $choices;
-    }
-
-    /**
-     * Configures the options for this type.
-     *
-     * @param OptionsResolver $resolver The resolver for the options
-     *
-     * @throws \Symfony\Component\OptionsResolver\Exception\AccessException
-     */
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => UserGroupDto::class,
-        ]);
-    }
 }
