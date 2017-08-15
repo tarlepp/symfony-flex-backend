95c4c706efbb05e5714063f058810e0bc22fdaf2
tarlepp <tarmo.leppanen@protacon.com>
2017-08-15 17:30:54 +0300

    feat: Added LoginLogger service

---
+++ src/Utils/LoginLogger.php
@@ -0,0 +1,157 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Utils/LoginLogger.php
+ *
+ * @Book  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Utils;
+
+use App\Entity\LoginLog;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Resource\LoginLogResource;
+use DeviceDetector\DeviceDetector;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Monolog\Logger;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Class LoginLogger
+ *
+ * @package App\Utils
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class LoginLogger implements LoginLoggerInterface
+{
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * @var LoginLogResource
+     */
+    private $loginLogResource;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @var string
+     */
+    private $agent;
+
+    /**
+     * @var DeviceDetector
+     */
+    private $deviceDetector;
+
+    /**
+     * LoginLogger constructor.
+     *
+     * @param LoggerInterface  $logger
+     * @param LoginLogResource $loginLogResource
+     * @param UserRepository   $userRepository
+     * @param RequestStack     $requestStack
+     */
+    public function __construct(
+        LoggerInterface $logger,
+        LoginLogResource $loginLogResource,
+        UserRepository $userRepository,
+        RequestStack $requestStack
+    ) {
+        // Store used services
+        $this->logger = $logger;
+        $this->loginLogResource = $loginLogResource;
+        $this->userRepository = $userRepository;
+        $this->request = $requestStack->getCurrentRequest();
+    }
+
+    /**
+     * Setter for User object
+     *
+     * @param UserInterface $user
+     *
+     * @return LoginLoggerInterface
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\UsernameNotFoundException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function setUser(UserInterface $user): LoginLoggerInterface
+    {
+        // We need to make sure that User object is right one
+        $user = $user instanceof User ? $user : $this->userRepository->loadUserByUsername($user->getUsername());
+
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Method to handle login event.
+     *
+     * @throws \Symfony\Component\HttpFoundation\Exception\SuspiciousOperationException
+     */
+    public function handle(): void
+    {
+        // Specify user agent
+        $this->agent = $this->request->headers->get('User-Agent');
+
+        // Parse user agent data with device detector
+        $this->deviceDetector = new DeviceDetector($this->agent);
+        $this->deviceDetector->parse();
+
+        // Create entry
+        $this->createEntry();
+
+        $this->logger->debug('Created new login entry to database.');
+    }
+
+    /**
+     * Method to create new login entry and store it to database.
+     *
+     * @return LoginLog
+     *
+     * @throws \Symfony\Component\HttpFoundation\Exception\SuspiciousOperationException
+     */
+    private function createEntry(): LoginLog
+    {
+        // Create new login entry
+        $userLogin = new LoginLog();
+        $userLogin->setUser($this->user);
+        $userLogin->setIp((string)$this->request->getClientIp());
+        $userLogin->setHost($this->request->getHost());
+        $userLogin->setAgent($this->agent);
+        $userLogin->setClientType($this->deviceDetector->getClient('type'));
+        $userLogin->setClientName($this->deviceDetector->getClient('name'));
+        $userLogin->setClientShortName($this->deviceDetector->getClient('short_name'));
+        $userLogin->setClientVersion($this->deviceDetector->getClient('version'));
+        $userLogin->setClientEngine($this->deviceDetector->getClient('engine'));
+        $userLogin->setOsName($this->deviceDetector->getOs('name'));
+        $userLogin->setOsShortName($this->deviceDetector->getOs('short_name'));
+        $userLogin->setOsVersion($this->deviceDetector->getOs('version'));
+        $userLogin->setOsPlatform($this->deviceDetector->getOs('platform'));
+        $userLogin->setDeviceName($this->deviceDetector->getDeviceName());
+        $userLogin->setBrandName($this->deviceDetector->getBrandName());
+        $userLogin->setModel($this->deviceDetector->getModel());
+        $userLogin->setLoginTime(new \DateTime('now', new \DateTimeZone('UTC')));
+
+        // And store entry to database
+        return $this->loginLogResource->save($userLogin, true);
+    }
+}
---
+++ src/Utils/LoginLoggerInterface.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Utils/LoginLoggerInterface.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Utils;
+
+use App\Repository\UserRepository;
+use App\Resource\LoginLogResource;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Interface LoginLogger
+ *
+ * @package App\Utils
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+interface LoginLoggerInterface
+{
+    /**
+     * LoginLogger constructor.
+     *
+     * @param LoggerInterface  $logger
+     * @param LoginLogResource $loginLogResource
+     * @param UserRepository   $userRepository
+     * @param RequestStack     $requestStack
+     */
+    public function __construct(
+        LoggerInterface $logger,
+        LoginLogResource $loginLogResource,
+        UserRepository $userRepository,
+        RequestStack $requestStack
+    );
+
+    /**
+     * Setter for User object
+     *
+     * @param UserInterface $user
+     *
+     * @return LoginLoggerInterface
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\UsernameNotFoundException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function setUser(UserInterface $user): LoginLoggerInterface;
+
+    /**
+     * Method to handle login event.
+     *
+     * @throws \Symfony\Component\HttpFoundation\Exception\SuspiciousOperationException
+     */
+    public function handle(): void;
+}
