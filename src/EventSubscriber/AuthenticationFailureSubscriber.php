bc8c9b86ac1ba64db57d524e87b000a9e1db9aa1
tarlepp <tarmo.leppanen@protacon.com>
2017-08-22 20:20:00 +0300

    feat: Added subscriber for lexik_jwt_authentication.on_authentication_failure events

---
+++ src/EventSubscriber/AuthenticationFailureSubscriber.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/EventSubscriber/AuthenticationFailureSubscriber.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\EventSubscriber;
+
+use App\Repository\UserRepository;
+use App\Utils\LoginLogger;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+
+/**
+ * Class AuthenticationFailureSubscriber
+ *
+ * @package App\EventSubscriber
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class AuthenticationFailureSubscriber
+{
+    /**
+     * @var LoginLogger
+     */
+    protected $loginLogger;
+
+    /**
+     * @var UserRepository
+     */
+    protected $userRepository;
+
+    /**
+     * AuthenticationFailureSubscriber constructor.
+     *
+     * @param LoginLogger    $loginLogger
+     * @param UserRepository $userRepository
+     */
+    public function __construct(LoginLogger $loginLogger, UserRepository $userRepository)
+    {
+        $this->loginLogger = $loginLogger;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Method to log login failures to database.
+     *
+     * This method is called when 'lexik_jwt_authentication.on_authentication_success' event is broadcast.
+     *
+     * @param AuthenticationFailureEvent $event
+     *
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @throws \Symfony\Component\HttpFoundation\Exception\SuspiciousOperationException
+     */
+    public function onAuthenticationFailure(AuthenticationFailureEvent $event): void
+    {
+        // Fetch user entity
+        $user = $this->userRepository->loadUserByUsername($event->getException()->getToken()->getUser());
+
+        $this->loginLogger->setUser($user);
+        $this->loginLogger->process(LoginLogger::TYPE_FAILURE);
+    }
+}
