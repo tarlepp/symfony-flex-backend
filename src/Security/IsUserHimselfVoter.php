631dc9c0b09cc3cc9299c0dce9538a2cd008b24b
tarlepp <tarmo.leppanen@protacon.com>
2017-08-31 21:20:43 +0300

    feat: Added IsUserHimself voter

---
+++ src/Security/IsUserHimselfVoter.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Security/IsUserHimselfVoter.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+/**
+ * Class IsUserHimselfVoter
+ *
+ * @package App\Security
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class IsUserHimselfVoter extends Voter
+{
+    const ATTRIBUTE = 'IS_USER_HIMSELF';
+
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute
+     * @param mixed  $subject   The subject to secure, e.g. an object the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject): bool
+    {
+        return $attribute === self::ATTRIBUTE && $subject instanceof User;
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string         $attribute
+     * @param User           $subject
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        return $token->isAuthenticated() && $token->getUser()->getId() === $subject->getId();
+    }
+}
