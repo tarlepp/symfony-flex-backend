79e6d573ec75fb331d2f1aafdaeeaec56171847b
tarlepp <tarmo.leppanen@protacon.com>
2017-09-02 14:49:41 +0300

    Style @Book => @Author

--- src/Utils/LoginLogger.php
+++ src/Utils/LoginLogger.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 /**
  * /src/Utils/LoginLogger.php
  *
- * @Book  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
 namespace App\Utils;
 
--- src/Utils/RequestLogger.php
+++ src/Utils/RequestLogger.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 /**
  * /src/Utils/RequestLogger.php
  *
- * @Book  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
 namespace App\Utils;
 
