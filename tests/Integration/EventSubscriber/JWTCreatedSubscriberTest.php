4f568ce31edd08c364b7345ba2241ba0c30fbb82
tarlepp <tarmo.leppanen@protacon.com>
2017-09-19 17:22:51 +0300

    tests: Added test cases for corner case

--- tests/Integration/EventSubscriber/JWTCreatedSubscriberTest.php
+++ tests/Integration/EventSubscriber/JWTCreatedSubscriberTest.php
@@ -11,6 +11,7 @@ use App\Entity\User;
 use App\EventSubscriber\JWTCreatedSubscriber;
 use App\Security\RolesService;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -23,7 +24,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
  */
 class JWTCreatedSubscriberTest extends KernelTestCase
 {
-    public function testThatFoo(): void
+    public function testThatPayloadContainsExpectedDataWhenRequestIsPresent(): void
     {
         // Create empty JWT payload
         $payload = [];
@@ -41,13 +42,17 @@ class JWTCreatedSubscriberTest extends KernelTestCase
         $requestStack = new RequestStack();
         $requestStack->push($request);
 
-        /** @var \PHPUnit_Framework_MockObject_MockObject|RolesService $roles */
+        /**
+         * @var \PHPUnit_Framework_MockObject_MockObject|RolesService    $roles
+         * @var \PHPUnit_Framework_MockObject_MockObject|LoggerInterface $logger
+         */
         $roles = $this->getMockBuilder(RolesService::class)->disableOriginalConstructor()->getMock();
+        $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
 
         // Create JWTCreatedEvent
         $event = new JWTCreatedEvent($payload, $user);
 
-        $subscriber = new JWTCreatedSubscriber($roles, $requestStack);
+        $subscriber = new JWTCreatedSubscriber($roles, $requestStack, $logger);
         $subscriber->onJWTCreated($event);
 
         $keys = ['exp', 'checksum', 'id', 'firstname', 'surname', 'email', 'roles'];
@@ -55,5 +60,74 @@ class JWTCreatedSubscriberTest extends KernelTestCase
         foreach ($keys as $key) {
             static::assertArrayHasKey($key, $event->getData());
         }
+    }
+
+    public function testThatPayloadContainsExpectedDataWhenRequestIsNotPresent(): void
+    {
+        // Create empty JWT payload
+        $payload = [];
+
+        // Create new user for JWT
+        $user = new User();
+        $user->setFirstname('firstname');
+        $user->setSurname('surname');
+        $user->setEmail('firstname.surname@test.com');
+
+        // Create RequestStack and push pure Request to it
+        $requestStack = new RequestStack();
+
+        /**
+         * @var \PHPUnit_Framework_MockObject_MockObject|RolesService    $roles
+         * @var \PHPUnit_Framework_MockObject_MockObject|LoggerInterface $logger
+         */
+        $roles = $this->getMockBuilder(RolesService::class)->disableOriginalConstructor()->getMock();
+        $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
+
+        // Create JWTCreatedEvent
+        $event = new JWTCreatedEvent($payload, $user);
+
+        $subscriber = new JWTCreatedSubscriber($roles, $requestStack, $logger);
+        $subscriber->onJWTCreated($event);
+
+        $keys = ['exp', 'id', 'firstname', 'surname', 'email', 'roles'];
+
+        foreach ($keys as $key) {
+            static::assertArrayHasKey($key, $event->getData());
+        }
+
+        static::assertArrayNotHasKey('checksum', $event->getData());
+    }
+
+    public function testThatLoggerAlertIsCalledIfRequestDoesNotExist(): void
+    {
+        // Create empty JWT payload
+        $payload = [];
+
+        // Create new user for JWT
+        $user = new User();
+        $user->setFirstname('firstname');
+        $user->setSurname('surname');
+        $user->setEmail('firstname.surname@test.com');
+
+        // Create RequestStack and push pure Request to it
+        $requestStack = new RequestStack();
+
+        /**
+         * @var \PHPUnit_Framework_MockObject_MockObject|RolesService    $roles
+         * @var \PHPUnit_Framework_MockObject_MockObject|LoggerInterface $logger
+         */
+        $roles = $this->getMockBuilder(RolesService::class)->disableOriginalConstructor()->getMock();
+        $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
+
+        $logger
+            ->expects(static::once())
+            ->method('alert')
+            ->with('Request not available');
+
+        // Create JWTCreatedEvent
+        $event = new JWTCreatedEvent($payload, $user);
+
+        $subscriber = new JWTCreatedSubscriber($roles, $requestStack, $logger);
+        $subscriber->onJWTCreated($event);
     }
 }
