241f25ea9b76d4061c31ff895e578d346a4ba513
tarlepp <tarmo.leppanen@protacon.com>
2017-09-24 13:41:39 +0300

    feat: Added ApiKey controller and resource.

---
+++ src/Controller/ApiKeyController.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Controller/ApiKeyController.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Controller;
+
+use App\Resource\ApiKeyResource;
+use App\Rest\Controller;
+use App\Rest\Traits\Actions;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+/** @noinspection PhpHierarchyChecksInspection */
+/** @noinspection PhpMissingParentCallCommonInspection */
+/**
+ * Class ApiKeyController
+ *
+ * @Route(path="/api_key")
+ *
+ * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+ *
+ * @SWG\Tag(name="ApiKey Management")
+ *
+ * @package App\Controller
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ *
+ * @method ApiKeyResource getResource()
+ */
+class ApiKeyController extends Controller
+{
+    // Traits for REST actions
+    use Actions\Root\CountAction;
+    use Actions\Root\FindAction;
+    use Actions\Root\FindOneAction;
+    use Actions\Root\IdsAction;
+    use Actions\Root\CreateAction;
+    use Actions\Root\DeleteAction;
+    use Actions\Root\PatchAction;
+    use Actions\Root\UpdateAction;
+}
---
+++ src/Resource/ApiKeyResource.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Resource/ApiKeyResource.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Resource;
+
+use App\DTO\RestDtoInterface;
+use App\Entity\EntityInterface;
+use App\Entity\User as Entity;
+use App\Repository\UserRepository as Repository;
+use App\Rest\RestResource;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/** @noinspection PhpHierarchyChecksInspection */
+/** @noinspection PhpMissingParentCallCommonInspection */
+/**
+ * Class ApiKeyResource
+ *
+ * @package App\Resource
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ *
+ * @codingStandardsIgnoreStart
+ *
+ * @method Repository  getRepository(): Repository
+ * @method Entity[]    find(array $criteria = null, array $orderBy = null, int $limit = null, int $offset = null, array $search = null): array
+ * @method Entity|null findOne(string $id, bool $throwExceptionIfNotFound = null): ?EntityInterface
+ * @method Entity|null findOneBy(array $criteria, array $orderBy = null, bool $throwExceptionIfNotFound = null): ?EntityInterface
+ * @method Entity      create(RestDtoInterface $dto, bool $skipValidation = null): EntityInterface
+ * @method Entity      update(string $id, RestDtoInterface $dto, bool $skipValidation = null): EntityInterface
+ * @method Entity      delete(string $id): EntityInterface
+ * @method Entity      save(EntityInterface $entity, bool $skipValidation = null): EntityInterface
+ *
+ * @codingStandardsIgnoreEnd
+ */
+class ApiKeyResource extends RestResource
+{
+    /**
+     * LogLoginFailureResource constructor.
+     *
+     * @param Repository         $repository
+     * @param ValidatorInterface $validator
+     */
+    public function __construct(Repository $repository, ValidatorInterface $validator)
+    {
+        $this->setRepository($repository);
+        $this->setValidator($validator);
+    }
+}
