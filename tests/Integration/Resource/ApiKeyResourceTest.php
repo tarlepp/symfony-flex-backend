a7851c738c1911b02c63aec23e56c167aa57d179
tarlepp <tarmo.leppanen@protacon.com>
2017-09-24 14:47:57 +0300

    tests: Added integration test for ApiKeyResource class

---
+++ tests/Integration/Resource/ApiKeyResourceTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Integration/Resource/ApiKeyResourceTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Resource;
+
+use App\Entity\ApiKey;
+use App\Repository\ApiKeyRepository;
+use App\Resource\ApiKeyResource;
+
+/**
+ * Class ApiKeyResourceTest
+ *
+ * @package App\Tests\Integration\Resource
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ApiKeyResourceTest extends ResourceTestCase
+{
+    protected $entityClass = ApiKey::class;
+    protected $resourceClass = ApiKeyResource::class;
+    protected $repositoryClass = ApiKeyRepository::class;
+}
