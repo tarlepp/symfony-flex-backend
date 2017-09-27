ac455ab2c01804f05db19cb0e1d8aa29607165e4
tarlepp <tarmo.leppanen@protacon.com>
2017-09-27 16:29:38 +0300

    feat: Added console command to list API keys

---
+++ src/Command/ApiKey/ListApiKeysCommand.php
@@ -0,0 +1,123 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Command/ApiKey/ListApiKeysCommand.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Command\ApiKey;
+
+use App\Entity\ApiKey;
+use App\Entity\UserGroup;
+use App\Resource\ApiKeyResource;
+use App\Security\RolesService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Class ListApiKeysCommand
+ *
+ * @package App\Command\ApiKey
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ListApiKeysCommand extends Command
+{
+    /**
+     * @var ApiKeyResource
+     */
+    private $apiKeyResource;
+
+    /**
+     * @var RolesService
+     */
+    private $rolesService;
+
+    /**
+     * @var SymfonyStyle
+     */
+    private $io;
+
+    /**
+     * ListUsersCommand constructor.
+     *
+     * @param ApiKeyResource $apiKeyResource
+     * @param RolesService   $rolesService
+     *
+     * @throws \Symfony\Component\Console\Exception\LogicException
+     */
+    public function __construct(ApiKeyResource $apiKeyResource, RolesService $rolesService)
+    {
+        parent::__construct('api-key:list');
+
+        $this->apiKeyResource = $apiKeyResource;
+        $this->rolesService = $rolesService;
+
+        $this->setDescription('Console command to list API keys');
+    }
+
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * Executes the current command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+        $this->io->write("\033\143");
+
+        static $headers = [
+            'Id',
+            'Token',
+            'Description',
+            'Groups',
+            'Roles (inherited)',
+        ];
+
+        $this->io->title('Current API keys');
+        $this->io->table($headers, $this->getRows());
+
+        return null;
+    }
+
+    /**
+     * @return array
+     */
+    private function getRows(): array
+    {
+        /**
+         * @param UserGroup $userGroup
+         *
+         * @return string
+         */
+        $formatterGroup = function (UserGroup $userGroup): string {
+            return \sprintf(
+                '%s (%s)',
+                $userGroup->getName(),
+                $userGroup->getRole()->getId()
+            );
+        };
+
+        /**
+         * @param ApiKey $apiToken
+         *
+         * @return array
+         */
+        $formatterUser = function (ApiKey $apiToken) use ($formatterGroup): array {
+            return [
+                $apiToken->getId(),
+                $apiToken->getToken(),
+                $apiToken->getDescription(),
+                \implode(",\n", $apiToken->getUserGroups()->map($formatterGroup)->toArray()),
+                \implode(",\n", $this->rolesService->getInheritedRoles($apiToken->getRoles())),
+            ];
+        };
+
+        return \array_map($formatterUser, $this->apiKeyResource->find(null, ['token' => 'ASC']));
+    }
+}
