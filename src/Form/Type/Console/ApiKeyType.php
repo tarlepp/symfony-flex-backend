0fea006c8c8c366ac9624e299e26e0d0e3724361
tarlepp <tarmo.leppanen@protacon.com>
2017-09-27 16:21:35 +0300

    feat: Added ApiKey type

---
+++ src/Form/Type/Console/ApiKeyType.php
@@ -0,0 +1,123 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Form/Type/Console/ApiKeyType.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Form\Type\Console;
+
+use App\DTO\ApiKey;
+use App\Entity\UserGroup;
+use App\Form\DataTransformer\UserGroupTransformer;
+use App\Resource\UserGroupResource;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class ApiKeyType
+ *
+ * @package App\Form\Type\Console
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ApiKeyType extends AbstractType
+{
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @var UserGroupResource
+     */
+    private $userGroupResource;
+
+    /**
+     * ApiKeyType constructor.
+     *
+     * @param ObjectManager     $objectManager
+     * @param UserGroupResource $userGroupResource
+     */
+    public function __construct(ObjectManager $objectManager, UserGroupResource $userGroupResource)
+    {
+        $this->objectManager = $objectManager;
+        $this->userGroupResource = $userGroupResource;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     *
+     * @throws \Symfony\Component\Form\Exception\InvalidArgumentException
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'description',
+                Type\TextType::class,
+                [
+                    'label'         => 'Description',
+                    'required'      => true,
+                    'empty_data'    => '',
+                ]
+            )
+            ->add(
+                'userGroups',
+                Type\ChoiceType::class,
+                [
+                    'choices'       => $this->getUserGroupChoices(),
+                    'multiple'      => true,
+                    'required'      => true,
+                    'empty_data'    => '',
+                ]
+            );
+
+        $builder->get('userGroups')->addModelTransformer(new UserGroupTransformer($this->objectManager));
+    }
+
+    /**
+     * Configures the options for this type.
+     *
+     * @param OptionsResolver $resolver The resolver for the options
+     *
+     * @throws \Symfony\Component\OptionsResolver\Exception\AccessException
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ApiKey::class
+        ]);
+    }
+
+    /**
+     * Method to create choices array for user groups.
+     *
+     * @return  array
+     */
+    private function getUserGroupChoices(): array
+    {
+        // Initialize output
+        $choices = [];
+
+        /**
+         * Lambda function to iterate all user groups and to create necessary choices array.
+         *
+         * @param UserGroup $userGroup
+         *
+         * @return void
+         */
+        $iterator = function (UserGroup $userGroup) use (&$choices) {
+            $name = $userGroup->getName() . ' [' . $userGroup->getRole()->getId() . ']';
+
+            $choices[$name] = $userGroup->getId();
+        };
+
+        \array_map($iterator, $this->userGroupResource->find());
+
+        return $choices;
+    }
+}
