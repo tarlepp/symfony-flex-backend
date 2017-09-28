2f1cec2fdcc40792baf74c3762124e1592de6fd2
tarlepp <tarmo.leppanen@protacon.com>
2017-09-28 20:25:49 +0300

    tests: Added missing test for ApiKey DTO class

---
+++ tests/Integration/DTO/ApiKeyTest.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Integration/DTO/ApiKeyTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\DTO;
+
+use App\DTO\ApiKey as ApiKeyDto;
+use App\Entity\ApiKey as ApiKeyEntity;
+use App\Entity\EntityInterface;
+use App\Entity\Role as RoleEntity;
+use App\Entity\UserGroup as UserGroupEntity;
+
+/**
+ * Class ApiKeyTest
+ *
+ * @package App\Tests\Integration\DTO
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ApiKeyTest extends DtoTestCase
+{
+    protected $dtoClass = ApiKeyDto::class;
+
+    public function testThatLoadMethodWorks(): void
+    {
+        // Create Role entity
+        $roleEntity = new RoleEntity('test role');
+
+        // Create UserGroup entity
+        $userGroupEntity = new UserGroupEntity();
+        $userGroupEntity->setName('test user group');
+        $userGroupEntity->setRole($roleEntity);
+
+        // Create ApiKey entity
+        $userEntity = new ApiKeyEntity();
+        $userEntity->setDescription('Some description');
+        $userEntity->addUserGroup($userGroupEntity);
+
+        /** @var ApiKeyDto $dto */
+        $dto = new $this->dtoClass();
+        $dto->load($userEntity);
+
+        static::assertSame('Some description', $dto->getDescription());
+        static::assertSame([$userGroupEntity->getId()], $dto->getUserGroups());
+    }
+
+    public function testThatUpdateMethodCallsExpectedEntityMethodsIfUserGroupsIsVisited(): void
+    {
+        $userGroups = [
+            'foo',
+            'bar',
+        ];
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject|EntityInterface $entity */
+        $entity = $this->getMockBuilder(EntityInterface::class)
+            ->setMethods(['getId', 'clearUserGroups', 'addUserGroup'])
+            ->getMock();
+
+        $entity
+            ->expects(static::once())
+            ->method('clearUserGroups');
+
+        $entity
+            ->expects(static::exactly(\count($userGroups)))
+            ->method('addUserGroup')
+            ->willReturn($entity);
+
+        /** @var ApiKeyDto $dto */
+        $dto = new $this->dtoClass();
+        $dto->setUserGroups($userGroups);
+        $dto->update($entity);
+    }
+}
