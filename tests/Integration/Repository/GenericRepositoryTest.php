2a17f367efc5f9991b8fbfedd4c6d149833b2f56
tarlepp <tarmo.leppanen@protacon.com>
2017-10-12 19:53:21 +0300

    tests: Added simple generic repository test

--- tests/Integration/Repository/GenericRepositoryTest.php
+++ tests/Integration/Repository/GenericRepositoryTest.php
@@ -9,13 +9,9 @@ namespace App\Tests\Integration\Repository;
 
 use App\Entity\EntityInterface;
 use App\Entity\User as UserEntity;
-use App\Repository\UserRepository;
+use App\Repository\BaseRepositoryInterface;
 use App\Resource\UserResource;
-use App\Rest\Repository;
 use Doctrine\Common\Proxy\Proxy;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\ClassMetadata;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
@@ -26,17 +22,8 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
  */
 class GenericRepositoryTest extends KernelTestCase
 {
-    protected $entityClass = UserEntity::class;
-    protected $resourceClass = UserResource::class;
-    protected $repositoryClass = UserRepository::class;
-
-    /**
-     * @return EntityManagerInterface|Object
-     */
-    private static function getEntityManager(): EntityManagerInterface
-    {
-        return static::$kernel->getContainer()->get('doctrine.orm.entity_manager');
-    }
+    private $entityClass = UserEntity::class;
+    private $resourceClass = UserResource::class;
 
     public function setUp(): void
     {
@@ -50,80 +37,9 @@ class GenericRepositoryTest extends KernelTestCase
         /** @var EntityInterface $entity */
         $entity = new $this->entityClass();
 
-        /** @var Repository $repository */
-        $repository = new $this->repositoryClass(static::getEntityManager(), new ClassMetadata($this->entityClass));
+        /** @var BaseRepositoryInterface $repository */
+        $repository = static::$kernel->getContainer()->get($this->resourceClass)->getRepository();
 
-        static::assertInstanceOf(
-            Proxy::class,
-            $repository->getReference($entity->getId())
-        );
-    }
-
-    public function testThatSaveMethodCallsExpectedServices(): void
-    {
-        /** @var \PHPUnit_Framework_MockObject_MockObject|EntityInterface $entityInterface */
-        $entityInterface = $this->createMock($this->entityClass);
-
-        // Create mock for entity manager
-        $entityManager = $this
-            ->getMockBuilder(EntityManager::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        // Check that 'persist' method is called
-        $entityManager
-            ->expects(static::once())
-            ->method('persist')
-            ->with($entityInterface);
-
-        $entityManager
-            ->expects(static::exactly(2))
-            ->method('isOpen')
-            ->willReturn(true);
-
-        // Check that 'flush' method is called
-        $entityManager
-            ->expects(static::once())
-            ->method('flush');
-
-        /** @var Repository $repository */
-        $repository = new $this->repositoryClass($entityManager, new ClassMetadata($this->entityClass));
-
-        // Call save method
-        $repository->save($entityInterface);
-    }
-
-    public function testThatRemoveMethodCallsExpectedServices(): void
-    {
-        /** @var \PHPUnit_Framework_MockObject_MockObject|EntityInterface $entityInterface */
-        $entityInterface = $this->createMock($this->entityClass);
-
-        // Create mock for entity manager
-        $entityManager = $this
-            ->getMockBuilder(EntityManager::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        // Check that 'persist' method is called
-        $entityManager
-            ->expects(static::once())
-            ->method('remove')
-            ->with($entityInterface);
-
-        $entityManager
-            ->expects(static::exactly(2))
-            ->method('isOpen')
-            ->willReturn(true);
-
-        // Check that 'flush' method is called
-        $entityManager
-            ->expects(static::once())
-            ->method('flush');
-
-        /** @var Repository $repository */
-        $repository = new $this->repositoryClass($entityManager, new ClassMetadata($this->entityClass));
-
-        // Call remove method
-        $repository->remove($entityInterface);
+        static::assertInstanceOf(Proxy::class, $repository->getReference($entity->getId()));
     }
 }
