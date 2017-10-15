8e56f026f1efb296c91b06afc59afbbab19ad42c
tarlepp <tarmo.leppanen@protacon.com>
2017-10-15 11:59:39 +0300

    feat: Added command to remove user group

---
+++ src/Command/User/RemoveUserGroupCommand.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Command/User/RemoveUserGroupCommand.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Command\User;
+
+use App\Entity\UserGroup;
+use App\Resource\UserGroupResource;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Class RemoveUserGroupCommand
+ *
+ * @package App\Command\User
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class RemoveUserGroupCommand extends Command
+{
+    /**
+     * @var UserGroupResource
+     */
+    private $userGroupResource;
+
+    /**
+     * @var UserHelper
+     */
+    private $userHelper;
+
+    /**
+     * RemoveUserGroupCommand constructor.
+     *
+     * @param UserGroupResource $userGroupResource
+     * @param UserHelper        $userHelper
+     *
+     * @throws \Symfony\Component\Console\Exception\LogicException
+     */
+    public function __construct(UserGroupResource $userGroupResource, UserHelper $userHelper)
+    {
+        parent::__construct('user:remove-group');
+
+        $this->userGroupResource = $userGroupResource;
+        $this->userHelper = $userHelper;
+
+        $this->setDescription('Console command to remove existing user group');
+    }
+
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * Executes the current command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->write("\033\143");
+
+        $groupFound = false;
+
+        while (!$groupFound) {
+            $userGroup = $this->userHelper->getUserGroup($io, 'Which user group you want to remove?');
+
+            $message = \sprintf(
+                'Is this the group [%s - %s (%s)] which you want to remove?',
+                $userGroup->getId(),
+                $userGroup->getName(),
+                $userGroup->getRole()->getId()
+            );
+
+            $groupFound = $io->confirm($message, false);
+        }
+
+        /** @var UserGroup $userGroup */
+
+        // Delete user group
+        $this->userGroupResource->delete($userGroup->getId());
+
+        if ($input->isInteractive()) {
+            $io->success([
+                'User group removed - have a nice day',
+            ]);
+        }
+
+        return null;
+    }
+}
