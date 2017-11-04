8e3866ec02bfcdcfb9620c0fba3e056d5a9dc8f8
tarlepp <tarmo.leppanen@protacon.com>
2017-11-04 17:11:32 +0200

    refactor: Simplified method structure

--- src/EventSubscriber/JWTDecodedSubscriber.php
+++ src/EventSubscriber/JWTDecodedSubscriber.php
@@ -9,6 +9,7 @@ namespace App\EventSubscriber;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
@@ -55,27 +56,38 @@ class JWTDecodedSubscriber
             return;
         }
 
-        // Get current payload and request object
-        $payload = $event->getPayload();
         $request = $this->requestStack->getCurrentRequest();
 
-        if ($request !== null) {
-            // Get bits for checksum calculation
-            $bits = [
-                $request->getClientIp(),
-                $request->headers->get('User-Agent'),
-            ];
-
-            // Calculate checksum
-            $checksum = \hash('sha512', \implode('|', $bits));
-
-            // Custom checks to validate user's JWT
-            if (!\array_key_exists('checksum', $payload) || $payload['checksum'] !== $checksum) {
-                $event->markAsInvalid();
-            }
-        } else {
+        if ($request === null) {
             $this->logger->error('Request not available');
 
+            $event->markAsInvalid();
+        } else {
+            $this->checkPayload($event, $request);
+        }
+    }
+
+    /**
+     * Method to check payload data.
+     *
+     * @param JWTDecodedEvent $event
+     * @param Request         $request
+     */
+    private function checkPayload(JWTDecodedEvent $event, Request $request): void
+    {
+        $payload = $event->getPayload();
+
+        // Get bits for checksum calculation
+        $bits = [
+            $request->getClientIp(),
+            $request->headers->get('User-Agent'),
+        ];
+
+        // Calculate checksum
+        $checksum = \hash('sha512', \implode('|', $bits));
+
+        // Custom checks to validate user's JWT
+        if (!\array_key_exists('checksum', $payload) || $payload['checksum'] !== $checksum) {
             $event->markAsInvalid();
         }
     }
