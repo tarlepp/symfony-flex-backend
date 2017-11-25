436932cc8139ed0d728998770a3316274e01f055
tarlepp <tarmo.leppanen@protacon.com>
2017-11-25 17:04:19 +0200

    refactor: Moved current user specified actions to separated controller.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -7,21 +7,11 @@ declare(strict_types = 1);
  */
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Security\ApiKeyUser;
-use App\Security\RolesService;
 use App\Utils\JSON;
-use Nelmio\ApiDocBundle\Annotation\Model;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Swagger\Annotations as SWG;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class AuthController
@@ -76,154 +66,5 @@ class AuthController
         );
 
         throw new HttpException(400, $message);
-    }
-
-    /**
-     * Endpoint action to get current user profile data.
-     *
-     * @Route("/profile");
-     *
-     * @Method("GET")
-     *
-     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
-     *
-     * @SWG\Parameter(
-     *      type="string",
-     *      name="Authorization",
-     *      in="header",
-     *      required=true,
-     *      description="Authorization header",
-     *      default="Bearer _your_jwt_here_",
-     *  )
-     * @SWG\Response(
-     *      response=200,
-     *      description="User profile data",
-     *      @SWG\Schema(
-     *          @Model(
-     *              type=User::class,
-     *              groups={"User", "User.userGroups", "User.roles", "UserGroup", "UserGroup.role"},
-     *          ),
-     *      ),
-     *  )
-     * @SWG\Response(
-     *      response=401,
-     *      description="Invalid token",
-     *      examples={
-     *          "Token not found": "{code: 401, message: 'JWT Token not found'}",
-     *          "Expired token": "{code: 401, message: 'Expired JWT Token'}",
-     *      },
-     *  )
-     * @SWG\Tag(name="Authentication")
-     *
-     * @param TokenStorageInterface $tokenStorage
-     * @param SerializerInterface   $serializer
-     * @param RolesService          $rolesService
-     *
-     * @return JsonResponse
-     *
-     * @throws \InvalidArgumentException
-     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
-     */
-    public function profileAction(
-        TokenStorageInterface $tokenStorage,
-        SerializerInterface $serializer,
-        RolesService $rolesService
-    ): JsonResponse {
-        /** @var User|ApiKeyUser $user */
-        /** @noinspection NullPointerExceptionInspection */
-        $user = $tokenStorage->getToken()->getUser();
-
-        // Get serializer groups for current user instance
-        $groups = $this->getSerializationGroupsForProfile($rolesService, $user);
-
-        // Create response
-        return new JsonResponse($serializer->serialize($user, 'json', ['groups' => $groups]), 200, [], true);
-    }
-
-    /**
-     * Endpoint action to get current user roles as an array.
-     *
-     * @Route("/roles");
-     *
-     * @Method("GET")
-     *
-     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
-     *
-     * @SWG\Parameter(
-     *      type="string",
-     *      name="Authorization",
-     *      in="header",
-     *      required=true,
-     *      description="Authorization header",
-     *      default="Bearer _your_jwt_here_",
-     *  )
-     * @SWG\Response(
-     *      response=200,
-     *      description="User roles",
-     *      @SWG\Schema(
-     *          type="array",
-     *          @SWG\Items(type="string"),
-     *      ),
-     *  )
-     * @SWG\Response(
-     *      response=401,
-     *      description="Invalid token",
-     *      examples={
-     *          "Token not found": "{code: 401, message: 'JWT Token not found'}",
-     *          "Expired token": "{code: 401, message: 'Expired JWT Token'}",
-     *      },
-     *  )
-     * @SWG\Tag(name="Authentication")
-     *
-     * @param TokenStorageInterface $tokenStorage
-     * @param RolesService          $rolesService
-     *
-     * @return JsonResponse
-     *
-     * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
-     */
-    public function rolesAction(TokenStorageInterface $tokenStorage, RolesService $rolesService): JsonResponse
-    {
-        /** @noinspection NullPointerExceptionInspection */
-        $user = $tokenStorage->getToken()->getUser();
-
-        return new JsonResponse($rolesService->getInheritedRoles($user->getRoles()));
-    }
-
-    /**
-     * @param RolesService  $rolesService
-     * @param UserInterface $user
-     *
-     * @return array
-     *
-     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
-     */
-    private function getSerializationGroupsForProfile(RolesService $rolesService, UserInterface $user): array
-    {
-        if ($user instanceof User) {
-            $groups = [
-                'User',
-                'User.userGroups',
-                'User.roles',
-                'UserGroup',
-                'UserGroup.role',
-            ];
-
-            // Set roles service to user entity, so we can get inherited roles
-            $user->setRolesService($rolesService);
-        } elseif ($user instanceof ApiKeyUser) {
-            $groups = [
-                'ApiKeyUser',
-                'ApiKeyUser.apiKey',
-                'ApiKey.description',
-                'ApiKey.userGroups',
-                'UserGroup',
-                'UserGroup.role',
-            ];
-        } else {
-            throw new AccessDeniedException('Not supported user');
-        }
-
-        return $groups;
     }
 }
