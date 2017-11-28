551696768fcaf2acf1ada3a32a73eb5131f2aa41
tarlepp <tarmo.leppanen@protacon.com>
2017-11-28 16:17:22 +0200

    feat: Added endpoint action to attach new user to specified user group.

--- src/Controller/UserGroupController.php
+++ src/Controller/UserGroupController.php
@@ -21,8 +21,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /** @noinspection PhpHierarchyChecksInspection */
 /** @noinspection PhpMissingParentCallCommonInspection */
@@ -143,5 +145,124 @@ class UserGroupController extends Controller
         $this->getResponseHandler()->setResource($userResource);
 
         return $this->getResponseHandler()->createResponse($request, $userResource->getUsersForGroup($userGroup));
+    }
+
+    /**
+     * Endpoint action to attach specified user to specified user group.
+     *
+     * @Route(
+     *      "/{userGroupId}/user/{userId}",
+     *      requirements={
+     *          "userGroupId" = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+     *          "userId" = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+     *      }
+     *  )
+     *
+     * @ParamConverter(
+     *      "userGroup",
+     *      class="App:UserGroup",
+     *      options={
+     *          "id" = "userGroupId",
+     *      },
+     *  )
+     * @ParamConverter(
+     *      "user",
+     *      class="App:User",
+     *      options={
+     *          "id" = "userId",
+     *      },
+     *  )
+     *
+     * @Method({"POST"})
+     *
+     * @Security("has_role('ROLE_ROOT')")
+     *
+     * @SWG\Parameter(
+     *      type="string",
+     *      name="Authorization",
+     *      in="header",
+     *      required=true,
+     *      description="Authorization header",
+     *      default="Bearer _your_jwt_here_",
+     *  )
+     * @SWG\Parameter(
+     *      type="string",
+     *      name="userGroupId",
+     *      in="path",
+     *      required=true,
+     *      description="User Group GUID",
+     *      default="User Group GUID",
+     *  )
+     * @SWG\Parameter(
+     *      type="string",
+     *      name="userId",
+     *      in="path",
+     *      required=true,
+     *      description="User GUID",
+     *      default="User GUID",
+     *  )
+     * @SWG\Response(
+     *      response=200,
+     *      description="Users",
+     *      @SWG\Schema(
+     *          type="array",
+     *          @Model(
+     *              type=App\Entity\User::class,
+     *              groups={"User"},
+     *          ),
+     *      ),
+     *  )
+     * @SWG\Response(
+     *      response=401,
+     *      description="Invalid token",
+     *      examples={
+     *          "Token not found": "{code: 401, message: 'JWT Token not found'}",
+     *          "Expired token": "{code: 401, message: 'Expired JWT Token'}",
+     *      },
+     *  )
+     * @SWG\Response(
+     *      response=403,
+     *      description="Access denied",
+     *  )
+     * @SWG\Tag(name="UserGroup Management")
+     *
+     * @param UserGroup           $userGroup
+     * @param User                $user
+     * @param SerializerInterface $serializer
+     *
+     * @return JsonResponse
+     */
+    public function attachUserAction(
+        UserGroup $userGroup,
+        User $user,
+        SerializerInterface $serializer
+    ): JsonResponse {
+        $this->getResource()->save($userGroup->addUser($user));
+
+        return $this->getUserResponse($userGroup, $serializer);
+    }
+
+    /**
+     * Helper method to create User response.
+     *
+     * @param UserGroup           $userGroup
+     * @param SerializerInterface $serializer
+     *
+     * @return JsonResponse
+     */
+    private function getUserResponse(UserGroup $userGroup, SerializerInterface $serializer): JsonResponse
+    {
+        static $groups = [
+            'groups' => [
+                'User',
+            ],
+        ];
+
+        return new JsonResponse(
+            $serializer->serialize($userGroup->getUsers()->getValues(), 'json', $groups),
+            200,
+            [],
+            true
+        );
     }
 }
