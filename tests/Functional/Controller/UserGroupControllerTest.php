3b038f263900efcf43a65a18b3f522cf6ce64061
tarlepp <tarmo.leppanen@protacon.com>
2017-11-28 17:51:09 +0200

    tests: Added missing test cases for attachUserAction method.

--- tests/Functional/Controller/UserGroupControllerTest.php
+++ tests/Functional/Controller/UserGroupControllerTest.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace App\Tests\Functional\Controller;
 
 use App\Resource\UserGroupResource;
+use App\Resource\UserResource;
 use App\Utils\JSON;
 use App\Utils\Tests\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
@@ -58,6 +59,90 @@ class UserGroupControllerTest extends WebTestCase
     }
 
     /**
+     * @dataProvider dataProviderTestThatAttachUserActionReturns403ForInvalidUser
+     *
+     * @param string $username
+     * @param string $password
+     */
+    public function testThatAttachUserActionReturns403ForInvalidUser(string $username, string $password): void
+    {
+        self::bootKernel();
+
+        /** @var UserGroupResource $userGroupResource */
+        $userGroupResource = static::$kernel->getContainer()->get(UserGroupResource::class);
+
+        /** @var UserResource $userResource */
+        $userResource = static::$kernel->getContainer()->get(UserResource::class);
+
+        $user = $userResource->findOneBy(['username' => $username]);
+        $userGroup = $userGroupResource->findOneBy(['name' => 'Root users']);
+
+        /** @noinspection NullPointerExceptionInspection */
+        $url = \sprintf(
+            '%s/%s/user/%s',
+            $this->baseUrl,
+            $userGroup->getId(),
+            $user->getId()
+        );
+
+        $client = $this->getClient($username, $password);
+        $client->request('POST', $url);
+
+        $response = $client->getResponse();
+
+        static::assertInstanceOf(Response::class, $response);
+
+        /** @noinspection NullPointerExceptionInspection */
+        static::assertSame(403, $response->getStatusCode());
+
+        /** @noinspection NullPointerExceptionInspection */
+        static::assertJsonStringEqualsJsonString(
+            '{"message":"Access denied.","code":0,"status":403}',
+            $response->getContent()
+        );
+    }
+
+    /**
+     * @dataProvider dataProviderTestThatAttachUserActionWorksAsExpected
+     *
+     * @param int $expectedStatus
+     */
+    public function testThatAttachUserActionWorksAsExpected(int $expectedStatus): void
+    {
+        self::bootKernel();
+
+        /** @var UserGroupResource $userGroupResource */
+        $userGroupResource = static::$kernel->getContainer()->get(UserGroupResource::class);
+
+        /** @var UserResource $userResource */
+        $userResource = static::$kernel->getContainer()->get(UserResource::class);
+
+        $user = $userResource->findOneBy(['username' => 'john']);
+        $userGroup = $userGroupResource->findOneBy(['name' => 'Root users']);
+
+        /** @noinspection NullPointerExceptionInspection */
+        $url = \sprintf(
+            '%s/%s/user/%s',
+            $this->baseUrl,
+            $userGroup->getId(),
+            $user->getId()
+        );
+
+        $client = $this->getClient('john-root', 'password-root');
+        $client->request('POST', $url);
+
+        $response = $client->getResponse();
+
+        static::assertInstanceOf(Response::class, $response);
+
+        /** @noinspection NullPointerExceptionInspection */
+        static::assertSame($expectedStatus, $response->getStatusCode());
+
+        /** @noinspection NullPointerExceptionInspection */
+        static::assertCount(2, JSON::decode($response->getContent()));
+    }
+
+    /**
      * @return array
      */
     public function dataProviderTestThatGetUserGroupUsersActionReturnsExpected(): array
@@ -74,6 +159,31 @@ class UserGroupControllerTest extends WebTestCase
             [3, $userGroupResource->findOneBy(['name' => 'Normal users'])->getId()],
             [1, $userGroupResource->findOneBy(['name' => 'Api users'])->getId()],
             [5, $userGroupResource->findOneBy(['name' => 'Logged in users'])->getId()],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestThatAttachUserActionReturns403ForInvalidUser(): array
+    {
+        return [
+            ['john',        'password'],
+            ['john-api',    'password-api'],
+            ['john-logged', 'password-logged'],
+            ['john-user',   'password-user'],
+            ['john-admin',  'password-admin'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderTestThatAttachUserActionWorksAsExpected(): array
+    {
+        return [
+            [201],
+            [200],
         ];
     }
 }
