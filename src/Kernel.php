509162d611f8130bba709134baf4a8f3d0e51d08
tarlepp <tarmo.leppanen@protacon.com>
2017-12-05 23:03:01 +0200

    refactor: Added custom compiler pass for test environment

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,32 +1,65 @@
 <?php
-
+declare(strict_types=1);
+/**
+ * /src/Kernel.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
-class Kernel extends BaseKernel
+/**
+ * Class Kernel
+ *
+ * @package App
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class Kernel extends BaseKernel implements CompilerPassInterface
 {
+    // Traits
     use MicroKernelTrait;
 
     private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * Gets the cache directory.
+     *
+     * @return string The cache directory
+     */
     public function getCacheDir(): string
     {
-        return dirname(__DIR__).'/var/cache/'.$this->environment;
+        return \dirname(__DIR__) . '/var/cache/' . $this->environment;
     }
 
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * Gets the log directory.
+     *
+     * @return string The log directory
+     */
     public function getLogDir(): string
     {
-        return dirname(__DIR__).'/var/log';
+        return \dirname(__DIR__) . '/var/log';
     }
 
+    /**
+     * Returns an array of bundles to register.
+     *
+     * @return iterable<BundleInterface> An array of bundle instances
+     */
     public function registerBundles(): iterable
     {
-        $contents = require dirname(__DIR__).'/config/bundles.php';
+        /** @noinspection UsingInclusionReturnValueInspection */
+        /** @var array $contents */
+        $contents = require \dirname(__DIR__) . '/config/bundles.php';
+
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
                 yield new $class();
@@ -34,26 +67,82 @@ class Kernel extends BaseKernel
         }
     }
 
+    /** @noinspection PhpUnusedParameterInspection */
+    /**
+     * Configures the container.
+     *
+     * You can register extensions:
+     *
+     * $c->loadFromExtension('framework', array(
+     *     'secret' => '%secret%'
+     * ));
+     *
+     * Or services:
+     *
+     * $c->register('halloween', 'FooBundle\HalloweenProvider');
+     *
+     * Or parameters:
+     *
+     * $c->setParameter('halloween', 'lot of fun');
+     *
+     * @param ContainerBuilder $container
+     * @param LoaderInterface  $loader
+     *
+     * @throws \Exception
+     */
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
-        $confDir = dirname(__DIR__).'/config';
-        $loader->load($confDir.'/packages/*'.self::CONFIG_EXTS, 'glob');
-        if (is_dir($confDir.'/packages/'.$this->environment)) {
-            $loader->load($confDir.'/packages/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
+        $confDir = \dirname(__DIR__) . '/config';
+
+        $loader->load($confDir . '/packages/*' . self::CONFIG_EXTS, 'glob');
+
+        if (is_dir($confDir . '/packages/' . $this->environment)) {
+            $loader->load($confDir . '/packages/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
         }
-        $loader->load($confDir.'/services'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/services_'.$this->environment.self::CONFIG_EXTS, 'glob');
+
+        $loader->load($confDir . '/services' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/services_' . $this->environment . self::CONFIG_EXTS, 'glob');
     }
 
+    /**
+     * Add or import routes into your application.
+     *
+     *     $routes->import('config/routing.yml');
+     *     $routes->add('/admin', 'AppBundle:Admin:dashboard', 'admin_dashboard');
+     *
+     * @param RouteCollectionBuilder $routes
+     *
+     * @throws \Symfony\Component\Config\Exception\FileLoaderLoadException
+     */
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
-        $confDir = dirname(__DIR__).'/config';
-        if (is_dir($confDir.'/routes/')) {
-            $routes->import($confDir.'/routes/*'.self::CONFIG_EXTS, '/', 'glob');
+        $confDir = \dirname(__DIR__) . '/config';
+
+        if (is_dir($confDir . '/routes/')) {
+            $routes->import($confDir . '/routes/*' . self::CONFIG_EXTS, '/', 'glob');
         }
-        if (is_dir($confDir.'/routes/'.$this->environment)) {
-            $routes->import($confDir.'/routes/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
+
+        if (is_dir($confDir . '/routes/' . $this->environment)) {
+            $routes->import($confDir . '/routes/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
         }
-        $routes->import($confDir.'/routes'.self::CONFIG_EXTS, '/', 'glob');
+
+        $routes->import($confDir . '/routes' . self::CONFIG_EXTS, '/', 'glob');
+    }
+
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
+     */
+    public function process(ContainerBuilder $container)
+    {
+        // Within test environment we need to expose certain services as public
+        if (\getenv('APP_ENV') === 'test') {
+            foreach ($container->findTaggedServiceIds('public_on_test') as $id => $tags) {
+                $container->getDefinition($id)->setPublic(true);
+            }
+        }
     }
 }
