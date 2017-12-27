aaeac020142009a83b1a1be37fb3cf167b6a10de
tarlepp <tarmo.leppanen@protacon.com>
2017-12-27 16:13:25 +0200

    feat: Added helper trait for Logger

---
+++ src/Helpers/LoggerAwareTrait.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/Helpers/LoggerAwareTrait.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Helpers;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Trait LoggerAwareTrait
+ *
+ * @package App\Helpers
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+trait LoggerAwareTrait
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @see https://symfony.com/doc/current/service_container/autowiring.html#autowiring-other-methods-e-g-setters
+     *
+     * @required
+     *
+     * @param LoggerInterface $logger
+     */
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+    }
+}
