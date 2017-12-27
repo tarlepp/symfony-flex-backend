3d2638e62707bf9f85009327546cde8c2625c837
tarlepp <tarmo.leppanen@protacon.com>
2017-12-27 17:01:59 +0200

    tests: Added tests for LoggerAwareTrait

---
+++ tests/Integration/Helpers/LoggerAwareTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Integration/Helpers/LoggerAwareTraitTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Helpers;
+
+use App\Tests\Integration\Helpers\src\LoggerAwareService;
+use App\Utils\Tests\PHPUnitUtil;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class LoggerAwareTraitTest
+ *
+ * @package App\Tests\Integration\Helpers
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class LoggerAwareTraitTest extends KernelTestCase
+{
+    public function testThatLoggerAttributeExists(): void
+    {
+        static::assertClassHasAttribute('logger', LoggerAwareService::class);
+    }
+
+    public function testThatLoggerIsInstanceOfLoggerInterface(): void
+    {
+        static::bootKernel();
+
+        $service = static::$kernel->getContainer()->get(LoggerAwareService::class);
+
+        $logger = PHPUnitUtil::getProperty('logger', $service);
+
+        static::assertInstanceOf(LoggerInterface::class, $logger);
+    }
+}
---
+++ tests/Integration/Helpers/src/LoggerAwareService.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * /tests/Integration/Helpers/src/LoggerAwareService.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Helpers\src;
+
+use App\Helpers\LoggerAwareTrait;
+
+/**
+ * Class LoggerAwareService
+ *
+ * @package App\Tests\Integration\Helpers\src
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class LoggerAwareService
+{
+    use LoggerAwareTrait;
+}
