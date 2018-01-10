077f3cb69b0b62d6321e95c2b0dec0275ee2032b
tarlepp <tarmo.leppanen@protacon.com>
2018-01-10 22:29:40 +0200

    tests: Added tests for Collection service

---
+++ tests/Integration/Resource/CollectionTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/Resource/CollectionTest.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Tests\Integration\Resource;
+
+use App\Resource\Collection;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class CollectionTest
+ *
+ * @package App\Tests\Integration\Resource
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class CollectionTest extends KernelTestCase
+{
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Resource 'FooBar' does not exists
+     */
+    public function testThatGetMethodThrowsAnException(): void
+    {
+        $collection = new Collection();
+        $collection->get('FooBar');
+    }
+}
