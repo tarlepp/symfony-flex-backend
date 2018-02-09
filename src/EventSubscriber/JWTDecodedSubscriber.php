e56972167491581184982cabfe3b45393874a207
tarlepp <tarmo.leppanen@protacon.com>
2018-02-09 19:56:47 +0200

    refactor: Real use of subscriber

--- src/EventSubscriber/JWTDecodedSubscriber.php
+++ src/EventSubscriber/JWTDecodedSubscriber.php
@@ -9,6 +9,8 @@ namespace App\EventSubscriber;
 
 use App\Helpers\LoggerAwareTrait;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -18,7 +20,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
  * @package App\EventSubscriber
  * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
-class JWTDecodedSubscriber
+class JWTDecodedSubscriber implements EventSubscriberInterface
 {
     // Traits
     use LoggerAwareTrait;
@@ -36,6 +38,31 @@ class JWTDecodedSubscriber
     public function __construct(RequestStack $requestStack)
     {
         $this->requestStack = $requestStack;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2')))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::JWT_DECODED => 'onJWTDecoded',
+        ];
     }
 
     /**
