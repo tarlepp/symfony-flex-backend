0c87828367045b7c8513f5f121a3d21c30388ad8
tarlepp <tarmo.leppanen@protacon.com>
2018-02-09 19:42:36 +0200

    refactor: Real use of subscriber

--- src/EventSubscriber/AuthenticationFailureSubscriber.php
+++ src/EventSubscriber/AuthenticationFailureSubscriber.php
@@ -12,6 +12,8 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Utils\LoginLogger;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Class AuthenticationFailureSubscriber
@@ -19,7 +21,7 @@ use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
  * @package App\EventSubscriber
  * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
-class AuthenticationFailureSubscriber
+class AuthenticationFailureSubscriber implements EventSubscriberInterface
 {
     /**
      * @var LoginLogger
@@ -44,9 +46,35 @@ class AuthenticationFailureSubscriber
     }
 
     /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2')))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::AUTHENTICATION_FAILURE => 'onAuthenticationFailure',
+        ];
+    }
+
+    /**
      * Method to log login failures to database.
      *
-     * This method is called when 'lexik_jwt_authentication.on_authentication_success' event is broadcast.
+     * This method is called when '\Lexik\Bundle\JWTAuthenticationBundle\Events::AUTHENTICATION_FAILURE'
+     * event is broadcast.
      *
      * @param AuthenticationFailureEvent $event
      *
