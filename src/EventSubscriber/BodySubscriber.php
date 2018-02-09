4e2a31c33a96ab8645d2da678ad6ecb7157b5037
tarlepp <tarmo.leppanen@protacon.com>
2018-02-09 19:51:45 +0200

    refactor: Real use of subscriber

--- src/EventSubscriber/BodySubscriber.php
+++ src/EventSubscriber/BodySubscriber.php
@@ -8,8 +8,10 @@ declare(strict_types = 1);
 namespace App\EventSubscriber;
 
 use App\Utils\JSON;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Class BodySubscriber
@@ -17,8 +19,36 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
  * @package App\EventSubscriber
  * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
-class BodySubscriber
+class BodySubscriber implements EventSubscriberInterface
 {
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2')))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [
+                'onKernelRequest',
+                10,
+            ],
+        ];
+    }
+
     /**
      * Implementation of BodySubscriber event. Purpose of this is to convert JSON request data to proper request
      * parameters.
