14c0ee5393fa7b9150e444eab73cb4bf64e4852b
tarlepp <tarmo.leppanen@protacon.com>
2018-03-10 21:20:33 +0200

    refactor: Changed '\<Function>' to 'use function <Function>' statements.

--- src/Form/Type/Traits/AddBasicFieldToForm.php
+++ src/Form/Type/Traits/AddBasicFieldToForm.php
@@ -8,6 +8,7 @@ declare(strict_types = 1);
 namespace App\Form\Type\Traits;
 
 use Symfony\Component\Form\FormBuilderInterface;
+use function call_user_func_array;
 
 /**
  * Trait AddBasicFieldToForm
@@ -24,7 +25,7 @@ trait AddBasicFieldToForm
     protected function addBasicFieldToForm(FormBuilderInterface $builder, array $fields): void
     {
         foreach ($fields as $params) {
-            \call_user_func_array([$builder, 'add'], $params);
+            call_user_func_array([$builder, 'add'], $params);
         }
     }
 }
