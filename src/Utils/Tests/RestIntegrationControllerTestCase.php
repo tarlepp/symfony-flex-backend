71d0709dfdfbace6a6999cae628ad76540dbdab9
tarlepp <tarmo.leppanen@protacon.com>
2018-03-17 16:18:20 +0200

    refactor: Changed '\<Function>' to 'use function <Function>' statements + class imports.

--- src/Utils/Tests/RestIntegrationControllerTestCase.php
+++ src/Utils/Tests/RestIntegrationControllerTestCase.php
@@ -8,6 +8,13 @@ declare(strict_types = 1);
 namespace App\Utils\Tests;
 
 use App\Rest\ControllerInterface;
+use ReflectionClass;
+use ReflectionException;
+use function gc_collect_cycles;
+use function gc_enable;
+use function get_class;
+use function mb_substr;
+use function sprintf;
 
 /**
  * Class RestIntegrationControllerTestCase
@@ -32,38 +39,20 @@ abstract class RestIntegrationControllerTestCase extends ContainerTestCase
      */
     protected $resourceClass;
 
-    protected function setUp(): void
-    {
-        gc_enable();
-
-        parent::setUp();
-
-        $this->controller = $this->getContainer()->get($this->controllerClass);
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        unset($this->controller);
-
-        gc_collect_cycles();
-    }
-
     /**
-     * @throws \ReflectionException
+     * @throws ReflectionException
      */
     public function testThatGivenControllerIsCorrect(): void
     {
-        $expected = \mb_substr((new \ReflectionClass($this))->getShortName(), 0, -4);
+        $expected = mb_substr((new ReflectionClass($this))->getShortName(), 0, -4);
 
-        $message = \sprintf(
+        $message = sprintf(
             'Your REST controller integration test \'%s\' uses likely wrong controller class \'%s\'',
-            \get_class($this),
+            get_class($this),
             $this->controllerClass
         );
 
-        static::assertSame($expected, (new \ReflectionClass($this->controller))->getShortName(), $message);
+        static::assertSame($expected, (new ReflectionClass($this->controller))->getShortName(), $message);
     }
 
     /**
@@ -74,5 +63,29 @@ abstract class RestIntegrationControllerTestCase extends ContainerTestCase
     {
         /** @noinspection UnnecessaryAssertionInspection */
         static::assertInstanceOf($this->resourceClass, $this->controller->getResource());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp(): void
+    {
+        gc_enable();
+
+        parent::setUp();
+
+        $this->controller = $this->getContainer()->get($this->controllerClass);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        unset($this->controller);
+
+        gc_collect_cycles();
     }
 }
