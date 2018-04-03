57d1bbe90f1daa5cadd65dba770dcd1ef37abef5
tarlepp <tarmo.leppanen@protacon.com>
2018-04-03 18:14:08 +0300

    refactor: Updated Kernel to same structure as "clean" install

--- src/Kernel.php
+++ src/Kernel.php
@@ -11,12 +11,11 @@ use App\DependencyInjection\Compiler\ResourceCollectionPass;
 use App\DependencyInjection\Compiler\TestCasePass;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
-use function dirname;
-use function is_dir;
 
 /**
  * Class Kernel
@@ -39,7 +38,7 @@ class Kernel extends BaseKernel implements CompilerPassInterface
      */
     public function getCacheDir(): string
     {
-        return dirname(__DIR__) . '/var/cache/' . $this->environment;
+        return $this->getProjectDir() . '/var/cache/' . $this->environment;
     }
 
     /** @noinspection PhpMissingParentCallCommonInspection */
@@ -50,7 +49,7 @@ class Kernel extends BaseKernel implements CompilerPassInterface
      */
     public function getLogDir(): string
     {
-        return dirname(__DIR__) . '/var/log';
+        return $this->getProjectDir() . '/var/log';
     }
 
     /**
@@ -61,11 +60,12 @@ class Kernel extends BaseKernel implements CompilerPassInterface
     public function registerBundles(): iterable
     {
         /** @noinspection UsingInclusionReturnValueInspection */
+        /** @noinspection PhpIncludeInspection */
         /** @var array $contents */
-        $contents = require dirname(__DIR__) . '/config/bundles.php';
+        $contents = require $this->getProjectDir() . '/config/bundles.php';
 
-        foreach ($contents as $class => $envs) {
-            if (isset($envs['all']) || isset($envs[$this->environment])) {
+        foreach ($contents as $class => $environments) {
+            if (isset($environments['all']) || isset($environments[$this->environment])) {
                 yield new $class();
             }
         }
@@ -120,16 +120,19 @@ class Kernel extends BaseKernel implements CompilerPassInterface
      */
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
-        $confDir = dirname(__DIR__) . '/config';
+        $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
 
-        $loader->load($confDir . '/packages/*' . self::CONFIG_EXTS, 'glob');
+        // Feel free to remove the "container.autowiring.strict_mode" parameter
+        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
+        $container->setParameter('container.autowiring.strict_mode', true);
+        $container->setParameter('container.dumper.inline_class_loader', true);
 
-        if (is_dir($confDir . '/packages/' . $this->environment)) {
-            $loader->load($confDir . '/packages/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
-        }
+        $confDir = $this->getProjectDir() . '/config';
 
-        $loader->load($confDir . '/services' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/services_' . $this->environment . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/' . $this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}_' . $this->environment.self::CONFIG_EXTS, 'glob');
     }
 
     /**
@@ -144,16 +147,10 @@ class Kernel extends BaseKernel implements CompilerPassInterface
      */
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
-        $confDir = dirname(__DIR__) . '/config';
+        $confDir = $this->getProjectDir() . '/config';
 
-        if (is_dir($confDir . '/routes/')) {
-            $routes->import($confDir . '/routes/*' . self::CONFIG_EXTS, '/', 'glob');
-        }
-
-        if (is_dir($confDir . '/routes/' . $this->environment)) {
-            $routes->import($confDir . '/routes/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
-        }
-
-        $routes->import($confDir . '/routes' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
     }
 }
