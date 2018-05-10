4f6fd9f0166eea8ffcf8eb37fd6bd4d690870435
tarlepp <tarmo.leppanen@protacon.com>
2018-05-10 15:47:47 +0300

    feat: Added new helper trait for console commands

---
+++ src/Command/Traits/SymfonyStyleTrait.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Command/Traits/SymfonyStyleTrait.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Command\Traits;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Trait SymfonyStyleTrait
+ *
+ * @package App\Command\Traits
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+trait SymfonyStyleTrait
+{
+    /**
+     * Method to get SymfonyStyle object for console commands.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @param bool|null       $clearScreen
+     *
+     * @return SymfonyStyle
+     */
+    protected function getSymfonyStyle(
+        InputInterface $input,
+        OutputInterface $output,
+        ?bool $clearScreen = null
+    ): SymfonyStyle {
+        $clearScreen = $clearScreen ?? true;
+
+        $io = new SymfonyStyle($input, $output);
+
+        if ($clearScreen) {
+            $io->write("\033\143");
+        }
+
+        return $io;
+    }
+}
