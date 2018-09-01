a5a62b7134644e800b962cd454cfa65276cdb27d
tarlepp <tarmo.leppanen@protacon.com>
2018-09-01 15:44:51 +0300

    tests: Fixed Psalm error

--- src/Utils/Tests/ContainerTestCase.php
+++ src/Utils/Tests/ContainerTestCase.php
@@ -19,7 +19,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 abstract class ContainerTestCase extends KernelTestCase
 {
     /**
-     * @var ContainerInterface
+     * @var ContainerInterface|null
      */
     private $testContainer;
 
