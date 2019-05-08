b016a4b2439b11d062162e08a95d8b13409f9856
tarlepp <tarmo.leppanen@protacon.com>
2019-05-08 17:47:08 +0300

    refactor: clean up for composer auto-scripts and docker entrypoint script

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -6,11 +6,9 @@ set -e
 #   1) Ensure that /app/var directory exists
 #   2) Install all dependencies
 #   3) Generate JWT encryption keys + allow apache to read this file
-#   4) Clear and warmup caches
-#   5) Create database if it not exists yet
-#   6) Run possible migrations, so that database is always up to date
-#   7) Install public assets
-#   8) Ensure that _all_ files have "correct" permissions
+#   4) Create database if it not exists yet
+#   5) Run possible migrations, so that database is always up to date
+#   6) Ensure that _all_ files have "correct" permissions
 #
 # Note that all the chmod stuff is for users who are using docker-compose within Linux environment. More info in link
 # below:
@@ -21,26 +19,19 @@ set -e
 mkdir -p /app/var
 
 # Step 2
-composer install --ansi
+composer install
 
 # Step 3
 make generate-jwt-keys
 chmod 644 /app/config/jwt/private.pem
 
 # Step 4
-php /app/bin/console cache:clear
-php /app/bin/console cache:warmup
-
-# Step 5
 php /app/bin/console doctrine:database:create --if-not-exists --no-interaction
 
-# Step 6
+# Step 5
 php /app/bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
-# Step 7
-php /app/bin/console assets:install --symlink --relative --no-interaction
-
-# Step 8
+# Step 6
 chmod -R o+s+w /app
 
 exec "$@"
