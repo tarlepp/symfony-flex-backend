d2fc5642cfa39ce05fcccc778c60a78600b6d48b
tarlepp <tarmo.leppanen@protacon.com>
2019-05-18 15:42:28 +0300

    fix: Removed @inheritDoc annotations

--- src/Security/SecurityUser.php
+++ src/Security/SecurityUser.php
@@ -68,7 +68,12 @@ class SecurityUser implements UserInterface
     }
 
     /**
-     * @inheritDoc
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string
      */
     public function getPassword(): string
     {
@@ -77,7 +82,9 @@ class SecurityUser implements UserInterface
     }
 
     /**
-     * @inheritDoc
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
      */
     public function getSalt(): void
     {
@@ -85,7 +92,9 @@ class SecurityUser implements UserInterface
     }
 
     /**
-     * @inheritDoc
+     * Returns the username used to authenticate the user.
+     *
+     * @return string
      */
     public function getUsername(): string
     {
@@ -93,7 +102,10 @@ class SecurityUser implements UserInterface
     }
 
     /**
-     * @inheritDoc
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
      */
     public function eraseCredentials(): void
     {
