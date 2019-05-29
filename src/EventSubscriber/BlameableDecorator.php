6f3bf8aecbd4d89d8185c217fff186acdfc2fa60
tarlepp <tarmo.leppanen@protacon.com>
2019-05-29 21:33:48 +0300

    feat: Added decorator for blameable listener/subscriber - this is needed to convert our `SecurityUser` to real `User` entity.

---
+++ src/EventSubscriber/BlameableDecorator.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/EventSubscriber/BlameableDecorator.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\EventSubscriber;
+
+use App\Resource\UserResource;
+use App\Security\SecurityUser;
+use Gedmo\Blameable\BlameableListener;
+use Throwable;
+
+/**
+ * Class BlameableDecorator
+ *
+ * @package App\EventSubscriber
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class BlameableDecorator extends BlameableListener
+{
+    /**
+     * @var UserResource
+     */
+    private $userResource;
+
+    /**
+     * @param UserResource $userResource
+     *
+     * @return BlameableDecorator
+     */
+    public function setUserResource(UserResource $userResource): self
+    {
+        $this->userResource = $userResource;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $user
+     *
+     * @throws Throwable
+     */
+    public function setUserValue($user): void
+    {
+        if ($user instanceof SecurityUser) {
+            $user = $this->userResource->findOne($user->getUuid());
+        }
+
+        parent::setUserValue($user);
+    }
+}
