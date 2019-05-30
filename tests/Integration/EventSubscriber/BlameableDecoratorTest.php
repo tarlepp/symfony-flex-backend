54dd380b05966c033a1bc148b16aa69b1c111c3d
tarlepp <tarmo.leppanen@protacon.com>
2019-05-30 14:15:52 +0300

    tests: Added missing test for `BlameableDecorator`

---
+++ tests/Integration/EventSubscriber/BlameableDecoratorTest.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/EventSubscriber/BlameableDecoratorTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Tests\Integration\EventSubscriber;
+
+use App\Entity\User;
+use App\EventSubscriber\BlameableDecorator;
+use App\Resource\UserResource;
+use App\Security\SecurityUser;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Throwable;
+
+/**
+ * Class BlameableDecoratorTest
+ *
+ * @package App\Tests\Integration\EventSubscriber
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class BlameableDecoratorTest extends KernelTestCase
+{
+    /**
+     * @throws Throwable
+     */
+    public function testThatSetUserValueCallsExpectedResourceMethod(): void
+    {
+        /**
+         * @var MockObject|UserResource $resource
+         */
+        $resource = $this->getMockBuilder(UserResource::class)->disableOriginalConstructor()->getMock();
+
+        $user = new User();
+        $domainUser = new SecurityUser($user);
+
+        $resource
+            ->expects(static::once())
+            ->method('findOne')
+            ->with($user->getId())
+            ->willReturn($user);
+
+        (new BlameableDecorator())
+            ->setUserResource($resource)
+            ->setUserValue($domainUser);
+    }
+}
