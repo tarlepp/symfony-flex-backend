13d840d4423cafcd2afa296fa503b106e9d96351
tarlepp <tarmo.leppanen@protacon.com>
2019-06-01 00:57:57 +0300

    feat: Implemented proper API key authenticator to replace old `simple_preauth`

---
+++ src/Security/Authenticator/ApiKeyAuthenticator.php
@@ -0,0 +1,250 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Security/Authenticator/ApiKeyAuthenticator.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Security\Authenticator;
+
+use App\Entity\ApiKey;
+use App\Security\ApiKeyUserInterface;
+use App\Security\Provider\ApiKeyUserProvider;
+use function is_array;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use UnexpectedValueException;
+use function count;
+use function preg_match;
+
+/**
+ * Class ApiKeyAuthenticator
+ *
+ * @package App\Security\Authenticator
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ApiKeyAuthenticator extends AbstractGuardAuthenticator
+{
+    /**
+     * @var ApiKeyUserProvider
+     */
+    private $apiKeyUserProvider;
+
+    /**
+     * ApiKeyAuthenticator constructor.
+     *
+     * @param ApiKeyUserProvider $apiKeyUserProvider
+     */
+    public function __construct(ApiKeyUserProvider $apiKeyUserProvider)
+    {
+        $this->apiKeyUserProvider = $apiKeyUserProvider;
+    }
+
+    /**
+     * Does the authenticator support the given Request?
+     *
+     * If this returns false, the authenticator will be skipped.
+     *
+     * @param Request $request
+     *
+     * @return bool
+     */
+    public function supports(Request $request): bool
+    {
+        $apiKey = $request->headers->get('Authorization', '');
+        $apiKey = is_array($apiKey) ? $apiKey[0] : (string)$apiKey;
+
+        preg_match('#^ApiKey (\w+)$#', $apiKey, $matches);
+
+        return count($matches) > 0;
+    }
+
+    /**
+     * Returns a response that directs the user to authenticate.
+     *
+     * This is called when an anonymous request accesses a resource that
+     * requires authentication. The job of this method is to return some
+     * response that "helps" the user start into the authentication process.
+     *
+     * Examples:
+     *
+     * - For a form login, you might redirect to the login page
+     *
+     *     return new RedirectResponse('/login');
+     *
+     * - For an API token authentication system, you return a 401 response
+     *
+     *     return new Response('Auth header required', 401);
+     *
+     * @param Request                 $request       The request that resulted in an AuthenticationException
+     * @param AuthenticationException $authException The exception that started the authentication process
+     *
+     * @return JsonResponse
+     */
+    public function start(Request $request, AuthenticationException $authException = null): JsonResponse
+    {
+        $data = [
+            'message' => 'Authentication Required',
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Get the authentication credentials from the request and return them
+     * as any type (e.g. an associate array).
+     *
+     * Whatever value you return here will be passed to getUser() and checkCredentials()
+     *
+     * For example, for a form login, you might:
+     *
+     *      return [
+     *          'username' => $request->request->get('_username'),
+     *          'password' => $request->request->get('_password'),
+     *      ];
+     *
+     * Or for an API token that's on a header, you might use:
+     *
+     *      return ['api_key' => $request->headers->get('X-API-TOKEN')];
+     *
+     * @param Request $request
+     *
+     * @return mixed Any non-null value
+     *
+     * @throws UnexpectedValueException If null is returned
+     */
+    public function getCredentials(Request $request)
+    {
+        $output = null;
+
+        $apiKey = $request->headers->get('Authorization', '');
+        $apiKey = is_array($apiKey) ? $apiKey[0] : (string)$apiKey;
+
+        preg_match('#^ApiKey (\w+)$#', $apiKey, $matches);
+
+        if (count($matches) > 0) {
+            $output = [
+                'token' => $matches[1],
+            ];
+        }
+
+        return $output;
+    }
+
+    /**
+     * Return a UserInterface object based on the credentials.
+     *
+     * The *credentials* are the return value from getCredentials()
+     *
+     * You may throw an AuthenticationException if you wish. If you return
+     * null, then a UsernameNotFoundException is thrown for you.
+     *
+     * @param mixed                 $credentials
+     * @param UserProviderInterface $userProvider
+     *
+     * @return ApiKeyUserInterface|null
+     *
+     * @throws UsernameNotFoundException
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider): ?ApiKeyUserInterface
+    {
+        $apiToken = $credentials['token'];
+
+        if ($apiToken === null) {
+            return null;
+        }
+
+        return $this->apiKeyUserProvider->loadUserByUsername($apiToken);
+    }
+
+    /**
+     * Returns true if the credentials are valid.
+     *
+     * If any value other than true is returned, authentication will
+     * fail. You may also throw an AuthenticationException if you wish
+     * to cause authentication to fail.
+     *
+     * The *credentials* are the return value from getCredentials()
+     *
+     * @param mixed         $credentials
+     * @param UserInterface $user
+     *
+     * @return bool
+     *
+     * @throws AuthenticationException
+     */
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        return $this->apiKeyUserProvider->getApiKeyForToken($credentials['token']) instanceof ApiKey;
+    }
+
+    /**
+     * Called when authentication executed and was successful!
+     *
+     * This should return the Response sent back to the user, like a
+     * RedirectResponse to the last page they visited.
+     *
+     * If you return null, the current request will continue, and the user
+     * will be authenticated. This makes sense, for example, with an API.
+     *
+     * @param Request        $request
+     * @param TokenInterface $token
+     * @param string         $providerKey The provider (i.e. firewall) key
+     *
+     * @return Response|null
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): ?Response
+    {
+        return null;
+    }
+
+    /**
+     * Called when authentication executed, but failed (e.g. wrong username password).
+     *
+     * This should return the Response sent back to the user, like a
+     * RedirectResponse to the login page or a 403 response.
+     *
+     * If you return null, the request will continue, but the user will
+     * not be authenticated. This is probably not what you want to do.
+     *
+     * @param Request                 $request
+     * @param AuthenticationException $exception
+     *
+     * @return JsonResponse
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): JsonResponse
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Does this method support remember me cookies?
+     *
+     * Remember me cookie will be set if *all* of the following are met:
+     *  A) This method returns true
+     *  B) The remember_me key under your firewall is configured
+     *  C) The "remember me" functionality is activated. This is usually
+     *      done by having a _remember_me checkbox in your form, but
+     *      can be configured by the "always_remember_me" and "remember_me_parameter"
+     *      parameters under the "remember_me" firewall key
+     *  D) The onAuthenticationSuccess method returns a Response object
+     *
+     * @return bool
+     */
+    public function supportsRememberMe(): bool
+    {
+        return false;
+    }
+}
