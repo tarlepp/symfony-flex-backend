320bbb6312770700e66c1b9cd709631e49fe6724
tarlepp <tarmo.leppanen@protacon.com>
2019-06-01 13:38:26 +0300

    feat: Updated `check.php` and added support that users can access to this file from browser within the same computer where `docker-compose up` was run.

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -4,7 +4,8 @@ set -e
 #
 # If we're starting web-server we need to do following:
 #   1) Modify docker-php-ext-xdebug.ini file to contain correct remote host value, note that for mac we need to use
-#      another value within this.
+#      another value within this. Also we want to export host IP so that we can use that within `check.php` to check
+#      that current environment is compatible with Symfony.
 #   2) Ensure that /app/var directory exists and clear possible existing one before that
 #   3) Install all dependencies
 #   4) Generate JWT encryption keys + allow apache to read this file
@@ -27,6 +28,8 @@ else
 fi
 
 sed -i "s/xdebug\.remote_host \=.*/xdebug\.remote_host\=$HOST/g" /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+
+export DOCKER_IP=`/sbin/ip route|awk '/default/ { print $3 }'`
 
 # Step 2
 rm -rf /app/var
