6d1257e30eba9e3c2c62254d833c24a5dfba65de
tarlepp <tarmo.leppanen@protacon.com>
2019-06-15 19:55:20 +0300

    refactor: User  DTO classes to new structure.

---
+++ src/DTO/User/UserCreate.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Rest/DTO/User/UserCreate.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\DTO\User;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class UserCreate
+ *
+ * @package App\DTO\User
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UserCreate extends User
+{
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     * @Assert\Length(min = 8, max = 255)
+     */
+    protected $password = '';
+}
