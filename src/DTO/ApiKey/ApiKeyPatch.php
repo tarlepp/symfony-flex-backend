068f74c025d4237cc027044beb4bc087abb2efe5
tarlepp <tarmo.leppanen@protacon.com>
2019-06-15 19:28:32 +0300

    refactor: Moved ApiKey DTO to own namespace and added classes for each supported type (create, patch and update).

---
+++ src/DTO/ApiKey/ApiKeyPatch.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/DTO/ApiKey/ApiKeyPatch.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\DTO\ApiKey;
+
+/**
+ * Class ApiKeyPatch
+ *
+ * @package App\DTO\ApiKey
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class ApiKeyPatch extends ApiKey
+{
+}
