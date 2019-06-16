19964ba3d5e7fdc253495e69252a0790c53792c9
tarlepp <tarmo.leppanen@protacon.com>
2019-06-16 14:32:50 +0300

    refactor: Proper tests for `UserGroup` DTO classes.

---
+++ tests/Integration/DTO/UserGroup/UserGroupUpdateTest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/DTO/UserGroup/UserGroupUpdateTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Tests\Integration\DTO\UserGroup;
+
+use App\DTO\UserGroup\UserGroupUpdate;
+use App\Tests\Integration\DTO\DtoTestCase;
+
+/**
+ * Class UserGroupUpdateTest
+ *
+ * @package App\Tests\Integration\DTO\UserGroup
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UserGroupUpdateTest extends DtoTestCase
+{
+    protected $dtoClass = UserGroupUpdate::class;
+}
