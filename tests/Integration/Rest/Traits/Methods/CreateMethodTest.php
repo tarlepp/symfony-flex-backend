34a2b216aa0eb8c0ce24ec75c8167259fd7ee29e
tarlepp <tarmo.leppanen@protacon.com>
2019-06-16 15:22:52 +0300

    refactor: REST method tests to use proper structure.

--- tests/Integration/Rest/Traits/Methods/CreateMethodTest.php
+++ tests/Integration/Rest/Traits/Methods/CreateMethodTest.php
@@ -50,14 +50,14 @@ class CreateMethodTest extends KernelTestCase
 
         /**
          * @var MockObject|CreateMethodInvalidTestClass $testClass
-         * @var MockObject|FormFactoryInterface         $formFactoryMock
+         * @var MockObject|RestDtoInterface             $restDtoInterface
          */
         $testClass = $this->getMockForAbstractClass(CreateMethodInvalidTestClass::class);
-        $formFactoryMock = $this->getMockBuilder(FormFactoryInterface::class)->getMock();
+        $restDtoInterface = $this->getMockBuilder(RestDtoInterface::class)->getMock();
 
         $request = Request::create('/');
 
-        $testClass->createMethod($request, $formFactoryMock);
+        $testClass->createMethod($request, $restDtoInterface);
     }
 
     /**
@@ -74,8 +74,8 @@ class CreateMethodTest extends KernelTestCase
         $resource = $this->createMock(RestResourceInterface::class);
         $responseHandler = $this->createMock(ResponseHandlerInterface::class);
 
-        /** @var MockObject|FormFactoryInterface $formFactoryMock */
-        $formFactoryMock = $this->getMockBuilder(FormFactoryInterface::class)->getMock();
+        /** @var MockObject|RestDtoInterface $restDtoInterface */
+        $restDtoInterface = $this->getMockBuilder(RestDtoInterface::class)->getMock();
 
         /** @var MockObject|CreateMethodTestClass $testClass */
         $testClass = $this->getMockForAbstractClass(
@@ -86,42 +86,7 @@ class CreateMethodTest extends KernelTestCase
         // Create request and response
         $request = Request::create('/', $httpMethod);
 
-        $testClass->createMethod($request, $formFactoryMock)->getContent();
-    }
-
-    /**
-     * @dataProvider dataProviderTestThatTraitHandlesException
-     *
-     * @param Exception $exception
-     * @param int        $expectedCode
-     *
-     * @throws Throwable
-     */
-    public function testThatTraitHandlesException(Exception $exception, int $expectedCode): void
-    {
-        $resource = $this->createMock(RestResourceInterface::class);
-        $responseHandler = $this->createMock(ResponseHandlerInterface::class);
-
-        /** @var MockObject|FormFactoryInterface $formFactoryMock */
-        $formFactoryMock = $this->getMockBuilder(FormFactoryInterface::class)->getMock();
-
-        /** @var MockObject|CreateMethodTestClass $testClass */
-        $testClass = $this->getMockForAbstractClass(
-            CreateMethodTestClass::class,
-            [$resource, $responseHandler]
-        );
-
-        $request = Request::create('/', 'POST');
-
-        $formFactoryMock
-            ->expects(static::once())
-            ->method('createNamed')
-            ->willThrowException($exception);
-
-        $this->expectException(HttpException::class);
-        $this->expectExceptionCode($expectedCode);
-
-        $testClass->createMethod($request, $formFactoryMock);
+        $testClass->createMethod($request, $restDtoInterface)->getContent();
     }
 
     /**
@@ -135,15 +100,10 @@ class CreateMethodTest extends KernelTestCase
         $responseHandler = $this->createMock(ResponseHandlerInterface::class);
 
         /**
-         * @var MockObject|FormFactoryInterface $formFactoryMock
-         * @var MockObject|FormInterface        $formInterfaceMock
-         * @var MockObject|RestDtoInterface     $dtoInterface
-         * @var MockObject|Request              $request
+         * @var MockObject|RestDtoInterface $restDtoInterface
+         * @var MockObject|Request          $request
          */
-        $formFactoryMock = $this->getMockBuilder(FormFactoryInterface::class)->getMock();
-        $formInterfaceMock = $this->getMockBuilder(FormInterface::class)->getMock();
-        $formConfigInterfaceMock = $this->getMockBuilder(FormConfigInterface::class)->getMock();
-        $dtoInterface = $this->createMock(RestDtoInterface::class);
+        $restDtoInterface = $this->getMockBuilder(RestDtoInterface::class)->getMock();
         $request = $this->createMock(Request::class);
 
         /** @var MockObject|CreateMethodTestClass $testClass */
@@ -153,117 +113,11 @@ class CreateMethodTest extends KernelTestCase
         );
 
         $request
-            ->expects(static::exactly(2))
+            ->expects(static::once())
             ->method('getMethod')
             ->willReturn('POST');
 
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('handleRequest')
-            ->with($request);
-
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('isSubmitted')
-            ->willReturn(true);
-
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('isValid')
-            ->willReturn(true);
-
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('getData')
-            ->willReturn($dtoInterface);
-
-        $formConfigInterfaceMock
-            ->expects(static::once())
-            ->method('getDataClass')
-            ->willReturn(null);
-
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('getConfig')
-            ->willReturn($formConfigInterfaceMock);
-
-        $formFactoryMock
-            ->expects(static::once())
-            ->method('createNamed')
-            ->withAnyParameters()
-            ->willReturn($formInterfaceMock);
-
-        $testClass->createMethod($request, $formFactoryMock);
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testThatTraitThrowsAnErrorIfFormIsInvalid(): void
-    {
-        $this->expectException(HttpException::class);
-
-        $resource = $this->createMock(RestResourceInterface::class);
-        $responseHandler = $this->createMock(ResponseHandlerInterface::class);
-
-        /** @var MockObject|CreateMethodTestClass $testClass */
-        $testClass = $this->getMockForAbstractClass(
-            CreateMethodTestClass::class,
-            [$resource, $responseHandler]
-        );
-
-        /**
-         * @var MockObject|FormFactoryInterface $formFactoryMock
-         * @var MockObject|FormInterface        $formInterfaceMock
-         * @var MockObject|Request              $request
-         */
-        $formFactoryMock = $this->getMockBuilder(FormFactoryInterface::class)->getMock();
-        $formInterfaceMock = $this->getMockBuilder(FormInterface::class)->getMock();
-        $formConfigInterfaceMock = $this->getMockBuilder(FormConfigInterface::class)->getMock();
-        $request = $this->createMock(Request::class);
-
-        $request
-            ->expects(static::exactly(2))
-            ->method('getMethod')
-            ->willReturn('POST');
-
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('handleRequest')
-            ->with($request);
-
-        $formConfigInterfaceMock
-            ->expects(static::once())
-            ->method('getDataClass')
-            ->willReturn(null);
-
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('isSubmitted')
-            ->willReturn(true);
-
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('isValid')
-            ->willReturn(false);
-
-        $formInterfaceMock
-            ->expects(static::once())
-            ->method('getConfig')
-            ->willReturn($formConfigInterfaceMock);
-
-        $formFactoryMock
-            ->expects(static::once())
-            ->method('createNamed')
-            ->withAnyParameters()
-            ->willReturn($formInterfaceMock);
-
-        $responseHandler
-            ->expects(static::once())
-            ->method('handleFormError')
-            ->willThrowException(new HttpException(400));
-
-        $testClass->createMethod($request, $formFactoryMock);
+        $testClass->createMethod($request, $restDtoInterface);
     }
 
     /**
