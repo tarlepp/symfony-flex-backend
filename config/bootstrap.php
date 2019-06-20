c1ebde2e01b7ffeb0d7cbd588428af0ffb00b443
tarlepp <tarmo.leppanen@protacon.com>
2019-06-20 19:16:20 +0300

    refactor: Updated `bootstrap.php` to follow latest fresh Symfony install.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,21 +5,26 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-// Ensure that current working directory is project root - this is needed to make relative paths to working properly
-chdir(dirname(__DIR__));
-
-if (!array_key_exists('APP_ENV', $_SERVER)) {
-    $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] ?? null;
-}
-
-if ($_SERVER['APP_ENV'] !== 'prod') {
-    if (!class_exists(Dotenv::class)) {
-        throw new RuntimeException('The "APP_ENV" environment variable is not set to "prod". Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
+// Load cached env vars if the .env.local.php file exists
+// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
+/** @noinspection UsingInclusionReturnValueInspection */
+/** @noinspection PhpUsageOfSilenceOperatorInspection */
+/** @noinspection PhpIncludeInspection */
+if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
+    foreach ($env as $k => $v) {
+        $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && strncmp($k, 'HTTP_', 5) !== 0 ? $_SERVER[$k] : $v);
     }
-
-    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+} elseif (!class_exists(Dotenv::class)) {
+    throw new RuntimeException(
+        'Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.'
+    );
+} else {
+    // load all the .env files
+    (new Dotenv(false))->loadEnv(dirname(__DIR__) . '/.env');
 }
 
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = $_SERVER['APP_ENV'] ?: $_ENV['APP_ENV'] ?: 'dev';
+/** @noinspection AdditionOperationOnArraysInspection */
+$_SERVER += $_ENV;
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? $_SERVER['APP_ENV'] !== 'prod';
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var($_SERVER['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN) ? '1' : '0';
