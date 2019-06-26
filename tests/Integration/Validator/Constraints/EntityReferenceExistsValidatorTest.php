a7929e16092a7496df428747df6e3fefb07db36a
tarlepp <tarmo.leppanen@protacon.com>
2019-06-26 19:53:25 +0300

    tests: Added more tests.

--- tests/Integration/Validator/Constraints/EntityReferenceExistsValidatorTest.php
+++ tests/Integration/Validator/Constraints/EntityReferenceExistsValidatorTest.php
@@ -8,10 +8,15 @@ declare(strict_types = 1);
 
 namespace App\Tests\Integration\Validator\Constraints;
 
+use App\Validator\Constraints\EntityReferenceExists;
 use App\Validator\Constraints\EntityReferenceExistsValidator;
+use Doctrine\ORM\Proxy\Proxy;
+use Generator;
+use stdClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
 
 /**
  * Class EntityReferenceExistsValidatorTest
@@ -31,5 +36,45 @@ class EntityReferenceExistsValidatorTest extends KernelTestCase
         $constraint = $this->getMockForAbstractClass(Constraint::class, [], 'SomeConstraint');
 
         (new EntityReferenceExistsValidator())->validate('', $constraint);
+    }
+
+    /**
+     * @dataProvider dataProviderTestThatValidateMethodThrowsUnexpectedValueException
+     *
+     * @param mixed  $value
+     * @param string $expectedMessage
+     */
+    public function testThatValidateMethodThrowsUnexpectedValueException(
+        $value,
+        string $expectedMessage
+    ): void {
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage($expectedMessage);
+
+        (new EntityReferenceExistsValidator())->validate($value, new EntityReferenceExists());
+    }
+
+    /**
+     * @return Generator
+     */
+    public function dataProviderTestThatValidateMethodThrowsUnexpectedValueException(): Generator
+    {
+        yield ['', 'Expected argument of type "Doctrine\ORM\Proxy\Proxy", "string" given'];
+
+        yield [new stdClass(), 'Expected argument of type "Doctrine\ORM\Proxy\Proxy", "stdClass" given'];
+
+        yield [[''], 'Expected argument of type "Doctrine\ORM\Proxy\Proxy", "string" given'];
+
+        yield [[new stdClass()], 'Expected argument of type "Doctrine\ORM\Proxy\Proxy", "stdClass" given'];
+
+        yield [
+            $this->getMockForAbstractClass(Proxy::class, [], 'ProxyClass'),
+            'Expected argument of type "App\Entity\EntityInterface", "ProxyClass" given',
+        ];
+
+        yield [
+            [$this->getMockForAbstractClass(Proxy::class, [], 'ProxyClass')],
+            'Expected argument of type "App\Entity\EntityInterface", "ProxyClass" given',
+        ];
     }
 }
