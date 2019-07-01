8b372d0e1b1b52b2c43aeda262dde708c734f77e
tarlepp <tarmo.leppanen@protacon.com>
2019-07-01 21:02:54 +0300

    tests: Implemented basic tests for `getCreatedAt` method.

---
+++ tests/Unit/Entity/DateDimensionTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Unit/Entity/DateDimensionTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\DateDimension;
+use DateTime;
+use DateTimeZone;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Throwable;
+
+/**
+ * Class DateDimensionTest
+ *
+ * @package App\Tests\Unit\Entity
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class DateDimensionTest extends KernelTestCase
+{
+    /**
+     * @throws Throwable
+     */
+    public function testThatGetCreatedAtReturnsExpected(): void
+    {
+        $dateTime = new DateTime('now', new DateTimeZone('UTC'));
+        $dateTime->setTime(10, 10, 10);
+
+        $entity = new DateDimension($dateTime);
+
+        static::assertSame($dateTime->format('u'), $entity->getCreatedAt()->format('u'));
+    }
+}
