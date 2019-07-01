5345f08eee9f93db565a4f84cfc00a49708ea2a9
tarlepp <tarmo.leppanen@protacon.com>
2019-07-01 19:08:51 +0300

    tests: Added test case for not so happy path.

--- tests/Integration/Validator/Constraints/EntityReferenceExistsValidatorTest.php
+++ tests/Integration/Validator/Constraints/EntityReferenceExistsValidatorTest.php
@@ -12,6 +12,7 @@ use App\Entity\EntityInterface;
 use App\Tests\Integration\Validator\Constraints\src\EntityReference;
 use App\Validator\Constraints\EntityReferenceExists;
 use App\Validator\Constraints\EntityReferenceExistsValidator;
+use Doctrine\ORM\EntityNotFoundException;
 use Doctrine\ORM\Proxy\Proxy;
 use Generator;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -22,6 +23,7 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Context\ExecutionContext;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 use Symfony\Component\Validator\Exception\UnexpectedValueException;
+use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
 
 /**
  * Class EntityReferenceExistsValidatorTest
@@ -82,6 +84,59 @@ class EntityReferenceExistsValidatorTest extends KernelTestCase
             ->expects(static::once())
             ->method('getCreatedAt')
             ->willReturn(null);
+
+        // Run validator
+        $validator = new EntityReferenceExistsValidator();
+        $validator->initialize($context);
+        $validator->setLogger($logger);
+        $validator->validate($value, new EntityReferenceExists());
+    }
+
+    public function testThatContextAndLoggerMethodsAreCalledIfEntityReferenceIsNotValidEntity(): void
+    {
+        /**
+         * @var MockObject|ConstraintViolationBuilderInterface $violation
+         * @var MockObject|ExecutionContext                    $context
+         * @var MockObject|LoggerInterface                     $logger
+         * @var MockObject|EntityInterface                     $value
+         */
+        $violation = $this->getMockBuilder(ConstraintViolationBuilderInterface::class)->getMock();
+        $context = $this->getMockBuilder(ExecutionContext::class)->disableOriginalConstructor()->getMock();
+        $logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
+        $value = $this->getMockForAbstractClass(EntityReference::class);
+
+        $exception = new EntityNotFoundException('Entity not found');
+
+        $violation
+            ->expects(static::exactly(2))
+            ->method('setParameter')
+            ->willReturn($violation);
+
+        $violation
+            ->expects(static::once())
+            ->method('setCode')
+            ->with('64888b5e-bded-449b-82ed-0cc1f73df14d')
+            ->willReturn($violation);
+
+        $violation
+            ->expects(static::once())
+            ->method('addViolation');
+
+        $context
+            ->expects(static::once())
+            ->method('buildViolation')
+            ->with('Invalid id value "{{ id }}" given for entity "{{ entity }}".')
+            ->willReturn($violation);
+
+        $logger
+            ->expects(static::once())
+            ->method('error')
+            ->with('Entity not found');
+
+        $value
+            ->expects(static::once())
+            ->method('getCreatedAt')
+            ->willThrowException($exception);
 
         // Run validator
         $validator = new EntityReferenceExistsValidator();
