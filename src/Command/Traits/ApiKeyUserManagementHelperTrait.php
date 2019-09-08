ee97d10393e32190897d9f7e1f29584b4eb1dca1
tarlepp <tarmo.leppanen@protacon.com>
2019-09-08 12:58:55 +0300

    refactor: Lets use wrapper for `getApplication` method.

--- src/Command/Traits/ApiKeyUserManagementHelperTrait.php
+++ src/Command/Traits/ApiKeyUserManagementHelperTrait.php
@@ -9,20 +9,21 @@ declare(strict_types = 1);
 namespace App\Command\Traits;
 
 use App\Security\RolesService;
-use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
 /**
  * Trait ApiKeyUserManagementHelperTrait
  *
  * @package App\Command\Traits
  * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
- *
- * @method Application getApplication()
  */
 trait ApiKeyUserManagementHelperTrait
 {
+    // Traits
+    use GetApplicationTrait;
+
     /**
      * @return RolesService
      */
@@ -33,8 +34,7 @@ trait ApiKeyUserManagementHelperTrait
      *
      * @param OutputInterface $output
      *
-     * @throws \Exception
-     * @throws \Symfony\Component\Console\Exception\CommandNotFoundException
+     * @throws Throwable
      */
     protected function createUserGroups(OutputInterface $output): void
     {
