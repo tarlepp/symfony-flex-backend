62888919919443d996176c5ab3a5d308af67d37f
tarlepp <tarmo.leppanen@protacon.com>
2019-09-30 17:33:17 +0300

    feat: Refactored validator to use proper string id value.

--- src/Validator/Constraints/UniqueEmailValidator.php
+++ src/Validator/Constraints/UniqueEmailValidator.php
@@ -48,7 +48,7 @@ class UniqueEmailValidator extends ConstraintValidator
     public function validate($value, Constraint $constraint): void
     {
         if ($value instanceof UserInterface
-            && !$this->repository->isEmailAvailable($value->getEmail(), $value->getId())
+            && !$this->repository->isEmailAvailable($value->getEmail(), $value->getId()->toString())
         ) {
             $this->context
                 ->buildViolation(UniqueEmail::MESSAGE)
--- src/Validator/Constraints/UniqueUsernameValidator.php
+++ src/Validator/Constraints/UniqueUsernameValidator.php
@@ -50,7 +50,7 @@ class UniqueUsernameValidator extends ConstraintValidator
     public function validate($value, Constraint $constraint): void
     {
         if ($value instanceof UserInterface
-            && !$this->repository->isUsernameAvailable($value->getUsername(), $value->getId())
+            && !$this->repository->isUsernameAvailable($value->getUsername(), $value->getId()->toString())
         ) {
             $this->context
                 ->buildViolation(UniqueUsername::MESSAGE)
