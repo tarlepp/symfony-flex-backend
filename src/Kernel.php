f07b1048c0e8f74db1ffc2b265db7efd0bcf2761
tarlepp <tarmo.leppanen@protacon.com>
2019-10-20 13:40:06 +0300

    chore: Updated all Symfony recipes.

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,13 +8,15 @@ declare(strict_types = 1);
 
 namespace App;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Exception\LoaderLoadException;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
+use function dirname;
 
 /**
  * Class Kernel
@@ -22,96 +24,47 @@ use Symfony\Component\Routing\RouteCollectionBuilder;
  * @package App
  * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
-class Kernel extends BaseKernel implements CompilerPassInterface
+class Kernel extends BaseKernel
 {
-    // Traits
     use MicroKernelTrait;
 
     private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
-    /** @noinspection PhpMissingParentCallCommonInspection */
     /**
-     * Gets the cache directory.
-     *
-     * @return string The cache directory
-     */
-    public function getCacheDir(): string
-    {
-        return $this->getProjectDir() . '/var/cache/' . $this->environment;
-    }
-
-    /** @noinspection PhpMissingParentCallCommonInspection */
-    /**
-     * Gets the log directory.
-     *
-     * @return string The log directory
-     */
-    public function getLogDir(): string
-    {
-        return $this->getProjectDir() . '/var/log';
-    }
-
-    /**
-     * Returns an array of bundles to register.
-     *
-     * @return iterable An array of bundle instances
+     * @return iterable
      */
     public function registerBundles(): iterable
     {
-        /** @var array $contents */
-        /** @noinspection UsingInclusionReturnValueInspection */
         /** @noinspection PhpIncludeInspection */
-        /** @psalm-suppress UnresolvableInclude */
+        /** @noinspection UsingInclusionReturnValueInspection */
         $contents = require $this->getProjectDir() . '/config/bundles.php';
 
-        foreach ($contents as $class => $environments) {
-            if (isset($environments['all']) || isset($environments[$this->environment])) {
+        foreach ($contents as $class => $envs) {
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
             }
         }
     }
 
+    /** @noinspection PhpMissingParentCallCommonInspection */
     /**
-     * You can modify the container here before it is dumped to PHP code.
-     *
-     * @param ContainerBuilder $container
+     * @return string
      */
-    public function process(ContainerBuilder $container): void
+    public function getProjectDir(): string
     {
+        return dirname(__DIR__);
     }
 
-    /** @noinspection PhpUnusedParameterInspection */
     /**
-     * Configures the container.
-     *
-     * You can register extensions:
-     *
-     * $c->loadFromExtension('framework', array(
-     *     'secret' => '%secret%'
-     * ));
-     *
-     * Or services:
-     *
-     * $c->register('halloween', 'FooBundle\HalloweenProvider');
-     *
-     * Or parameters:
-     *
-     * $c->setParameter('halloween', 'lot of fun');
-     *
      * @param ContainerBuilder $container
      * @param LoaderInterface  $loader
      *
-     * @throws \Exception
+     * @throws Exception
      */
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
         $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
-
-        // Feel free to remove the "container.autowiring.strict_mode" parameter
-        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
-        $container->setParameter('container.autowiring.strict_mode', true);
         $container->setParameter('container.dumper.inline_class_loader', true);
-
         $confDir = $this->getProjectDir() . '/config';
 
         $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
@@ -121,21 +74,16 @@ class Kernel extends BaseKernel implements CompilerPassInterface
     }
 
     /**
-     * Add or import routes into your application.
-     *
-     *     $routes->import('config/routing.yml');
-     *     $routes->add('/admin', 'AppBundle:Admin:dashboard', 'admin_dashboard');
-     *
      * @param RouteCollectionBuilder $routes
      *
-     * @throws \Symfony\Component\Config\Exception\FileLoaderLoadException
+     * @throws LoaderLoadException
      */
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
         $confDir = $this->getProjectDir() . '/config';
 
-        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
     }
 }
