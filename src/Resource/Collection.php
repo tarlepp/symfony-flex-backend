cd3802e7d13a9a9562ecc9627b6a3f7c6cda08b2
tarlepp <tarmo.leppanen@protacon.com>
2019-11-08 20:02:03 +0200

    feat: Implemented support to fetch REST resource class by given entity - note that if there is multiple resource classes for same entity you will get the "first" one of those.

--- src/Resource/Collection.php
+++ src/Resource/Collection.php
@@ -10,10 +10,12 @@ namespace App\Resource;
 
 use App\Collection\CollectionTrait;
 use App\Rest\RestResourceInterface;
+use CallbackFilterIterator;
 use Closure;
 use Countable;
 use InvalidArgumentException;
 use IteratorAggregate;
+use IteratorIterator;
 use function sprintf;
 
 /**
@@ -43,6 +45,37 @@ class Collection implements Countable
     }
 
     /**
+     * @param string $className
+     *
+     * @return RestResourceInterface|null
+     */
+    public function getEntityResource(string $className): ?RestResourceInterface
+    {
+        $current = $this->getFilteredItemByEntity($className);
+
+        if ($current === null) {
+            $message = sprintf(
+                'Resource class does not exists for entity \'%s\'',
+                $className
+            );
+
+            throw new InvalidArgumentException($message);
+        }
+
+        return $current;
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return bool
+     */
+    public function hasEntityResource(?string $className): bool
+    {
+        return $this->getFilteredItemByEntity((string)$className) !== null;
+    }
+
+    /**
      * @param string|null $className
      *
      * @return Closure
@@ -67,5 +100,23 @@ class Collection implements Countable
         );
 
         throw new InvalidArgumentException($message);
+    }
+
+    /**
+     * @param string $entityName
+     *
+     * @return RestResourceInterface|null
+     */
+    private function getFilteredItemByEntity(string $entityName): ?RestResourceInterface
+    {
+        $filteredIterator = new CallbackFilterIterator(
+            new IteratorIterator($this->items->getIterator()),
+            static function (RestResourceInterface $restResource) use ($entityName): bool {
+                return $restResource->getEntityName() === $entityName;
+            }
+        );
+        $filteredIterator->rewind();
+
+        return $filteredIterator->current();
     }
 }
--- tests/Integration/Resource/CollectionTest.php
+++ tests/Integration/Resource/CollectionTest.php
@@ -8,6 +8,15 @@ declare(strict_types = 1);
 
 namespace App\Tests\Integration\Resource;
 
+use App\Entity\ApiKey;
+use App\Entity\DateDimension;
+use App\Entity\Healthz;
+use App\Entity\LogLogin;
+use App\Entity\LogLoginFailure;
+use App\Entity\LogRequest;
+use App\Entity\Role;
+use App\Entity\User;
+use App\Entity\UserGroup;
 use App\Resource\ApiKeyResource;
 use App\Resource\Collection;
 use App\Resource\DateDimensionResource;
@@ -20,6 +29,7 @@ use App\Resource\UserGroupResource;
 use App\Resource\UserResource;
 use Generator;
 use InvalidArgumentException;
+use stdClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\PropertyAccess\Tests\Fixtures\TraversableArrayObject;
 
@@ -42,11 +52,25 @@ class CollectionTest extends KernelTestCase
         unset($collection);
     }
 
+    public function testThatGetEntityResourceMethodThrowsAnException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Resource class does not exists for entity \'FooBar\'');
+
+        $collection = new Collection(new TraversableArrayObject());
+        $collection->getEntityResource('FooBar');
+
+        unset($collection);
+    }
+
     public function testThatGetAllReturnsCorrectCountOfResources(): void
     {
-        $collection = $this->getCollection();
+        static::assertCount(9, $this->getCollection()->getAll());
+    }
 
-        static::assertCount(9, $collection->getAll());
+    public function testThatCountMethodReturnsExpectedCount(): void
+    {
+        static::assertSame(9, $this->getCollection()->count(), 'REST resource count from collection was not expected');
     }
 
     /**
@@ -56,10 +80,18 @@ class CollectionTest extends KernelTestCase
      */
     public function testThatGetReturnsExpectedResource(string $resourceClass): void
     {
-        $collection = $this->getCollection();
+        static::assertInstanceOf($resourceClass, $this->getCollection()->get($resourceClass));
+    }
 
-        /** @noinspection UnnecessaryAssertionInspection */
-        static::assertInstanceOf($resourceClass, $collection->get($resourceClass));
+    /**
+     * @dataProvider dataProviderTestThatGetEntityResourceReturnsExpectedResource
+     *
+     * @param string $entityClass
+     * @param string $resourceClass
+     */
+    public function testThatGetEntityResourceReturnsExpectedResource(string $entityClass, string $resourceClass): void
+    {
+        static::assertInstanceOf($resourceClass, $this->getCollection()->getEntityResource($entityClass));
     }
 
     /**
@@ -70,9 +102,18 @@ class CollectionTest extends KernelTestCase
      */
     public function testThatHasReturnsExpected(bool $expected, ?string $resource): void
     {
-        $collection = $this->getCollection();
+        static::assertSame($expected, $this->getCollection()->has($resource));
+    }
 
-        static::assertSame($expected, $collection->has($resource));
+    /**
+     * @dataProvider dataProviderTestThatHasEntityResourceReturnsExpected
+     *
+     * @param bool        $expected
+     * @param string|null $resource
+     */
+    public function testThatHasEntityResourceReturnsExpected(bool $expected, ?string $resource): void
+    {
+        static::assertSame($expected, $this->getCollection()->hasEntityResource($resource));
     }
 
     /**
@@ -94,6 +135,22 @@ class CollectionTest extends KernelTestCase
     /**
      * @return Generator
      */
+    public function dataProviderTestThatGetEntityResourceReturnsExpectedResource(): Generator
+    {
+        yield [ApiKey::class, ApiKeyResource::class];
+        yield [DateDimension::class, DateDimensionResource::class];
+        yield [Healthz::class, HealthzResource::class];
+        yield [LogLoginFailure::class, LogLoginFailureResource::class];
+        yield [LogLogin::class, LogLoginResource::class];
+        yield [LogRequest::class, LogRequestResource::class];
+        yield [Role::class, RoleResource::class];
+        yield [UserGroup::class, UserGroupResource::class];
+        yield [User::class, UserResource::class];
+    }
+
+    /**
+     * @return Generator
+     */
     public function dataProviderTestThatHasReturnsExpected(): Generator
     {
         yield [true, ApiKeyResource::class];
@@ -107,6 +164,26 @@ class CollectionTest extends KernelTestCase
         yield [true, UserResource::class];
         yield [false, null];
         yield [false, 'ResourceThatDoesNotExists'];
+        yield [false, stdClass::class];
+    }
+
+    /**
+     * @return Generator
+     */
+    public function dataProviderTestThatHasEntityResourceReturnsExpected(): Generator
+    {
+        yield [true, ApiKey::class];
+        yield [true, DateDimension::class];
+        yield [true, Healthz::class];
+        yield [true, LogLoginFailure::class];
+        yield [true, LogLogin::class];
+        yield [true, LogRequest::class];
+        yield [true, Role::class];
+        yield [true, UserGroup::class];
+        yield [true, User::class];
+        yield [false, null];
+        yield [false, 'ResourceThatDoesNotExists'];
+        yield [false, stdClass::class];
     }
 
     /**
