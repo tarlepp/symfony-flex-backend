f6bfb011c7780c5b5d5bd11d08fcba29b79cb886
tarlepp <tarmo.leppanen@protacon.com>
2019-11-18 21:04:07 +0200

    tests: Added test to cover `resolve` method of `EntityValueResolver`.

--- tests/Integration/ArgumentResolver/EntityValueResolverTest.php
+++ tests/Integration/ArgumentResolver/EntityValueResolverTest.php
@@ -11,11 +11,14 @@ namespace App\Tests\Integration\ArgumentResolver;
 use App\ArgumentResolver\EntityValueResolver;
 use App\Entity\User;
 use App\Resource\ResourceCollection;
+use App\Resource\UserResource;
 use PHPUnit\Framework\MockObject\MockObject;
 use stdClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Throwable;
+use function iterator_to_array;
 
 /**
  * Class EntityValueResolverTest
@@ -79,5 +82,38 @@ class EntityValueResolverTest extends KernelTestCase
         $metadata = new ArgumentMetadata('foo', User::class, false, false, null);
 
         static::assertFalse($resolver->supports(Request::create('/', 'GET', ['foo' => 'bar']), $metadata));
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testThatResolveMethodReturnsExpected(): void
+    {
+        /**
+         * @var MockObject|ResourceCollection $resourceCollection
+         * @var MockObject|UserResource       $userResource
+         */
+        $resourceCollection = $this->getMockBuilder(ResourceCollection::class)->disableOriginalConstructor()->getMock();
+        $userResource = $this->getMockBuilder(UserResource::class)->disableOriginalConstructor()->getMock();
+
+        $user = new User();
+
+        $resourceCollection
+            ->expects(static::once())
+            ->method('getEntityResource')
+            ->with(User::class)
+            ->willReturn($userResource);
+
+        $userResource
+            ->expects(static::once())
+            ->method('findOne')
+            ->with($user->getId())
+            ->willReturn($user);
+
+        $resolver = new EntityValueResolver($resourceCollection);
+        $metadata = new ArgumentMetadata('user', User::class, false, false, null);
+        $request = Request::create('/', 'GET', ['user' => $user->getId()]);
+
+        static::assertSame([$user], iterator_to_array($resolver->resolve($request, $metadata)));
     }
 }
