411f720c019a7c4e50e7bc97a2b5c52fe855e652
Tarmo Leppänen <tarmo.leppanen@kase.fi>
2020-02-14 21:13:06 +0200

    Feat: Remove `RolesService` from `User` entity (#292)
    * Feat: Remove `RolesService` from `User` entity
    
    Finally removed that service from that entity so that entity itself doesn't need to know something that it really should not know at all...
    
    * Removed obsolete `use` statements.
    
    * One more clean up
    
    * One more clean up
    
    * One more clean up
    
    * One more clean up
    
    * One more clean up

--- src/Entity/Traits/UserRelations.php
+++ src/Entity/Traits/UserRelations.php
@@ -14,13 +14,9 @@ use App\Entity\LogLoginFailure;
 use App\Entity\LogRequest;
 use App\Entity\User;
 use App\Entity\UserGroup;
-use App\Security\RolesService;
-use App\Security\Interfaces\RolesServiceInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Serializer\Annotation\Groups;
-use function array_unique;
-use function array_values;
 
 /**
  * Class UserRelations
@@ -90,25 +86,13 @@ trait UserRelations
     protected Collection $logsLoginFailure;
 
     /**
-     * @var RolesServiceInterface
-     */
-    private $rolesService;
-
-    /**
-     * @param RolesServiceInterface $rolesService
-     *
-     * @return User
-     */
-    public function setRolesService(RolesServiceInterface $rolesService): User
-    {
-        $this->rolesService = $rolesService;
-
-        /** @noinspection PhpIncompatibleReturnTypeInspection */
-        return $this;
-    }
-
-    /**
      * Getter for roles.
+     *
+     * Note that this will only return _direct_ roles that user has and
+     * not the inherited ones!
+     *
+     * If you want to get user inherited roles you need to implement that
+     * logic by yourself OR use eg. `/user/{uuid}/roles` API endpoint.
      *
      * @psalm-return array<int, string>
      *
@@ -120,19 +104,7 @@ trait UserRelations
      */
     public function getRoles(): array
     {
-        $iterator = static function (UserGroup $userGroup): string {
-            return $userGroup->getRole()->getId();
-        };
-
-        /** @var array<int, string> $output */
-        $output = $this->userGroups->map($iterator)->toArray();
-
-        // And if we have roles service present we can fetch all inherited roles
-        if ($this->rolesService instanceof RolesService) {
-            $output = $this->rolesService->getInheritedRoles($output);
-        }
-
-        return array_values(array_map('\strval', array_unique($output)));
+        return $this->userGroups->map(fn (UserGroup $userGroup): string => $userGroup->getRole()->getId())->toArray();
     }
 
     /**
