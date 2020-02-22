8560b17957e8f235720456a6a1aff9849367a6f5
tarlepp <tarmo.leppanen@protacon.com>
2020-02-22 16:16:37 +0200

    Added support for translated authentication failure.

---
+++ src/Security/Handler/TranslatedAuthenticationFailureHandler.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Security/Handler/TranslatedAuthenticationFailureHandler.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Security\Handler;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationFailureHandler;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Class TranslatedAuthenticationFailureHandler
+ *
+ * @package App\Security\Handler
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class TranslatedAuthenticationFailureHandler extends AuthenticationFailureHandler
+{
+    private TranslatorInterface $translator;
+
+    /**
+     * TranslatedAuthenticationFailureHandler constructor.
+     *
+     * @param EventDispatcherInterface $dispatcher
+     * @param TranslatorInterface      $translator
+     */
+    public function __construct(EventDispatcherInterface $dispatcher, TranslatorInterface $translator)
+    {
+        parent::__construct($dispatcher);
+
+        $this->translator = $translator;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @param Request                 $request
+     * @param AuthenticationException $exception
+     *
+     * @return Response
+     *
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $event = new AuthenticationFailureEvent(
+            $exception,
+            new JWTAuthenticationFailureResponse(
+                $this->translator->trans('Invalid credentials.', [], 'security')
+            )
+        );
+
+        $this->dispatcher->dispatch($event);
+
+        return $event->getResponse();
+    }
+}
