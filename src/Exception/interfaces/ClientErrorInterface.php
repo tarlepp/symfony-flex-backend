72ac925b1db86d70add45d7219bc5acefb89c459
tarlepp <tarmo.leppanen@protacon.com>
2020-02-26 20:45:44 +0200

    Added proper docs to interface.

--- src/Exception/interfaces/ClientErrorInterface.php
+++ src/Exception/interfaces/ClientErrorInterface.php
@@ -11,6 +11,14 @@ namespace App\Exception\interfaces;
 /**
  * Class ClientErrorInterface
  *
+ * If you want to expose _your_ exception class message as-is on `prod` environment that should
+ * implement either this interface or `Symfony\Component\HttpKernel\HttpKernelInterface` to get
+ * that message to frontend side - otherwise you will just get `Internal server error.` message
+ * with HTTP status 500.
+ *
+ * If your exception is not setting `code` properly, note that you will get that HTTP status 500
+ * on those - so it's _your_ responsibility to set "proper" status code in your exception class.
+ *
  * @package App\Exception\interfaces
  * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
  */
