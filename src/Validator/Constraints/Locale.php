21190308daca1810dfde1007451cc9d0a4fd27bc
tarlepp <tarmo.leppanen@protacon.com>
2020-03-01 14:48:35 +0200

    Added new custom validators for `language`, `locale` and `timezone` so that we can use those on entity and dto level easier.

---
+++ src/Validator/Constraints/Locale.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Validator/Constraints/Locale.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Class Locale
+ *
+ * @Annotation
+ * @Target({"PROPERTY"})
+ *
+ * @package App\Validator\Constraints
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class Locale extends Constraint
+{
+    /**
+     * Unique constant for validator constrain
+     */
+    public const INVALID_LOCALE = '44e3862f-2d38-46d4-b1ae-632990814af6';
+
+    /**
+     * Message for validation error
+     */
+    public const MESSAGE = 'This locale "{{ locale }}" is not valid.';
+
+    /**
+     * Error names configuration
+     *
+     * @var array<string, string>
+     */
+    protected static $errorNames = [
+        self::INVALID_LOCALE => 'INVALID_LOCALE',
+    ];
+
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * {@inheritdoc}
+     */
+    public function getTargets(): string
+    {
+        return self::PROPERTY_CONSTRAINT;
+    }
+}
---
+++ src/Validator/Constraints/Timezone.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Validator/Constraints/Timezone.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Class Timezone
+ *
+ * @Annotation
+ * @Target({"PROPERTY"})
+ *
+ * @package App\Validator\Constraints
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class Timezone extends Constraint
+{
+    /**
+     * Unique constant for validator constrain
+     */
+    public const INVALID_TIMEZONE = '1f8dd2a3-5b61-43ca-a6b2-af553f86ac17';
+
+    /**
+     * Message for validation error
+     */
+    public const MESSAGE = 'This timezone "{{ timezone }}" is not valid.';
+
+    /**
+     * Error names configuration
+     *
+     * @var array<string, string>
+     */
+    protected static $errorNames = [
+        self::INVALID_TIMEZONE => 'INVALID_TIMEZONE',
+    ];
+
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    /**
+     * {@inheritdoc}
+     */
+    public function getTargets(): string
+    {
+        return self::PROPERTY_CONSTRAINT;
+    }
+}
