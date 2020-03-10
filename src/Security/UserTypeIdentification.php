33b79d5840f5e931fed957f42c61b2b2a1ab0472
tarlepp <tarmo.leppanen@protacon.com>
2020-03-10 22:22:12 +0200

    Added `UserTypeIdentification` service to handle common `TokenStorageInterface` stuff

---
+++ src/Security/UserTypeIdentification.php
@@ -0,0 +1,126 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Security/UserTypeIdentification.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Security;
+
+use App\Entity\ApiKey;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Class UserTypeIdentification
+ *
+ * @package App\Security
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UserTypeIdentification
+{
+    private TokenStorageInterface $tokenStorage;
+    private UserRepository $userRepository;
+
+    /**
+     * UserService constructor.
+     *
+     * @param TokenStorageInterface $tokenStorage
+     * @param UserRepository        $userRepository
+     */
+    public function __construct(TokenStorageInterface $tokenStorage, UserRepository $userRepository)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Helper method to get current logged in ApiKey entity via token storage.
+     *
+     * @return ApiKey|null
+     */
+    public function getApiKey(): ?ApiKey
+    {
+        $apiKeyUser = $this->getApiKeyUser();
+
+        return $apiKeyUser === null ? null : $apiKeyUser->getApiKey();
+    }
+
+    /**
+     * Helper method to get current logged in User entity via token storage.
+     *
+     * @return User|null
+     *
+     * @throws NonUniqueResultException
+     */
+    public function getUser(): ?User
+    {
+        $user = $this->getSecurityUser();
+
+        return $user === null ? null : $this->userRepository->loadUserByUsername($user->getUsername(), true);
+    }
+
+    /**
+     * Helper method to get user identify object via token storage.
+     *
+     * @return SecurityUser|ApiKeyUser|null
+     */
+    public function getIdentify(): ?UserInterface
+    {
+        return $this->getSecurityUser() ?? $this->getApiKeyUser();
+    }
+
+    /**
+     * Helper method to get current logged in ApiKeyUser via token storage.
+     *
+     * @return ApiKeyUser|null
+     */
+    public function getApiKeyUser(): ?ApiKeyUser
+    {
+        $apiKeyUser = $this->getUserToken();
+
+        if ($apiKeyUser instanceof ApiKeyUser) {
+            $output = $apiKeyUser;
+        }
+
+        return $output ?? null;
+    }
+
+    /**
+     * Helper method to get current logged in SecurityUser via token storage.
+     *
+     * @return SecurityUser|null
+     */
+    public function getSecurityUser(): ?SecurityUser
+    {
+        $securityUser = $this->getUserToken();
+
+        if ($securityUser instanceof SecurityUser) {
+            $output = $securityUser;
+        }
+
+        return $output ?? null;
+    }
+
+    /**
+     * Returns a user representation. Can be a UserInterface instance, an object
+     * implementing a __toString method, or the username as a regular string.
+     *
+     * @return object|string|null
+     */
+    private function getUserToken()
+    {
+        $token = $this->tokenStorage->getToken();
+
+        if (!($token === null || $token instanceof AnonymousToken)) {
+            $output = $token->getUser();
+        }
+
+        return $output ?? null;
+    }
+}
