7b48c6c24309b682cf9863bc3a299c8c2d6344b8
tarlepp <tarmo.leppanen@protacon.com>
2020-03-13 21:24:22 +0200

    Added missing tests

---
+++ tests/Unit/Validator/Constraints/LanguageTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Unit/Validator/Constraints/LanguageTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Tests\Unit\Validator\Constraints;
+
+use App\Validator\Constraints\Language;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class LanguageTest
+ *
+ * @package App\Tests\Unit\Validator\Constraints
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class LanguageTest extends KernelTestCase
+{
+    public function testThatGetTargetsReturnsExpected(): void
+    {
+        static::assertSame('property', (new Language())->getTargets());
+    }
+}
---
+++ tests/Unit/Validator/Constraints/UniqueUsernameTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Unit/Validator/Constraints/UniqueUsernameTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+
+namespace App\Tests\Unit\Validator\Constraints;
+
+use App\Validator\Constraints\UniqueUsername;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class UniqueUsernameTest
+ *
+ * @package App\Tests\Unit\Validator\Constraints
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class UniqueUsernameTest extends KernelTestCase
+{
+    public function testThatGetTargetsReturnsExpected(): void
+    {
+        static::assertSame('class', (new UniqueUsername())->getTargets());
+    }
+}
