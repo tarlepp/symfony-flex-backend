a9565409e8575701fbd48c770562d1de0276a244
tarlepp <tarmo.leppanen@protacon.com>
2020-04-10 21:01:58 +0300

    Refactor: Change the logic of `ExecuteMultipleCommandTrait`

--- src/Command/Traits/ExecuteMultipleCommandTrait.php
+++ src/Command/Traits/ExecuteMultipleCommandTrait.php
@@ -62,9 +62,9 @@ trait ExecuteMultipleCommandTrait
     {
         $this->io = new SymfonyStyle($input, $output);
         $this->io->write("\033\143");
+        $command = $this->ask();
 
-        /** @noinspection PhpAssignmentInConditionInspection */
-        while ($command = $this->ask()) {
+        while ($command !== null) {
             $arguments = [
                 'command' => $command,
             ];
@@ -73,6 +73,8 @@ trait ExecuteMultipleCommandTrait
 
             $cmd = $this->getApplication()->find((string)$command);
             $cmd->run($input, $output);
+
+            $command = $this->ask();
         }
 
         if ($input->isInteractive()) {
@@ -85,9 +87,9 @@ trait ExecuteMultipleCommandTrait
     /**
      * Method to ask user to make choose one of defined choices.
      *
-     * @return string|bool
+     * @return string|null
      */
-    private function ask()
+    private function ask(): ?string
     {
         $index = array_search(
             $this->io->choice('What you want to do', array_values($this->choices)),
@@ -97,6 +99,6 @@ trait ExecuteMultipleCommandTrait
 
         $choice = (string)array_values(array_flip($this->choices))[(int)$index];
 
-        return $choice === '0' ? false : $choice;
+        return $choice === '0' ? null : $choice;
     }
 }
