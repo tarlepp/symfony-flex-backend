95773f4bcc8fa0df487124d36541482e003fe6e8
tarlepp <tarmo.leppanen@protacon.com>
2020-10-29 20:24:42 +0200

    protacon => pinja

--- tests/Integration/Rest/Traits/Methods/src/FindMethodInvalidTestClass.php
+++ tests/Integration/Rest/Traits/Methods/src/FindMethodInvalidTestClass.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 /**
  * /tests/Integration/Rest/Traits/Methods/FindMethodInvalidTestClass.php
  *
- * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
 
 namespace App\Tests\Integration\Rest\Traits\Methods\src;
@@ -16,7 +16,7 @@ use App\Rest\Traits\RestMethodHelper;
  * Class FindMethodInvalidTestClass - just a dummy class so that we can actually test that trait.
  *
  * @package App\Tests\Integration\Rest\Traits\Methods\src
- * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
 abstract class FindMethodInvalidTestClass
 {
