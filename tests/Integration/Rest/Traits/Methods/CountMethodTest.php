4da436a2f21087e77e55866fdd4a90cbc841abb2
tarlepp <tarmo.leppanen@protacon.com>
2020-10-29 20:24:27 +0200

    Refactored tests; added missing @testdox annotations, nicer setup for test cases, etc.

--- tests/Integration/Rest/Traits/Methods/CountMethodTest.php
+++ tests/Integration/Rest/Traits/Methods/CountMethodTest.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 /**
  * /tests/Integration/Rest/Traits/Methods/CountMethodTest.php
  *
- * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
 
 namespace App\Tests\Integration\Rest\Traits\Methods;
@@ -12,29 +12,53 @@ use App\Rest\Interfaces\ResponseHandlerInterface;
 use App\Rest\Interfaces\RestResourceInterface;
 use App\Tests\Integration\Rest\Traits\Methods\src\CountMethodInvalidTestClass;
 use App\Tests\Integration\Rest\Traits\Methods\src\CountMethodTestClass;
+use App\Utils\Tests\StringableArrayObject;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\NoResultException;
 use Exception;
 use Generator;
+use InvalidArgumentException;
 use LogicException;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 /**
  * Class CountMethodTest
  *
  * @package App\Tests\Integration\Rest\Traits\Methods
- * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
 class CountMethodTest extends KernelTestCase
 {
     /**
+     * @var MockObject|RestResourceInterface
+     */
+    private $resource;
+
+    /**
+     * @var MockObject|ResponseHandlerInterface
+     */
+    private $responseHandler;
+
+    /**
+     * @var MockObject|CountMethodTestClass
+     */
+    private $validTestClass;
+
+    /**
+     * @var MockObject|CountMethodInvalidTestClass
+     */
+    private $inValidTestClass;
+
+    /**
      * @throws Throwable
+     *
+     * @testdox Test that `countMethod` throws an exception if class doesn't implement `ControllerInterface`
      */
     public function testThatTraitThrowsAnException(): void
     {
@@ -46,12 +70,7 @@ class CountMethodTest extends KernelTestCase
         );
         /** @codingStandardsIgnoreEnd */
 
-        /** @var MockObject|CountMethodInvalidTestClass $testClass */
-        $testClass = $this->getMockForAbstractClass(CountMethodInvalidTestClass::class);
-
-        $request = Request::create('/');
-
-        $testClass->countMethod($request);
+        $this->inValidTestClass->countMethod(Request::create('/'));
     }
 
     /**
@@ -59,121 +78,78 @@ class CountMethodTest extends KernelTestCase
      *
      * @throws Throwable
      *
-     * @testdox Test that `App\Rest\Traits\Methods\CountMethod` throws an exception with `$httpMethod` HTTP method.
+     * @testdox Test that `countMethod` throws an exception when using `$httpMethod` HTTP method
      */
     public function testThatTraitThrowsAnExceptionWithWrongHttpMethod(string $httpMethod): void
     {
         $this->expectException(MethodNotAllowedHttpException::class);
 
-        $resource = $this->createMock(RestResourceInterface::class);
-        $responseHandler = $this->createMock(ResponseHandlerInterface::class);
-
-        /** @var MockObject|CountMethodTestClass $testClass */
-        $testClass = $this->getMockForAbstractClass(
-            CountMethodTestClass::class,
-            [$resource, $responseHandler]
-        );
-
-        // Create request and response
-        $request = Request::create('/', $httpMethod);
-
-        $testClass->countMethod($request)->getContent();
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function testThatTraitCallsProcessCriteriaIfItExists(): void
-    {
-        $resource = $this->createMock(RestResourceInterface::class);
-        $responseHandler = $this->createMock(ResponseHandlerInterface::class);
-
-        /** @var MockObject|CountMethodTestClass $testClass */
-        $testClass = $this->getMockForAbstractClass(
-            CountMethodTestClass::class,
-            [$resource, $responseHandler],
-            '',
-            true,
-            true,
-            true,
-            ['processCriteria']
-        );
-
-        // Create request
-        $request = Request::create('/');
-
-        $testClass
-            ->expects(static::once())
-            ->method('processCriteria')
-            ->withAnyParameters();
-
-        $testClass->countMethod($request)->getContent();
+        $this->validTestClass->countMethod(Request::create('/', $httpMethod))->getContent();
     }
 
     /**
      * @dataProvider dataProviderTestThatTraitHandlesException
      *
-     * @param Exception $exception
-     *
      * @throws Throwable
      *
-     * @testdox Test that `App\Rest\Traits\Methods\CountMethod` uses `$expectedCode` code on HttpException.
+     * @testdox Test that `countMethod` uses `$expectedCode` HTTP status code with `$exception` exception
      */
-    public function testThatTraitHandlesException(\Throwable $exception, int $expectedCode): void
+    public function testThatTraitHandlesException(Throwable $exception, int $expectedCode): void
     {
-        $resource = $this->createMock(RestResourceInterface::class);
-        $responseHandler = $this->createMock(ResponseHandlerInterface::class);
-
-        /** @var MockObject|CountMethodTestClass $testClass */
-        $testClass = $this->getMockForAbstractClass(
-            CountMethodTestClass::class,
-            [$resource, $responseHandler]
-        );
-
         $request = Request::create('/');
 
-        $resource
+        $this->resource
             ->expects(static::once())
             ->method('count')
+            ->with([], [])
             ->willThrowException($exception);
 
         $this->expectException(HttpException::class);
         $this->expectExceptionCode($expectedCode);
 
-        $testClass->countMethod($request);
+        $this->validTestClass->countMethod($request);
+    }
+
+    /**
+     * @dataProvider dataProviderTestThatTraitCallsServiceMethods
+     *
+     * @throws Throwable
+     *
+     * @testdox Test that `countMethod` method calls expected service methods when using `$queryString` as query string
+     */
+    public function testThatTraitCallsServiceMethods(
+        string $queryString,
+        StringableArrayObject $criteria,
+        StringableArrayObject $search
+    ): void {
+        $request = Request::create('/' . $queryString);
+
+        $this->resource
+            ->expects(static::once())
+            ->method('count')
+            ->with($criteria->getArrayCopy(), $search->getArrayCopy())
+            ->willReturn(0);
+
+        $this->responseHandler
+            ->expects(static::once())
+            ->method('createResponse')
+            ->with($request, ['count' => 0], $this->resource);
+
+        $this->validTestClass->countMethod($request);
     }
 
     /**
      * @throws Throwable
+     *
+     * @testdox Test that `countMethod` throws an exception when `?where` parameter is not valid JSON
      */
-    public function testThatTraitCallsServiceMethods(): void
+    public function testThatTraitThrowsAnExceptionWhenWhereParameterIsNotValidJson(): void
     {
-        $resource = $this->createMock(RestResourceInterface::class);
-        $responseHandler = $this->createMock(ResponseHandlerInterface::class);
+        $this->expectException(HttpException::class);
+        $this->expectExceptionCode(400);
+        $this->expectExceptionMessage('Current \'where\' parameter is not valid JSON.');
 
-        /** @var MockObject|CountMethodTestClass $testClass */
-        $testClass = $this->getMockForAbstractClass(
-            CountMethodTestClass::class,
-            [$resource, $responseHandler]
-        );
-
-        // Create request and response
-        $request = Request::create('/count');
-        $response = new Response('123');
-
-        $resource
-            ->expects(static::once())
-            ->method('count')
-            ->withAnyParameters()
-            ->willReturn(123);
-
-        $responseHandler
-            ->expects(static::once())
-            ->method('createResponse')
-            ->withAnyParameters()
-            ->willReturn($response);
-
-        $testClass->countMethod($request)->getContent();
+        $this->validTestClass->countMethod(Request::create('/?where=foo'));
     }
 
     public function dataProviderTestThatTraitThrowsAnExceptionWithWrongHttpMethod(): Generator
@@ -190,9 +166,80 @@ class CountMethodTest extends KernelTestCase
 
     public function dataProviderTestThatTraitHandlesException(): Generator
     {
-        yield [new HttpException(400), 0];
+        yield [new HttpException(400, '', null, [], 400), 400];
         yield [new NoResultException(), 404];
+        yield [new NotFoundHttpException(), 404];
         yield [new NonUniqueResultException(), 500];
         yield [new Exception(), 400];
+        yield [new LogicException(), 400];
+        yield [new InvalidArgumentException(), 400];
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function dataProviderTestThatTraitCallsServiceMethods(): Generator
+    {
+        yield ['', new StringableArrayObject([]), new StringableArrayObject([])];
+
+        yield [
+            '?where={"foo": "bar"}',
+            new StringableArrayObject(['foo' => 'bar']),
+            new StringableArrayObject([]),
+        ];
+
+        yield [
+            '?where={"foo": {"bar": "foobar"}}',
+            new StringableArrayObject(['foo' => ['bar' => 'foobar']]),
+            new StringableArrayObject([]),
+        ];
+
+        yield [
+            '?search=term',
+            new StringableArrayObject([]),
+            new StringableArrayObject(['or' => ['term']]),
+        ];
+
+        yield [
+            '?search=term1+term2',
+            new StringableArrayObject([]),
+            new StringableArrayObject(['or' => ['term1', 'term2']]),
+        ];
+
+        yield [
+            '?search={"and": ["term1", "term2"]}',
+            new StringableArrayObject([]),
+            new StringableArrayObject(['and' => ['term1', 'term2']]),
+        ];
+
+        yield [
+            '?search={"or": ["term1", "term2"]}',
+            new StringableArrayObject([]),
+            new StringableArrayObject(['or' => ['term1', 'term2']]),
+        ];
+
+        yield [
+            '?search={"and": ["term1", "term2"], "or": ["term3", "term4"]}',
+            new StringableArrayObject([]),
+            new StringableArrayObject(['and' => ['term1', 'term2'], 'or' => ['term3', 'term4']]),
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->resource = $this->getMockBuilder(RestResourceInterface::class)->getMock();
+
+        $this->responseHandler = $this->getMockBuilder(ResponseHandlerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->validTestClass = $this->getMockForAbstractClass(
+            CountMethodTestClass::class,
+            [$this->resource, $this->responseHandler]
+        );
+
+        $this->inValidTestClass = $this->getMockForAbstractClass(CountMethodInvalidTestClass::class);
     }
 }
