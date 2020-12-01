7245d8b19e01ad7c3da57dbf265132b79cf58434
tarlepp <tarmo.leppanen@protacon.com>
2020-12-01 17:52:36 +0200

    Fixed indentations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 /**
  * /src/Entity/User.php
  *
- * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
 
 namespace App\Entity;
@@ -35,14 +35,14 @@ use Throwable;
  * @ORM\Table(
  *      name="user",
  *      uniqueConstraints={
- * @ORM\UniqueConstraint(name="uq_username", columns={"username"}),
- * @ORM\UniqueConstraint(name="uq_email", columns={"email"}),
+ *          @ORM\UniqueConstraint(name="uq_username", columns={"username"}),
+ *          @ORM\UniqueConstraint(name="uq_email", columns={"email"}),
  *      },
  *  )
  * @ORM\Entity()
  *
  * @package App\Entity
- * @author TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
 class User implements EntityInterface, UserInterface, UserGroupAwareInterface
 {
