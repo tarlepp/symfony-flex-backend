a6de07622a12abf648e68e4089fd7b94db157551
tarlepp <tarmo.leppanen@protacon.com>
2021-01-20 17:48:16 +0200

    Chore/Recipes - updated `symfony/framework-bundle` recipe

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use function dirname;
+use function is_file;
 
 /**
  * Class Kernel
@@ -27,8 +29,13 @@ class Kernel extends BaseKernel
     {
         $container->import('../config/{packages}/*.yaml');
         $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
-        $container->import('../config/{services}.yaml');
-        $container->import('../config/{services}_' . $this->environment . '.yaml');
+
+        if (is_file(dirname(__DIR__) . '/config/services.yaml')) {
+            $container->import('../config/services.yaml');
+            $container->import('../config/{services}_' . $this->environment.'.yaml');
+        } elseif (is_file($path = dirname(__DIR__) . '/config/services.php')) {
+            (require $path)($container->withPath($path), $this);
+        }
     }
 
     /**
@@ -38,6 +45,11 @@ class Kernel extends BaseKernel
     {
         $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
-        $routes->import('../config/{routes}.yaml');
+
+        if (is_file(dirname(__DIR__) . '/config/routes.yaml')) {
+            $routes->import('../config/routes.yaml');
+        } elseif (is_file($path = dirname(__DIR__) . '/config/routes.php')) {
+            (require $path)($routes->withPath($path), $this);
+        }
     }
 }
