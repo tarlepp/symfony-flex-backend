c7e0dc2a7aad43e8fc4886dce406a8e535f4a8cf
tarlepp <tarmo.leppanen@protacon.com>
2021-02-14 20:56:19 +0200

    Harmonized annotations styles

--- src/Entity/LogRequest.php
+++ src/Entity/LogRequest.php
@@ -27,13 +27,13 @@ use function mb_strlen;
  * @ORM\Table(
  *      name="log_request",
  *      indexes={
- * @ORM\Index(name="user_id", columns={"user_id"}),
- * @ORM\Index(name="api_key_id", columns={"api_key_id"}),
- * @ORM\Index(name="request_date", columns={"date"}),
- *      }
+ *          @ORM\Index(name="user_id", columns={"user_id"}),
+ *          @ORM\Index(name="api_key_id", columns={"api_key_id"}),
+ *          @ORM\Index(name="request_date", columns={"date"}),
+ *      },
  *  )
  * @ORM\Entity(
- *      readOnly=true
+ *      readOnly=true,
  *  )
  * @ORM\HasLifecycleCallbacks()
  *
@@ -77,7 +77,7 @@ class LogRequest implements EntityInterface
      *      inversedBy="logsRequest",
      *  )
      * @ORM\JoinColumns({
-     * @ORM\JoinColumn(
+     *      @ORM\JoinColumn(
      *          name="user_id",
      *          referencedColumnName="id",
      *          nullable=true,
@@ -97,7 +97,7 @@ class LogRequest implements EntityInterface
      *      inversedBy="logsRequest",
      *  )
      * @ORM\JoinColumns({
-     * @ORM\JoinColumn(
+     *      @ORM\JoinColumn(
      *          name="api_key_id",
      *          referencedColumnName="id",
      *          nullable=true,
