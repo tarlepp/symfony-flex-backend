cb02371dc6828c65d35f4f4180006ea79dba22c0
tarlepp <tarmo.leppanen@protacon.com>
2021-02-14 20:03:22 +0200

    Fixed return types

--- src/Entity/Traits/LogRequestProcessRequestTrait.php
+++ src/Entity/Traits/LogRequestProcessRequestTrait.php
@@ -354,7 +354,7 @@ trait LogRequestProcessRequestTrait
         // Clean possible sensitive data from parameters
         array_walk(
             $rawHeaders,
-            fn (mixed &$value, string $key): void => $this->cleanParameters($value, $key),
+            fn (mixed &$value, string $key) => $this->cleanParameters($value, $key),
         );
 
         $this->headers = $rawHeaders;
@@ -364,7 +364,7 @@ trait LogRequestProcessRequestTrait
         // Clean possible sensitive data from parameters
         array_walk(
             $rawParameters,
-            fn (mixed &$value, string $key): void => $this->cleanParameters($value, $key),
+            fn (mixed &$value, string $key) => $this->cleanParameters($value, $key),
         );
 
         $this->parameters = $rawParameters;
@@ -444,7 +444,7 @@ trait LogRequestProcessRequestTrait
         if (is_array($value)) {
             array_walk(
                 $value,
-                fn (mixed &$value, string $key): void => $this->cleanParameters($value, $key),
+                fn (mixed &$value, string $key) => $this->cleanParameters($value, $key),
             );
         }
     }
