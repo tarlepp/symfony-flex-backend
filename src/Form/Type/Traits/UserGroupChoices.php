cf2d22e86c08f221f1b866d6cc656cb889e2e76c
tarlepp <tarmo.leppanen@protacon.com>
2021-02-16 20:55:50 +0200

    Added missing `@property` annotation + cleaned up the code a bit

--- src/Form/Type/Traits/UserGroupChoices.php
+++ src/Form/Type/Traits/UserGroupChoices.php
@@ -9,6 +9,7 @@ declare(strict_types = 1);
 namespace App\Form\Type\Traits;
 
 use App\Entity\UserGroup;
+use App\Resource\UserGroupResource;
 use Throwable;
 use function array_map;
 
@@ -17,6 +18,8 @@ use function array_map;
  *
  * @package App\Form\Type\Traits
  * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ *
+ * @property UserGroupResource $userGroupResource
  */
 trait UserGroupChoices
 {
@@ -33,9 +36,8 @@ trait UserGroupChoices
         $choices = [];
 
         /**
-         * Lambda function to iterate all user groups and to create necessary choices array.
-         *
-         * @param UserGroup $userGroup
+         * Lambda function to iterate all user groups and to create necessary
+         * choices array.
          */
         $iterator = static function (UserGroup $userGroup) use (&$choices): void {
             $name = $userGroup->getName() . ' [' . $userGroup->getRole()->getId() . ']';
@@ -43,9 +45,7 @@ trait UserGroupChoices
             $choices[$name] = $userGroup->getId();
         };
 
-        $userGroups = $this->userGroupResource->find();
-
-        array_map($iterator, $userGroups);
+        array_map($iterator, $this->userGroupResource->find());
 
         return $choices;
     }
