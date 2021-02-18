f0ca2968e1dd65b4fbc7d590966558a479a92b15
tarlepp <tarmo.leppanen@protacon.com>
2021-02-18 19:58:10 +0200

    Clean up

--- src/Validator/Constraints/Locale.php
+++ src/Validator/Constraints/Locale.php
@@ -13,6 +13,11 @@ use Symfony\Component\Validator\Constraint;
 /**
  * Class Locale
  *
+ * Usage example;
+ *  App\Validator\Constraints\Locale()
+ *
+ * Just add that to your property as an annotation and you're good to go.
+ *
  * @Annotation
  * @Target({"PROPERTY"})
  *
@@ -21,14 +26,7 @@ use Symfony\Component\Validator\Constraint;
  */
 class Locale extends Constraint
 {
-    /**
-     * Unique constant for validator constrain
-     */
     public const INVALID_LOCALE = '44e3862f-2d38-46d4-b1ae-632990814af6';
-
-    /**
-     * Message for validation error
-     */
     public const MESSAGE = 'This locale "{{ locale }}" is not valid.';
 
     /**
@@ -41,9 +39,6 @@ class Locale extends Constraint
     ];
 
     /** @noinspection PhpMissingParentCallCommonInspection */
-    /**
-     * {@inheritdoc}
-     */
     public function getTargets(): string
     {
         return self::PROPERTY_CONSTRAINT;
