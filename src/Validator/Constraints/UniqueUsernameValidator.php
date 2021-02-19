4461dbd06b468885eee6e00b22dc43cafec0700f
tarlepp <tarmo.leppanen@protacon.com>
2021-02-19 16:21:52 +0200

    Use PHP 8 constructor property promotion

--- src/Validator/Constraints/UniqueUsernameValidator.php
+++ src/Validator/Constraints/UniqueUsernameValidator.php
@@ -22,11 +22,9 @@ use Symfony\Component\Validator\ConstraintValidator;
  */
 class UniqueUsernameValidator extends ConstraintValidator
 {
-    private UserRepository $repository;
-
-    public function __construct(UserRepository $repository)
-    {
-        $this->repository = $repository;
+    public function __construct(
+        private UserRepository $repository,
+    ) {
     }
 
     /**
