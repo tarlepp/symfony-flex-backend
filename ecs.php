a21b3a722164ec6f1a24a45263331d7703732ad9
tarlepp <tarmo.leppanen@protacon.com>
2021-02-25 20:40:58 +0200

    Changed ECS configuration to match new directory structure

--- ecs.php
+++ ecs.php
@@ -37,11 +37,11 @@ use Symplify\CodingStandard\Fixer\Strict\BlankLineAfterStrictTypesFixer;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $imports = [
-        '/tools/04_ecs/vendor/symplify/easy-coding-standard/config/set/psr12.php',
-        '/tools/04_ecs/vendor/symplify/easy-coding-standard/config/set/php80-migration-risky.php',
-        '/tools/04_ecs/vendor/symplify/easy-coding-standard/config/set/clean-code.php',
-        '/tools/04_ecs/vendor/symplify/easy-coding-standard/config/set/common.php',
-        '/tools/04_ecs/vendor/symplify/easy-coding-standard/config/set/symfony.php',
+        '/tools/04_symplify/vendor/symplify/easy-coding-standard/config/set/psr12.php',
+        '/tools/04_symplify/vendor/symplify/easy-coding-standard/config/set/php80-migration-risky.php',
+        '/tools/04_symplify/vendor/symplify/easy-coding-standard/config/set/clean-code.php',
+        '/tools/04_symplify/vendor/symplify/easy-coding-standard/config/set/common.php',
+        '/tools/04_symplify/vendor/symplify/easy-coding-standard/config/set/symfony.php',
     ];
 
     array_map(
