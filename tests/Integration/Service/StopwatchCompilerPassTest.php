9aae8a8f5314a30a155a6e03a4c6d2c003eae280
tarlepp <tarmo.leppanen@protacon.com>
2021-04-02 21:03:53 +0300

    Added tests for new services

---
+++ tests/Integration/Service/StopwatchCompilerPassTest.php
@@ -0,0 +1,86 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/Service/StopwatchCompilerPassTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+
+namespace App\Tests\Integration\Service;
+
+use App\Service\StopwatchCompilerPass;
+use stdClass;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+/**
+ * Class StopwatchCompilerPassTest
+ *
+ * @package App\Tests\Integration\Service
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+class StopwatchCompilerPassTest extends KernelTestCase
+{
+    /**
+     * @testdox Test that `findTaggedServiceIds` method is called expected times
+     */
+    public function testThatFindTaggedServiceIdsMethodIsCalled(): void
+    {
+        $container = $this->getMockBuilder(ContainerBuilder::class)->disableOriginalConstructor()->getMock();
+
+        $container
+            ->expects(static::exactly(5))
+            ->method('findTaggedServiceIds')
+            ->willReturn([]);
+
+        (new StopwatchCompilerPass())->process($container);
+    }
+
+    /**
+     * @testdox Test that no other container methods are called when tagged service is not supported
+     */
+    public function testThatIfServiceStartsWithAppNoOtherContainerMethodsAreCalled(): void
+    {
+        $container = $this->getMockBuilder(ContainerBuilder::class)->disableOriginalConstructor()->getMock();
+
+        $container
+            ->expects(static::exactly(5))
+            ->method('findTaggedServiceIds')
+            ->willReturn([stdClass::class => []]);
+
+        $container
+            ->expects(static::never())
+            ->method('getDefinition');
+
+        $container
+            ->expects(static::never())
+            ->method('setDefinition');
+
+        (new StopwatchCompilerPass())->process($container);
+    }
+
+    public function testThatAllExpectedContainerMethodsAreCalled(): void
+    {
+        $container = $this->getMockBuilder(ContainerBuilder::class)->disableOriginalConstructor()->getMock();
+        $definition = $this->getMockBuilder(Definition::class)->disableOriginalConstructor()->getMock();
+
+        $container
+            ->expects(static::exactly(5))
+            ->method('findTaggedServiceIds')
+            ->willReturn(['App\Foo' => []]);
+
+        $container
+            ->expects(static::exactly(5))
+            ->method('getDefinition')
+            ->with('App\Foo')
+            ->willReturn($definition);
+
+        $container
+            ->expects(static::exactly(5))
+            ->method('setDefinition')
+            ->with('App\Foo.stopwatch');
+
+        (new StopwatchCompilerPass())->process($container);
+    }
+}
---
+++ tests/Integration/Service/StopwatchDecoratorTest.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/Service/StopwatchDecoratorTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+
+namespace App\Tests\Integration\Service;
+
+use App\Service\StopwatchDecorator;
+use App\Validator\Constraints\EntityReferenceExists;
+use Generator;
+use ProxyManager\Factory\AccessInterceptorValueHolderFactory;
+use ProxyManager\Proxy\AccessInterceptorValueHolderInterface;
+use stdClass;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+/**
+ * Class StopwatchDecoratorTest
+ *
+ * @package App\Tests\Integration\Service
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+class StopwatchDecoratorTest extends KernelTestCase
+{
+    /**
+     * @dataProvider dataProviderTestThatDecorateMethodReturnsExpected
+     *
+     * @param class-string $expected
+     *
+     * @testdox Test that `decorate` method returns `$expected` when using `$service` instance as an input
+     */
+    public function testThatDecorateMethodReturnsExpected(string $expected, object $service): void
+    {
+        $factory = new AccessInterceptorValueHolderFactory();
+        $stopWatch = new Stopwatch();
+
+        $decorator = new StopwatchDecorator($factory, $stopWatch);
+
+        static::assertInstanceOf($expected, $decorator->decorate($service));
+    }
+
+    public function testThatDecoratorCallsStopWatchStartAndStopMethods(): void
+    {
+        $service = new EntityReferenceExists();
+
+        $factory = new AccessInterceptorValueHolderFactory();
+        $stopWatch = $this->getMockBuilder(Stopwatch::class)->disableOriginalConstructor()->getMock();
+
+        $stopWatch
+            ->expects(static::once())
+            ->method('start')
+            ->with('EntityReferenceExists->getTargets', 'App\Validator\Constraints\EntityReferenceExists');
+
+        $decorator = new StopwatchDecorator($factory, $stopWatch);
+
+        /** @var EntityReferenceExists $decoratedService */
+        $decoratedService = $decorator->decorate($service);
+
+        static::assertSame('property', $decoratedService->getTargets());
+    }
+
+    /**
+     * @return Generator<array{0: string, 1: object}>
+     */
+    public function dataProviderTestThatDecorateMethodReturnsExpected(): Generator
+    {
+        yield [AccessInterceptorValueHolderInterface::class, new EntityReferenceExists()];
+        yield [stdClass::class, new stdClass()];
+    }
+}
