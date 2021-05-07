046c89185b80ea115fb525bce9fb28da08bcd485
tarlepp <tarmo.leppanen@protacon.com>
2021-05-07 19:57:10 +0300

    Refactored tests under `App\Tests\Integration\Validator\Constraints` namespace

--- tests/Integration/Validator/Constraints/LocaleValidatorTest.php
+++ tests/Integration/Validator/Constraints/LocaleValidatorTest.php
@@ -11,11 +11,9 @@ namespace App\Tests\Integration\Validator\Constraints;
 use App\Service\Localization;
 use App\Validator\Constraints\Locale;
 use App\Validator\Constraints\LocaleValidator;
-use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Context\ExecutionContext;
 use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
-use function assert;
 
 /**
  * Class LocaleValidatorTest
@@ -25,88 +23,58 @@ use function assert;
  */
 class LocaleValidatorTest extends KernelTestCase
 {
-    private ?MockObject $builder = null;
-    private MockObject | ExecutionContext | null $context = null;
-    private MockObject | Localization | null $localization = null;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->context = $this->getMockBuilder(ExecutionContext::class)->disableOriginalConstructor()->getMock();
-        $this->builder = $this->getMockBuilder(ConstraintViolationBuilderInterface::class)->getMock();
-        $this->localization = $this->getMockBuilder(Localization::class)->disableOriginalConstructor()->getMock();
-    }
-
     /**
      * @testdox Test that `LocaleValidator::validate` method calls expected service methods
      */
     public function testThatValidateCallsExpectedMethods(): void
     {
-        $this->getLocalizationMock()
+        [$constraintViolationBuilderMock, $executionContextMock, $localizationMock] = $this->getMocks();
+
+        $localizationMock
             ->expects(static::once())
             ->method('getLocales')
             ->willReturn(['bar']);
 
-        $this->getContextMock()
+        $executionContextMock
             ->expects(static::once())
             ->method('buildViolation')
             ->with(Locale::MESSAGE)
-            ->willReturn($this->builder);
+            ->willReturn($constraintViolationBuilderMock);
 
-        $this->getBuilderMock()
+        $constraintViolationBuilderMock
             ->expects(static::once())
             ->method('setParameter')
-            ->willReturn($this->builder);
+            ->willReturn($constraintViolationBuilderMock);
 
-        $this->getBuilderMock()
+        $constraintViolationBuilderMock
             ->expects(static::once())
             ->method('setCode')
             ->with(Locale::INVALID_LOCALE)
-            ->willReturn($this->builder);
+            ->willReturn($constraintViolationBuilderMock);
 
-        $this->getBuilderMock()
+        $constraintViolationBuilderMock
             ->expects(static::once())
             ->method('addViolation');
 
         // Run validator
-        $validator = new LocaleValidator($this->getLocalization());
-        $validator->initialize($this->getContext());
+        $validator = new LocaleValidator($localizationMock);
+        $validator->initialize($executionContextMock);
         $validator->validate('foo', new Locale());
     }
 
-    private function getLocalization(): Localization
+    /**
+     * @return array{
+     *      0: \PHPUnit\Framework\MockObject\MockObject&ConstraintViolationBuilderInterface,
+     *      1: \PHPUnit\Framework\MockObject\MockObject&ExecutionContext,
+     *      2: \PHPUnit\Framework\MockObject\MockObject&Localization,
+     *  }
+     */
+    private function getMocks(): array
     {
-        assert($this->localization instanceof Localization);
-
-        return $this->localization;
-    }
-
-    private function getLocalizationMock(): MockObject
-    {
-        assert($this->localization instanceof MockObject);
-
-        return $this->localization;
-    }
-
-    private function getContext(): ExecutionContext
-    {
-        assert($this->context instanceof ExecutionContext);
-
-        return $this->context;
-    }
-
-    private function getContextMock(): MockObject
-    {
-        assert($this->context instanceof MockObject);
-
-        return $this->context;
-    }
-
-    private function getBuilderMock(): MockObject
-    {
-        assert($this->builder instanceof MockObject);
-
-        return $this->builder;
+        return [
+            $this->getMockBuilder(ConstraintViolationBuilderInterface::class)->getMock(),
+            $this->getMockBuilder(ExecutionContext::class)->disableOriginalConstructor()->getMock(),
+            $this->getMockBuilder(Localization::class)->disableOriginalConstructor()->getMock(),
+        ];
     }
 }
--- tests/Integration/Validator/Constraints/TimezoneValidatorTest.php
+++ tests/Integration/Validator/Constraints/TimezoneValidatorTest.php
@@ -12,11 +12,9 @@ use App\Entity\User;
 use App\Service\Localization;
 use App\Validator\Constraints\Timezone;
 use App\Validator\Constraints\TimezoneValidator;
-use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Context\ExecutionContext;
 use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
-use function assert;
 
 /**
  * Class TimezoneValidatorTest
@@ -26,19 +24,6 @@ use function assert;
  */
 class TimezoneValidatorTest extends KernelTestCase
 {
-    private ?MockObject $builder = null;
-    private MockObject | ExecutionContext | null $context = null;
-    private MockObject | Localization | null $localization = null;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->builder = $this->getMockBuilder(ConstraintViolationBuilderInterface::class)->getMock();
-        $this->context = $this->getMockBuilder(ExecutionContext::class)->disableOriginalConstructor()->getMock();
-        $this->localization = $this->getMockBuilder(Localization::class)->disableOriginalConstructor()->getMock();
-    }
-
     /**
      * @testdox Test that `TimezoneValidator::validate` method calls expected service methods
      */
@@ -48,70 +33,53 @@ class TimezoneValidatorTest extends KernelTestCase
         $user = (new User())
             ->setTimezone('foo/bar');
 
-        $this->getLocalizationMock()
+        [$constraintViolationBuilderMock, $executionContextMock, $localizationMock] = $this->getMocks();
+
+        $localizationMock
             ->expects(static::once())
             ->method('getTimezones')
             ->willReturn(['bar/foo']);
 
-        $this->getContextMock()
+        $executionContextMock
             ->expects(static::once())
             ->method('buildViolation')
             ->with(Timezone::MESSAGE)
-            ->willReturn($this->builder);
+            ->willReturn($constraintViolationBuilderMock);
 
-        $this->getBuilderMock()
+        $constraintViolationBuilderMock
             ->expects(static::once())
             ->method('setParameter')
-            ->willReturn($this->builder);
+            ->willReturn($constraintViolationBuilderMock);
 
-        $this->getBuilderMock()
+        $constraintViolationBuilderMock
             ->expects(static::once())
             ->method('setCode')
             ->with(Timezone::INVALID_TIMEZONE)
-            ->willReturn($this->builder);
+            ->willReturn($constraintViolationBuilderMock);
 
-        $this->getBuilderMock()
+        $constraintViolationBuilderMock
             ->expects(static::once())
             ->method('addViolation');
 
         // Run validator
-        $validator = new TimezoneValidator($this->getLocalization());
-        $validator->initialize($this->getContext());
+        $validator = new TimezoneValidator($localizationMock);
+        $validator->initialize($executionContextMock);
         $validator->validate($user, new Timezone());
     }
 
-    private function getLocalization(): Localization
+    /**
+     * @return array{
+     *      0: \PHPUnit\Framework\MockObject\MockObject&ConstraintViolationBuilderInterface,
+     *      1: \PHPUnit\Framework\MockObject\MockObject&ExecutionContext,
+     *      2: \PHPUnit\Framework\MockObject\MockObject&Localization,
+     *  }
+     */
+    private function getMocks(): array
     {
-        assert($this->localization instanceof Localization);
-
-        return $this->localization;
-    }
-
-    private function getLocalizationMock(): MockObject
-    {
-        assert($this->localization instanceof MockObject);
-
-        return $this->localization;
-    }
-
-    private function getContext(): ExecutionContext
-    {
-        assert($this->context instanceof ExecutionContext);
-
-        return $this->context;
-    }
-
-    private function getContextMock(): MockObject
-    {
-        assert($this->context instanceof MockObject);
-
-        return $this->context;
-    }
-
-    private function getBuilderMock(): MockObject
-    {
-        assert($this->builder instanceof MockObject);
-
-        return $this->builder;
+        return [
+            $this->getMockBuilder(ConstraintViolationBuilderInterface::class)->getMock(),
+            $this->getMockBuilder(ExecutionContext::class)->disableOriginalConstructor()->getMock(),
+            $this->getMockBuilder(Localization::class)->disableOriginalConstructor()->getMock(),
+        ];
     }
 }
