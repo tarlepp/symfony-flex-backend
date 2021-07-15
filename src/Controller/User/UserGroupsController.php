c5be570246963b69231334b400c6d9bd53ba3f9f
tarlepp <tarmo.leppanen@protacon.com>
2021-07-15 17:22:32 +0300

    Removed obsolete import statement

--- src/Controller/User/UserGroupsController.php
+++ src/Controller/User/UserGroupsController.php
@@ -17,7 +17,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
