a2c30474c0155f54ed1e7be67ef09fe312ebd2ca
tarlepp <tarmo.leppanen@protacon.com>
2021-07-24 04:59:18 +0300

    Fix/Command `check-dependencies` added extra `TableSeparator` - literally brain fart

--- src/Command/Utils/CheckDependencies.php
+++ src/Command/Utils/CheckDependencies.php
@@ -155,7 +155,7 @@ class CheckDependencies extends Command
                 // First row of current library
                 if ($row === 0) {
                     // We want to add table separator between different libraries
-                    if ($rows !== 0) {
+                    if ($rows !== []) {
                         $rows[] = new TableSeparator();
                     }
 
