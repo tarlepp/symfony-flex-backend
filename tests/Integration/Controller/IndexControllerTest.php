fe2133a832e2bbd5c89b030f46ab1eb42d617a4d
tarlepp <tarmo.leppanen@protacon.com>
2021-11-14 20:57:05 +0200

    Added missing integration test classes for controllers

---
+++ tests/Integration/Controller/IndexControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/Controller/IndexControllerTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+
+namespace App\Tests\Integration\Controller;
+
+use App\Controller\IndexController;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class IndexControllerTest
+ *
+ * @package App\Tests\Integration\Controller
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+class IndexControllerTest extends KernelTestCase
+{
+    /**
+     * @testdox Test that `__invoke` method returns proper response
+     */
+    public function testThatInvokeMethodReturnsExpectedResponse(): void
+    {
+        $response = (new IndexController())();
+        $content = $response->getContent();
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertNotFalse($content);
+        self::assertJson($content);
+    }
+}
