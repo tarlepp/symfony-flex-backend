267b30856369ad2f3fca9db62dd91f86839e0f17
tarlepp <tarmo.leppanen@protacon.com>
2021-11-14 13:43:32 +0200

    Tests/Added test cases to get more coverage

--- tests/Integration/ArgumentResolver/RestDtoValueResolverTest.php
+++ tests/Integration/ArgumentResolver/RestDtoValueResolverTest.php
@@ -9,9 +9,13 @@ declare(strict_types = 1);
 namespace App\Tests\Integration\ArgumentResolver;
 
 use App\ArgumentResolver\RestDtoValueResolver;
+use App\DTO\RestDtoInterface;
+use App\Rest\Controller;
 use App\Rest\ControllerCollection;
+use App\Utils\Tests\WebTestCase;
 use AutoMapperPlus\AutoMapperInterface;
 use BadMethodCallException;
+use Generator;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
@@ -23,8 +27,26 @@ use Throwable;
  * @package App\Tests\Integration\ArgumentResolver
  * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
-class RestDtoValueResolverTest extends KernelTestCase
+class RestDtoValueResolverTest extends WebTestCase
 {
+    /**
+     * @dataProvider dataProviderTestThatSupportMethodWorksAsExpected
+     *
+     * @testdox Test that `supports` method returns expected result `$expected`
+     */
+    public function testThatSupportMethodWorksAsExpected(
+        bool $expected,
+        ControllerCollection $controllerCollection,
+        Request $request,
+        ArgumentMetadata $argumentMetadata
+    ): void {
+        $autoMapper = $this->getMockBuilder(AutoMapperInterface::class)->getMock();
+
+        $resolver = new RestDtoValueResolver($controllerCollection, $autoMapper);
+
+        self::assertSame($expected, $resolver->supports($request, $argumentMetadata));
+    }
+
     /**
      * @throws Throwable
      *
@@ -51,5 +73,141 @@ class RestDtoValueResolverTest extends KernelTestCase
 
         // Note that we need to actually get current value here
         $resolver->resolve($request, $metadata)->current();
+    }
+
+    /**
+     * @throws Throwable
+     *
+     * @testdox Test that `resolve` method works as expected
+     */
+    public function testThatResolveMethodWorksAsExpected(): void
+    {
+        $controllerCollection = $this->getMockBuilder(ControllerCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $autoMapper = $this->getMockBuilder(AutoMapperInterface::class)->getMock();
+        $controller = $this->getMockBuilder(Controller::class)->getMock();
+        $restDto = $this->getMockBuilder(RestDtoInterface::class)->getMock();
+
+        $resolver = new RestDtoValueResolver($controllerCollection, $autoMapper);
+        $metadata = new ArgumentMetadata('foo', RestDtoInterface::class, false, false, null);
+        $request = new Request(attributes: ['_controller' => 'foo::createAction']);
+
+        $resolver->supports($request, $metadata);
+
+        $dto = $this->getMockBuilder(RestDtoInterface::class)->getMock();
+        $controllerCollection
+            ->expects(self::once())
+            ->method('has')
+            ->with('foo')
+            ->willReturn(true);
+
+        $controllerCollection
+            ->expects(self::once())
+            ->method('get')
+            ->with('foo')
+            ->willReturn($controller);
+
+        $controller
+            ->expects(self::once())
+            ->method('getDtoClass')
+            ->with('createMethod')
+            ->willReturn(RestDtoInterface::class);
+
+        $autoMapper
+            ->expects(self::once())
+            ->method('map')
+            ->with($request, RestDtoInterface::class)
+            ->willReturn($restDto);
+
+        $resolver->supports($request, $metadata);
+        static::assertSame($restDto, $resolver->resolve($request, $metadata)->current());
+    }
+
+    public function dataProviderTestThatSupportMethodWorksAsExpected(): Generator
+    {
+        $controllerCollection = $this->getMockBuilder(ControllerCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $argumentMetaData = new ArgumentMetadata('foo', null, false, false, null);
+
+        yield [
+            false,
+            $controllerCollection,
+            Request::create('/'),
+            $argumentMetaData,
+        ];
+
+        $request = new Request(attributes: ['_controller' => 'foo::bar']);
+
+        yield [
+            false,
+            $controllerCollection,
+            $request,
+            $argumentMetaData,
+        ];
+
+        $controllerCollection = $this->getMockBuilder(ControllerCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $controllerCollection
+            ->expects(self::once())
+            ->method('has')
+            ->with('foo')
+            ->willReturn(false);
+
+        $request = new Request(attributes: ['_controller' => 'foo::createAction']);
+
+        $argumentMetaData = new ArgumentMetadata('foo', RestDtoInterface::class, false, false, null);
+
+        yield [
+            false,
+            $controllerCollection,
+            $request,
+            $argumentMetaData,
+        ];
+
+        $controllerCollection = $this->getMockBuilder(ControllerCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $controllerCollection
+            ->expects(self::never())
+            ->method('has')
+            ->with('foo');
+
+        $request = new Request(attributes: ['_controller' => 'foo::createAction']);
+
+        $argumentMetaData = new ArgumentMetadata('foo', null, false, false, null);
+
+        yield [
+            false,
+            $controllerCollection,
+            $request,
+            $argumentMetaData,
+        ];
+
+        $controllerCollection = $this->getMockBuilder(ControllerCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $controllerCollection
+            ->expects(self::once())
+            ->method('has')
+            ->with('foo')
+            ->willReturn(true);
+
+        $request = new Request(attributes: ['_controller' => 'foo::createAction']);
+
+        $argumentMetaData = new ArgumentMetadata('foo', RestDtoInterface::class, false, false, null);
+
+        yield [
+            true,
+            $controllerCollection,
+            $request,
+            $argumentMetaData,
+        ];
     }
 }
