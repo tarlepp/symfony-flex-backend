4cc920d97fbea1f96b6ad45cb2c7f7000b23bb98
tarlepp <tarmo.leppanen@protacon.com>
2021-11-14 14:09:02 +0200

    Tests/Added missing integration tests for localization controllers

---
+++ tests/Integration/Controller/v1/Localization/TimeZoneControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/Controller/v1/Localization/TimeZoneControllerTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+
+namespace App\Tests\Integration\Controller\v1\Localization;
+
+use App\Controller\v1\Localization\TimeZoneController;
+use App\Service\Localization;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class TimeZoneControllerTest
+ *
+ * @package App\Tests\Integration\Controller\v1\Localization
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+class TimeZoneControllerTest extends KernelTestCase
+{
+    /**
+     * @testdox Test that controller calls expected service method(s) and returns expected response
+     */
+    public function testThatInvokeMethodCallsExpectedServiceMethods(): void
+    {
+        $Localization = $this->getMockBuilder(Localization::class)->disableOriginalConstructor()->getMock();
+
+        $Localization
+            ->expects(static::once())
+            ->method('getTimeZones')
+            ->willReturn([
+                [
+                    'timezone' => 'Europe',
+                    'identifier' => 'Europe/Helsinki',
+                    'offset' => 'GMT+2:00',
+                    'value' => 'Europe/Helsinki',
+                ],
+            ]);
+
+        $response = (new TimeZoneController($Localization))();
+        $content = $response->getContent();
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertNotFalse($content);
+        self::assertJson(
+            '[{"timezone":"Europe","identifier":"Europe/Helsinki","offset":"GMT+2:00","value":"Europe/Helsinki"}]',
+            $content,
+        );
+    }
+}
