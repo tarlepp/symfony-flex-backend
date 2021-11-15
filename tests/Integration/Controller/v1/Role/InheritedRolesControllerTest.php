d805ccaad06c83b0adbfe6fc79ab31cd0ad019ef
tarlepp <tarmo.leppanen@protacon.com>
2021-11-15 17:54:24 +0200

    Added missing test class for `InheritedRolesController`

---
+++ tests/Integration/Controller/v1/Role/InheritedRolesControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/Controller/v1/Role/InheritedRolesControllerTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+
+namespace App\Tests\Integration\Controller\v1\Role;
+
+use App\Controller\v1\Role\InheritedRolesController;
+use App\Entity\Role;
+use App\Security\RolesService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Class InheritedRolesControllerTest
+ *
+ * @package App\Tests\Integration\Controller\v1\Role
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+class InheritedRolesControllerTest extends KernelTestCase
+{
+    public function testThatInvokeMethodCallsExpectedMethodsAndReturnExpected(): void
+    {
+        $rolesService = $this->getMockBuilder(RolesService::class)->disableOriginalConstructor()->getMock();
+        $role = new Role('Test');
+
+        $rolesService
+            ->expects(self::once())
+            ->method('getInheritedRoles')
+            ->with([$role->getId()])
+            ->willReturn([$role]);
+
+        (new InheritedRolesController($rolesService))->__invoke($role);
+    }
+}
