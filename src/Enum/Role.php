52c0dcb2ada1e982af1e1da29b364ccaa0ccc351
tarlepp <tarmo.leppanen@protacon.com>
2022-03-16 19:09:41 +0200

    Added `Role` enum

---
+++ src/Enum/Role.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Enum/Role.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+
+// phpcs:ignoreFile
+
+namespace App\Enum;
+
+use App\Enum\Interfaces\EnumInterface;
+use App\Enum\Traits\GetValues;
+use InvalidArgumentException;
+use function is_string;
+use function mb_strpos;
+use function mb_strtolower;
+use function mb_substr;
+
+/**
+ * Enum Role
+ *
+ * @package App\Enum
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+enum Role: string implements EnumInterface
+{
+    use GetValues;
+
+    case ROLE_LOGGED = 'ROLE_LOGGED';
+    case ROLE_USER = 'ROLE_USER';
+    case ROLE_ADMIN = 'ROLE_ADMIN';
+    case ROLE_ROOT = 'ROLE_ROOT';
+    case ROLE_API = 'ROLE_API';
+
+    public static function getLabelForRole(self|string $role): string
+    {
+        return match (is_string($role) ? self::tryFrom($role) : $role) {
+            self::ROLE_LOGGED => 'Logged in users',
+            self::ROLE_USER => 'Normal users',
+            self::ROLE_ADMIN => 'Admin users',
+            self::ROLE_ROOT => 'Root users',
+            self::ROLE_API => 'API users',
+            default => throw new InvalidArgumentException('Unknown - ' . (is_string($role) ? $role : $role->value)),
+        };
+    }
+
+    public static function getShortForRole(self|string $role): string
+    {
+        $roleEnum = (is_string($role) ? self::tryFrom($role) : $role);
+
+        if ($roleEnum === null) {
+            throw new InvalidArgumentException('Unknown - ' . (is_string($role) ? $role : $role->value));
+        }
+
+        $offset = mb_strpos($roleEnum->value, '_');
+        $offset = $offset !== false ? $offset + 1 : 0;
+
+        return mb_strtolower(mb_substr($roleEnum->value, $offset));
+    }
+
+    public function getLabel(): string
+    {
+        return self::getLabelForRole($this);
+    }
+
+    public function getShort(): string
+    {
+        return self::getShortForRole($this);
+    }
+}
