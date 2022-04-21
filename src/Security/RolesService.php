dbecd3857a0729a069530ceaf0de6c2e905704fe
tarlepp <tarmo.leppanen@protacon.com>
2022-04-21 19:59:54 +0300

    Removed obsolete interface

--- src/Security/RolesService.php
+++ src/Security/RolesService.php
@@ -8,7 +8,6 @@ declare(strict_types = 1);
 
 namespace App\Security;
 
-use App\Security\Interfaces\RolesServiceInterface;
 use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
 use function array_unique;
 use function array_values;
@@ -19,13 +18,20 @@ use function array_values;
  * @package App\Security
  * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
  */
-class RolesService implements RolesServiceInterface
+class RolesService
 {
     public function __construct(
-        private RoleHierarchyInterface $roleHierarchy,
+        private readonly RoleHierarchyInterface $roleHierarchy,
     ) {
     }
 
+    /**
+     * Helper method to get inherited roles for given roles.
+     *
+     * @param array<int, string> $roles
+     *
+     * @return array<int, string>
+     */
     public function getInheritedRoles(array $roles): array
     {
         return array_values(array_unique($this->roleHierarchy->getReachableRoleNames($roles)));
