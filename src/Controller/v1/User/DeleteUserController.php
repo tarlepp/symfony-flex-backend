88423f58fe327870a9e83bf34a69b7e8b5f99451
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2022-12-28 01:18:33 +0200

    Removed obsolete use statement

--- src/Controller/v1/User/DeleteUserController.php
+++ src/Controller/v1/User/DeleteUserController.php
@@ -14,7 +14,6 @@ use App\Rest\Controller;
 use App\Rest\Traits\Methods;
 use App\Security\RolesService;
 use OpenApi\Annotations as OA;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
