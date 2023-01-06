b5634907a4fcffbbd79206ae72187610ec920f17
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2023-01-06 14:27:35 +0200

    Changed rest trait actions to use `Symfony\Component\Routing\Requirement\Requirement` instead of "magic" strings

--- src/Rest/Traits/Actions/Root/FindOneAction.php
+++ src/Rest/Traits/Actions/Root/FindOneAction.php
@@ -13,6 +13,7 @@ use App\Rest\Traits\Methods\FindOneMethod;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Throwable;
 
@@ -36,7 +37,7 @@ trait FindOneAction
     #[Route(
         path: '/{id}',
         requirements: [
-            'id' => '%app.uuid_v1_regex%',
+            'id' => Requirement::UUID_V1,
         ],
         methods: [Request::METHOD_GET],
     )]
