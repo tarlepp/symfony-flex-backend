c23e92aed7319b46e78d181b4616517370621266
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2023-01-11 17:44:08 +0200

    Refactored existing `EnumLanguageType` to use real `Language` enum

---
+++ src/Doctrine/DBAL/Types/RealEnumType.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /src/Doctrine/DBAL/Types/RealEnumType.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+
+namespace App\Doctrine\DBAL\Types;
+
+use App\Enum\Interfaces\DatabaseEnumInterface;
+use BackedEnum;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use InvalidArgumentException;
+use function array_map;
+use function gettype;
+use function implode;
+use function in_array;
+use function is_string;
+
+/**
+ * Class RealEnumType
+ *
+ * @package App\Doctrine\DBAL\Types
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+abstract class RealEnumType extends Type
+{
+    protected static string $name;
+
+    /**
+     * @psalm-var class-string<DatabaseEnumInterface&BackedEnum>
+     */
+    protected static string $enum;
+
+    /**
+     * @return array<int, string>
+     */
+    public static function getValues(): array
+    {
+        return static::$enum::getValues();
+    }
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        $enumDefinition = implode(
+            ', ',
+            array_map(static fn (string $value): string => "'" . $value . "'", static::getValues()),
+        );
+
+        return 'ENUM(' . $enumDefinition . ')';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
+    {
+        if (!in_array($value, static::$enum::cases(), true)) {
+            $message = sprintf(
+                "Invalid '%s' value '%s'",
+                static::$name,
+                is_string($value) ? $value : gettype($value),
+            );
+
+            throw new InvalidArgumentException($message);
+        }
+
+        return (string)parent::convertToDatabaseValue($value->value, $platform);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): DatabaseEnumInterface
+    {
+        $value = (string)parent::convertToPHPValue($value, $platform);
+        $enum = static::$enum::tryFrom($value);
+
+        if ($enum !== null) {
+            return $enum;
+        }
+
+        throw ConversionException::conversionFailedFormat(
+            gettype($value),
+            static::$name,
+            'One of: "' . implode('", "', static::getValues()) . '"',
+        );
+    }
+}
