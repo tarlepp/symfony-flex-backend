fe7ec5cc6d7aead70e8dfc36cae1550a37937599
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2023-03-11 18:52:50 +0200

    Removed obsolete p12 certs

--- docker/nginx/ssl/create-keys.sh
+++ docker/nginx/ssl/create-keys.sh
@@ -7,5 +7,5 @@ openssl req -new -key tls.key -out tls.csr
 openssl x509 -req -in tls.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out tls.crt -days 10000 -sha256 -passin pass: -extfile openssl.cnf
 
 # To export p12 format
-openssl pkcs12 -export -inkey ./rootCA.key -in ./rootCA.pem -out ./rootCA.p12
-openssl pkcs12 -export -inkey ./tls.key -in ./tls.crt -out ./tls.p12
+#openssl pkcs12 -export -inkey ./rootCA.key -in ./rootCA.pem -out ./rootCA.p12
+#openssl pkcs12 -export -inkey ./tls.key -in ./tls.crt -out ./tls.p12
