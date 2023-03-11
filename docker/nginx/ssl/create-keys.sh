c33ed49137c7b5a71f8c32a44cc962a7170f93a5
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2023-03-11 15:55:22 +0200

    Added p12 cert

--- docker/nginx/ssl/create-keys.sh
+++ docker/nginx/ssl/create-keys.sh
@@ -5,3 +5,6 @@ openssl req -x509 -new -nodes -passin pass: -key rootCA.key -sha256 -days 10000 
 openssl genrsa -out tls.key 2048
 openssl req -new -key tls.key -out tls.csr
 openssl x509 -req -in tls.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out tls.crt -days 10000 -sha256 -passin pass: -extfile openssl.cnf
+
+# To export p12 format
+# openssl pkcs12 -export -inkey ./rootCA.key -in ./rootCA.pem -out ./rootCA.p12
