d7eb3c189b3bf5287168520b1f1bcc031f1f975a
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2023-09-30 13:32:31 +0300

    Added database migration

---
+++ migrations/Version20230930102814.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types = 1);
+
+// phpcs:ignoreFile
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230930102814 extends AbstractMigration
+{
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function getDescription(): string
+    {
+        return 'TODO: Describe reason for this migration';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof AbstractMySQLPlatform,
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $sql = <<<SQL
+ALTER TABLE log_request 
+CHANGE headers headers JSON NOT NULL COMMENT '(DC2Type:json)', 
+CHANGE parameters parameters JSON NOT NULL COMMENT '(DC2Type:json)'
+SQL;
+
+        $this->addSql($sql);
+    }
+
+    /**
+    * @noinspection PhpMissingParentCallCommonInspection
+    */
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof AbstractMySQLPlatform,
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $sql = <<<SQL
+ALTER TABLE log_request 
+CHANGE headers headers LONGTEXT NOT NULL COMMENT '(DC2Type:json)', 
+CHANGE parameters parameters LONGTEXT NOT NULL COMMENT '(DC2Type:json)'
+SQL;
+
+
+        $this->addSql('');
+    }
+}
