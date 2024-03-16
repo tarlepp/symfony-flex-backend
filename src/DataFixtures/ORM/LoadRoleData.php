31b3e4ed9cc21cf48c26298cc0ff92bfb8eb520c
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2024-03-17 01:41:32 +0200

    Removed obsolete use statement

--- src/DataFixtures/ORM/LoadRoleData.php
+++ src/DataFixtures/ORM/LoadRoleData.php
@@ -13,7 +13,6 @@ use App\Security\Interfaces\RolesServiceInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Throwable;
 use function array_map;
 
