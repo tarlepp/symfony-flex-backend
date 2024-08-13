de18734d0298a1535a07004b8710f4d1ed5d3d03
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2024-08-14 02:12:55 +0300

    Changed `use Symfony\Component\Routing\Annotation\Route;` to `use Symfony\Component\Routing\Attribute\Route;` within tests

--- tests/E2E/Rest/src/Controller/ControllerForLifeCycleTests.php
+++ tests/E2E/Rest/src/Controller/ControllerForLifeCycleTests.php
@@ -16,7 +16,7 @@ use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\EnumRequirement;
 use Throwable;
 
