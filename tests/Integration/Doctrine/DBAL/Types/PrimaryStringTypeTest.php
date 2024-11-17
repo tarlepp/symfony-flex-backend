a0273ed3cdac8d073562dda6d7ee6cbe5f15fb8c
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2024-11-17 21:15:16 +0200

    Added missing test class

---
+++ tests/Integration/Doctrine/DBAL/Types/PrimaryStringTypeTest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types = 1);
+/**
+ * /tests/Integration/Doctrine/DBAL/Types/PrimaryStringTypeTest.php
+ *
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+
+namespace App\Tests\Integration\Doctrine\DBAL\Types;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * @package App\Tests\Integration\Doctrine\DBAL\Types
+ * @author TLe, Tarmo Leppänen <tarmo.leppanen@pinja.com>
+ */
+class PrimaryStringTypeTest extends KernelTestCase
+{
+    public function testThatSomething(): void
+    {
+        static::markTestSkipped('This test is not yet implemented');
+    }
+}
